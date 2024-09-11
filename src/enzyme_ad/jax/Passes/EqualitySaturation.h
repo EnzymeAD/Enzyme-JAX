9b8247282d6f2c7f8645a703444fd4e80c10a8ed
smjleo <seojun.lee@oriel.ox.ac.uk>
2024-09-11 22:28:27 +0100

    apply formatting

--- src/enzyme_ad/jax/Passes/EqualitySaturation.h
+++ src/enzyme_ad/jax/Passes/EqualitySaturation.h
@@ -13,23 +13,18 @@ struct Shape;
 /**
  * Functions exposed to Rust (Tensat) for getting the cost of new operations.
  */
-uint64_t get_cost(
-Ops op,
-rust::Vec<tensat::Shape> operand_dims,
-rust::Vec<Type> operand_types,
-rust::Vec<tensat::Shape> other_vector_args,
-rust::Vec<int64_t> int_args);
 
-mlir::Type newTensorType(mlir::OpBuilder &builder,
-                   tensat::Shape &dims,
-                   tensat::Type type);
-mlir::Type tensatTypeToMlirType(mlir::OpBuilder &builder,
-                          tensat::Type type);
+uint64_t get_cost(Ops op, rust::Vec<tensat::Shape> operand_dims,
+                  rust::Vec<Type> operand_types,
+                  rust::Vec<tensat::Shape> other_vector_args,
+                  rust::Vec<int64_t> int_args);
 
-rust::Vec<Shape> get_shape(
-Ops op,
-rust::Vec<tensat::Shape> operand_dims,
-rust::Vec<Type> operand_types,
-rust::Vec<tensat::Shape> other_vector_args,
-rust::Vec<int64_t> int_args);
+mlir::Type newTensorType(mlir::OpBuilder &builder, tensat::Shape &dims,
+                         tensat::Type type);
+mlir::Type tensatTypeToMlirType(mlir::OpBuilder &builder, tensat::Type type);
+
+rust::Vec<Shape> get_shape(Ops op, rust::Vec<tensat::Shape> operand_dims,
+                           rust::Vec<Type> operand_types,
+                           rust::Vec<tensat::Shape> other_vector_args,
+                           rust::Vec<int64_t> int_args);
 } // namespace tensat
