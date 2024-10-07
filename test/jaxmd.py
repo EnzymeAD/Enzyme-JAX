431f7acf8cf2fd594ae92e905f251ae380a94612
William S. Moses <gh@wsmoses.com>
2024-10-07 11:10:44 -0400

    remove extra print

--- test/jaxmd.py
+++ test/jaxmd.py
@@ -243,8 +243,8 @@ class JAXMD(EnzymeJaxTest):
             state.chain.tau,
             state.chain.kinetic_energy,
         ]
-        for i, v in enumerate(self.ins):
-            print("i=", i, v)
+        # for i, v in enumerate(self.ins):
+        #    print("i=", i, v)
         self.dins = [x.copy() for x in self.ins]
         self.douts = [x.copy() for x in self.ins]
         self.AllPipelines = pipelines
