d73e8040a807beddc25e3694a16dec1499a90955
Lorenzo Chelini <l.chelini@icloud.com>
2025-01-25 20:18:24 +0100

    nit

--- src/enzyme_ad/jax/Passes/PropagateConstantBound.cpp
+++ src/enzyme_ad/jax/Passes/PropagateConstantBound.cpp
@@ -1,12 +1,9 @@
-//===- RemoveDuplicateFuncDef.cpp - Remove duplicate fund def -------------- //
+//===- PropagateConstantBounds.cpp - Remove duplicate fund def ------------- //
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-//===---------------------------------------------------------------------===//
-//
-// This file implements a pass to remove duplicate function definitions.
 //===---------------------------------------------------------------------===//
 
 #include "src/enzyme_ad/jax/Passes/PassDetails.h"
