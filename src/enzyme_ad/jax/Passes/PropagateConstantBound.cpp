ba9b19cb81a9b839f2f2d6829f72e45925d67f29
Lorenzo Chelini <l.chelini@icloud.com>
2025-01-28 16:30:44 +0100

    Annotate kernel params

--- src/enzyme_ad/jax/Passes/PropagateConstantBound.cpp
+++ src/enzyme_ad/jax/Passes/PropagateConstantBound.cpp
@@ -34,28 +34,80 @@ struct PropagateConstantBoundsPass
     : public enzyme::impl::PropagateConstantBoundsPassBase<
           PropagateConstantBoundsPass> {
 
-  static void attachConstantRangeIfConstant(MLIRContext *ctx,
-                                            Operation *maybeCst,
-                                            Operation *target) {
-    APInt intValue;
-    if (matchPattern(maybeCst, m_ConstantInt(&intValue)))
-      target->setAttr("range", LLVM::ConstantRangeAttr::get(
-                                   ctx, 32, 0, intValue.getSExtValue()));
+  static int32_t getSizeInBytes(Type ty) {
+    int32_t bitWidth = 0;
+    if (auto inType = dyn_cast<IntegerType>(ty)) {
+      bitWidth = inType.getWidth();
+      if (bitWidth == 1)
+        return 1;
+    }
+    if (auto floatType = dyn_cast<FloatType>(ty))
+      bitWidth = floatType.getWidth();
+    assert(bitWidth != 0);
+    return bitWidth / 8;
   }
 
-  static void replaceWithConstantIfConstant(OpBuilder &builder,
-                                            Operation *maybeCst,
-                                            Operation *target) {
-    APInt intValue;
-    auto loc = target->getLoc();
-    if (matchPattern(maybeCst, m_ConstantInt(&intValue))) {
-      builder.setInsertionPoint(target);
-      auto newCst = builder.create<LLVM::ConstantOp>(
-          loc, builder.getI32Type(),
-          builder.getIntegerAttr(builder.getI32Type(),
-                                 intValue.getSExtValue()));
-      target->getResult(0).replaceAllUsesWith(newCst.getResult());
+  static int64_t getMemRefSizeInBytes(Value operand) {
+    auto ty = operand.getType();
+    int64_t numberOfElems = 0;
+    if (auto tensorTy = dyn_cast<RankedTensorType>(ty)) {
+      numberOfElems = tensorTy.getNumElements();
     }
+    return numberOfElems * getSizeInBytes(getElementTypeOrSelf(ty));
+  }
+
+  struct ThreadAndBlockInfo {
+    int64_t threadIdX = std::numeric_limits<int64_t>::min();
+    int64_t threadIdY = std::numeric_limits<int64_t>::min();
+    int64_t threadIdZ = std::numeric_limits<int64_t>::min();
+
+    int64_t blockIdX = std::numeric_limits<int64_t>::min();
+    int64_t blockIdY = std::numeric_limits<int64_t>::min();
+    int64_t blockIdZ = std::numeric_limits<int64_t>::min();
+
+    int64_t blockDimX = std::numeric_limits<int64_t>::min();
+    int64_t blockDimY = std::numeric_limits<int64_t>::min();
+    int64_t blockDimZ = std::numeric_limits<int64_t>::min();
+
+    int64_t gridDimX = std::numeric_limits<int64_t>::min();
+    int64_t gridDimY = std::numeric_limits<int64_t>::min();
+    int64_t gridDimZ = std::numeric_limits<int64_t>::min();
+  };
+
+  static ThreadAndBlockInfo getRange(FunctionOpInterface callee,
+                                     enzymexla::KernelCallOp caller) {
+    ThreadAndBlockInfo info;
+    Region *reg = callee.getCallableRegion();
+
+    APInt intValue;
+    if (matchPattern(caller.getBlockx(), m_ConstantInt(&intValue)))
+      info.threadIdX = intValue.getSExtValue();
+    if (matchPattern(caller.getBlocky(), m_ConstantInt(&intValue)))
+      info.threadIdY = intValue.getSExtValue();
+    if (matchPattern(caller.getBlockz(), m_ConstantInt(&intValue)))
+      info.threadIdZ = intValue.getSExtValue();
+
+    if (matchPattern(caller.getGridx(), m_ConstantInt(&intValue)))
+      info.blockIdX = intValue.getSExtValue();
+    if (matchPattern(caller.getGridy(), m_ConstantInt(&intValue)))
+      info.blockIdY = intValue.getSExtValue();
+    if (matchPattern(caller.getGridz(), m_ConstantInt(&intValue)))
+      info.blockIdZ = intValue.getSExtValue();
+
+    if (matchPattern(caller.getBlockx(), m_ConstantInt(&intValue)))
+      info.blockDimX = intValue.getSExtValue();
+    if (matchPattern(caller.getBlocky(), m_ConstantInt(&intValue)))
+      info.blockDimY = intValue.getSExtValue();
+    if (matchPattern(caller.getBlockz(), m_ConstantInt(&intValue)))
+      info.blockDimZ = intValue.getSExtValue();
+
+    if (matchPattern(caller.getGridx(), m_ConstantInt(&intValue)))
+      info.gridDimX = intValue.getSExtValue();
+    if (matchPattern(caller.getGridy(), m_ConstantInt(&intValue)))
+      info.gridDimY = intValue.getSExtValue();
+    if (matchPattern(caller.getGridz(), m_ConstantInt(&intValue)))
+      info.gridDimZ = intValue.getSExtValue();
+    return info;
   }
 
   void runOnOperation() override {
@@ -64,71 +116,150 @@ struct PropagateConstantBoundsPass
     OpBuilder builder(ctx);
     SymbolTable symTable(moduleOp);
 
+    DenseMap<FunctionOpInterface, SetVector<enzymexla::KernelCallOp>>
+        funcToKernelMap;
     moduleOp->walk([&](enzymexla::KernelCallOp callOp) {
       auto symbolName = callOp.getFn();
-      auto callee = symTable.lookup<LLVM::LLVMFuncOp>(symbolName);
+      auto callee = symTable.lookup<FunctionOpInterface>(symbolName);
       if (!callee)
         return;
+      funcToKernelMap[callee].insert(callOp);
+    });
+    if (funcToKernelMap.empty())
+      return;
+
+    // Iterate over all the callees.
+    for (auto [callee, callers] : funcToKernelMap) {
+      SmallVector<int64_t> dereferenceable(callee.getNumArguments(),
+                                           std::numeric_limits<int64_t>::max());
+      ThreadAndBlockInfo maxRange;
+
+      // Iterate over all the callers for this callee. Compute the max over all
+      // callers for thread/blocks related things while min for dereferenceable
+      // attribute.
+      for (auto caller : callers) {
+        ThreadAndBlockInfo currentRange = getRange(callee, caller);
+        maxRange.threadIdX =
+            std::max(maxRange.threadIdX, currentRange.threadIdX);
+        maxRange.threadIdY =
+            std::max(maxRange.threadIdY, currentRange.threadIdY);
+        maxRange.threadIdZ =
+            std::max(maxRange.threadIdZ, currentRange.threadIdZ);
+
+        maxRange.blockIdX = std::max(maxRange.blockIdX, currentRange.blockIdX);
+        maxRange.blockIdY = std::max(maxRange.blockIdY, currentRange.blockIdY);
+        maxRange.blockIdZ = std::max(maxRange.blockIdZ, currentRange.blockIdZ);
+
+        maxRange.blockDimX =
+            std::max(maxRange.blockDimX, currentRange.blockDimX);
+        maxRange.blockDimY =
+            std::max(maxRange.blockDimY, currentRange.blockDimY);
+        maxRange.blockDimZ =
+            std::max(maxRange.blockDimZ, currentRange.blockDimZ);
+
+        maxRange.gridDimX = std::max(maxRange.gridDimX, currentRange.gridDimX);
+        maxRange.gridDimY = std::max(maxRange.gridDimY, currentRange.gridDimY);
+        maxRange.gridDimZ = std::max(maxRange.gridDimZ, currentRange.gridDimZ);
+
+        for (auto [index, operand] : llvm::enumerate(callee.getArguments())) {
+          dereferenceable[index] =
+              std::min(dereferenceable[index],
+                       getMemRefSizeInBytes(caller.getInputs()[index]));
+        }
+      }
       Region *reg = callee.getCallableRegion();
-      // thread idx
-      reg->walk([&](NVVM::ThreadIdXOp idxOp) {
-        attachConstantRangeIfConstant(ctx, callOp.getBlockx().getDefiningOp(),
-                                      idxOp.getOperation());
+      std::string constantRangeAttrName = "range";
+
+      auto setConstantRangeAttrIfConstant = [&](auto op, int64_t maxValue) {
+        if (maxValue == std::numeric_limits<int64_t>::min())
+          return;
+        op->setAttr(constantRangeAttrName,
+                    LLVM::ConstantRangeAttr::get(ctx, 32, 0, maxValue));
+      };
+
+      reg->walk([&](NVVM::ThreadIdXOp threadIdXOp) {
+        setConstantRangeAttrIfConstant(threadIdXOp, maxRange.threadIdX);
       });
-      reg->walk([&](NVVM::ThreadIdYOp idyOp) {
-        attachConstantRangeIfConstant(ctx, callOp.getBlocky().getDefiningOp(),
-                                      idyOp.getOperation());
+      reg->walk([&](NVVM::ThreadIdYOp threadIdYOp) {
+        setConstantRangeAttrIfConstant(threadIdYOp, maxRange.threadIdY);
       });
-      reg->walk([&](NVVM::ThreadIdZOp idzOp) {
-        attachConstantRangeIfConstant(ctx, callOp.getBlockz().getDefiningOp(),
-                                      idzOp.getOperation());
+      reg->walk([&](NVVM::ThreadIdZOp threadIdZOp) {
+        setConstantRangeAttrIfConstant(threadIdZOp, maxRange.threadIdZ);
       });
-      // thread range
-      reg->walk([&](NVVM::BlockDimXOp blockIdxOp) {
-        replaceWithConstantIfConstant(builder,
-                                      callOp.getBlockx().getDefiningOp(),
-                                      blockIdxOp.getOperation());
-      });
-      reg->walk([&](NVVM::BlockDimYOp blockIdyOp) {
-        replaceWithConstantIfConstant(builder,
-                                      callOp.getBlocky().getDefiningOp(),
-                                      blockIdyOp.getOperation());
-      });
-      reg->walk([&](NVVM::BlockDimZOp blockIdzOp) {
-        replaceWithConstantIfConstant(builder,
-                                      callOp.getBlockz().getDefiningOp(),
-                                      blockIdzOp.getOperation());
-      });
-      // block index
       reg->walk([&](NVVM::BlockIdXOp blkIdxOp) {
-        attachConstantRangeIfConstant(ctx, callOp.getGridx().getDefiningOp(),
-                                      blkIdxOp.getOperation());
+        setConstantRangeAttrIfConstant(blkIdxOp, maxRange.blockIdX);
       });
       reg->walk([&](NVVM::BlockIdYOp blkIdyOp) {
-        attachConstantRangeIfConstant(ctx, callOp.getGridy().getDefiningOp(),
-                                      blkIdyOp.getOperation());
+        setConstantRangeAttrIfConstant(blkIdyOp, maxRange.blockIdY);
       });
       reg->walk([&](NVVM::BlockIdZOp blkIdzOp) {
-        attachConstantRangeIfConstant(ctx, callOp.getGridz().getDefiningOp(),
-                                      blkIdzOp.getOperation());
+        setConstantRangeAttrIfConstant(blkIdzOp, maxRange.blockIdZ);
       });
-      // block range
-      reg->walk([&](NVVM::GridDimXOp gridIdxOp) {
-        replaceWithConstantIfConstant(builder,
-                                      callOp.getGridx().getDefiningOp(),
-                                      gridIdxOp.getOperation());
+
+      // Attempt to replace BlockDim and GridDim with constants if we have a
+      // single caller for this callee and BlockDim and GridDim are known
+      // constants. Otherwise set an interval range, if BlockDim and
+      // GridDim are known constants.
+      auto replaceWithConstantOrSetConstantRangeAttr =
+          [&](auto op, int64_t maxValue, bool hasSingleCaller) {
+            if (maxValue == std::numeric_limits<int64_t>::min())
+              return;
+            if (hasSingleCaller) {
+              builder.setInsertionPoint(op);
+              auto newCst = builder.create<LLVM::ConstantOp>(
+                  op->getLoc(), builder.getI32Type(),
+                  builder.getIntegerAttr(builder.getI32Type(), maxValue));
+              op->getResult(0).replaceAllUsesWith(newCst.getResult());
+            } else {
+              setConstantRangeAttrIfConstant(op, maxValue);
+            }
+          };
+
+      bool hasSingleCaller = callers.size() == 1;
+      reg->walk([&](NVVM::BlockDimXOp blockDimIdXOp) {
+        replaceWithConstantOrSetConstantRangeAttr(
+            blockDimIdXOp, maxRange.blockDimX, hasSingleCaller);
       });
-      reg->walk([&](NVVM::GridDimYOp gridIdyOp) {
-        replaceWithConstantIfConstant(builder,
-                                      callOp.getGridy().getDefiningOp(),
-                                      gridIdyOp.getOperation());
+      reg->walk([&](NVVM::BlockDimYOp blockDimIdYOp) {
+        replaceWithConstantOrSetConstantRangeAttr(
+            blockDimIdYOp, maxRange.blockDimY, hasSingleCaller);
       });
-      reg->walk([&](NVVM::GridDimZOp gridIdzOp) {
-        replaceWithConstantIfConstant(builder,
-                                      callOp.getGridz().getDefiningOp(),
-                                      gridIdzOp.getOperation());
+      reg->walk([&](NVVM::BlockDimZOp blockDimIdZOp) {
+        replaceWithConstantOrSetConstantRangeAttr(
+            blockDimIdZOp, maxRange.blockDimZ, hasSingleCaller);
       });
-    });
+      reg->walk([&](NVVM::GridDimXOp gridDimXOp) {
+        replaceWithConstantOrSetConstantRangeAttr(gridDimXOp, maxRange.gridDimX,
+                                                  hasSingleCaller);
+      });
+      reg->walk([&](NVVM::GridDimYOp gridDimYOp) {
+        replaceWithConstantOrSetConstantRangeAttr(gridDimYOp, maxRange.gridDimY,
+                                                  hasSingleCaller);
+      });
+      reg->walk([&](NVVM::GridDimZOp gridDimZOp) {
+        replaceWithConstantOrSetConstantRangeAttr(gridDimZOp, maxRange.gridDimZ,
+                                                  hasSingleCaller);
+      });
+
+      // Set no-alias for each callee arguments. No-alias is guaranteed.
+      // Set alignment to 128. An alignment of 128 is guaranteed.
+      // Set dereferenceable for each operand in callee.
+      MLIRContext *ctx = callee->getContext();
+      for (auto [index, valTy] : llvm::enumerate(callee.getArgumentTypes())) {
+        if (auto ptr = dyn_cast<LLVM::LLVMPointerType>(valTy)) {
+          callee.setArgAttr(index, LLVM::LLVMDialect::getNoAliasAttrName(),
+                            UnitAttr::get(ctx));
+          callee.setArgAttr(index, LLVM::LLVMDialect::getAlignAttrName(),
+                            IntegerAttr::get(IntegerType::get(ctx, 32), 128));
+          if (dereferenceable[index] != std::numeric_limits<int64_t>::max()) {
+            callee.setArgAttr(index,
+                              LLVM::LLVMDialect::getDereferenceableAttrName(),
+                              IntegerAttr::get(IntegerType::get(ctx, 64),
+                                               dereferenceable[index]));
+          }
+        }
+      }
+    }
   }
 };
 } // end namespace
