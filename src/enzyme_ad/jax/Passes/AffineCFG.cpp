17b9ab111141451f5b43bffce15592d2fdb88263
William S. Moses <gh@wsmoses.com>
2025-02-17 14:48:54 -0600

    Add affinecfg

--- src/enzyme_ad/jax/Passes/AffineCFG.cpp
+++ src/enzyme_ad/jax/Passes/AffineCFG.cpp
@@ -1,5 +1,3 @@
-#include "PassDetails.h"
-
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Affine/Passes.h"
 #include "mlir/Dialect/Arith/IR/Arith.h"
@@ -15,6 +13,7 @@
 #include "llvm/ADT/SmallSet.h"
 #include "llvm/Support/Debug.h"
 #include <deque>
+#include "mlir/Interfaces/FunctionInterfaces.h"
 
 #define DEBUG_TYPE "affine-cfg"
 
@@ -28,8 +27,6 @@ namespace enzyme {
 using namespace mlir;
 using namespace mlir::arith;
 using namespace mlir::affine;
-
-bool isReadOnly(Operation *op);
 
 bool isValidSymbolInt(Value value, bool recur = true);
 bool isValidSymbolInt(Operation *defOp, bool recur) {
@@ -725,7 +722,7 @@ void fully2ComposeIntegerSetAndOperands(PatternRewriter &builder,
 }
 
 namespace {
-struct AffineCFGPass : public enzyme::impl::AffineCFGPassBase<AffineCFGPass> {
+struct AffineCFGPass : public enzyme::impl::AffineCFGBase<AffineCFGPass> {
   void runOnOperation() override;
 };
 } // namespace
@@ -1569,4 +1566,396 @@ void AffineCFGPass::runOnOperation() {
           CanonicalieForBounds>(getOperation()->getContext());
   GreedyRewriteConfig config;
   (void)applyPatternsAndFoldGreedily(getOperation(), std::move(rpl), config);
+}
+
+bool valueCmp(Cmp cmp, Value bval, ValueOrInt val) {
+  if (auto icast = bval.getDefiningOp<IndexCastOp>()) {
+    return valueCmp(cmp, icast.getIn(), val);
+  }
+
+  IntegerAttr iattr;
+  if (matchPattern(bval, m_Constant(&iattr))) {
+    switch (cmp) {
+    case Cmp::EQ:
+      return val == iattr.getValue();
+    case Cmp::LT:
+      return val > iattr.getValue();
+    case Cmp::LE:
+      return val >= iattr.getValue();
+    case Cmp::GT:
+      return val < iattr.getValue();
+    case Cmp::GE:
+      return val <= iattr.getValue();
+    }
+  }
+
+  if (auto baval = dyn_cast<BlockArgument>(bval)) {
+    if (affine::AffineForOp afFor =
+            dyn_cast<affine::AffineForOp>(baval.getOwner()->getParentOp())) {
+      auto for_lb = afFor.getLowerBoundMap().getResults()[baval.getArgNumber()];
+      auto for_ub = afFor.getUpperBoundMap().getResults()[baval.getArgNumber()];
+      switch (cmp) {
+      // \forall i \in [LB, UB) == k   => LB == k and UB == k+1
+      case Cmp::EQ: {
+        if (!valueCmp(Cmp::EQ, for_lb, afFor.getLowerBoundMap().getNumDims(),
+                      afFor.getLowerBoundOperands(), val))
+          return false;
+        if (!val.isValue) {
+          if (!valueCmp(Cmp::EQ, for_ub, afFor.getUpperBoundMap().getNumDims(),
+                        afFor.getUpperBoundOperands(), val.i_val + 1))
+            return false;
+          return true;
+        }
+        return false;
+      }
+      // \forall i \in [LB, UB) < k   => UB <= k
+      case Cmp::LT: {
+        return valueCmp(Cmp::LE, for_ub, afFor.getUpperBoundMap().getNumDims(),
+                        afFor.getUpperBoundOperands(), val);
+      }
+      // \forall i \in [LB, UB) <= k   => UB-1 <= k  => UB <= k+1
+      case Cmp::LE: {
+        if (!val.isValue) {
+          return valueCmp(Cmp::LE, for_ub,
+                          afFor.getUpperBoundMap().getNumDims(),
+                          afFor.getUpperBoundOperands(), val.i_val + 1);
+        }
+        return valueCmp(Cmp::LE, for_ub, afFor.getUpperBoundMap().getNumDims(),
+                        afFor.getUpperBoundOperands(), val);
+      }
+      // \forall i \in [LB, UB) > k   => LB > k
+      case Cmp::GT: {
+        return valueCmp(Cmp::GT, for_lb, afFor.getLowerBoundMap().getNumDims(),
+                        afFor.getLowerBoundOperands(), val);
+      }
+      // \forall i \in [LB, UB) >= k   => LB >= k
+      case Cmp::GE: {
+        return valueCmp(Cmp::GE, for_lb, afFor.getLowerBoundMap().getNumDims(),
+                        afFor.getLowerBoundOperands(), val);
+      }
+      }
+    }
+    if (affine::AffineParallelOp afFor = dyn_cast<affine::AffineParallelOp>(
+            baval.getOwner()->getParentOp())) {
+      switch (cmp) {
+      // \forall i \in [max(LB...), min(UB...)) == k   => all(LB == k) and
+      // all(UB == k+1)
+      case Cmp::EQ: {
+        for (auto for_lb :
+             afFor.getLowerBoundMap(baval.getArgNumber()).getResults())
+          if (!valueCmp(Cmp::EQ, for_lb, afFor.getLowerBoundsMap().getNumDims(),
+                        afFor.getLowerBoundsOperands(), val))
+            return false;
+        if (!val.isValue) {
+          for (auto for_ub :
+               afFor.getUpperBoundMap(baval.getArgNumber()).getResults())
+            if (!valueCmp(Cmp::EQ, for_ub,
+                          afFor.getUpperBoundsMap().getNumDims(),
+                          afFor.getUpperBoundsOperands(), val.i_val + 1))
+              return false;
+          return true;
+        }
+        return false;
+      }
+      // \forall i \in [max(LB...), min(UB...)) < k   => any(UB <= k)
+      case Cmp::LT: {
+        for (auto for_ub :
+             afFor.getUpperBoundMap(baval.getArgNumber()).getResults())
+          if (valueCmp(Cmp::LE, for_ub, afFor.getUpperBoundsMap().getNumDims(),
+                       afFor.getUpperBoundsOperands(), val))
+            return true;
+        return false;
+      }
+      // \forall i \in [max(LB...), min(UB...)) <= k   => any(UB-1 <= k)  =>
+      // any(UB <= k+1)
+      case Cmp::LE: {
+        if (!val.isValue) {
+          for (auto for_ub :
+               afFor.getUpperBoundMap(baval.getArgNumber()).getResults())
+            if (valueCmp(Cmp::LE, for_ub,
+                         afFor.getUpperBoundsMap().getNumDims(),
+                         afFor.getUpperBoundsOperands(), val.i_val + 1))
+              return true;
+          return false;
+        }
+
+        for (auto for_ub :
+             afFor.getUpperBoundMap(baval.getArgNumber()).getResults())
+          if (valueCmp(Cmp::LE, for_ub, afFor.getUpperBoundsMap().getNumDims(),
+                       afFor.getUpperBoundsOperands(), val))
+            return true;
+        return false;
+      }
+      // \forall i \in [max(LB...), min(UB...)) > k   => any(LB > k)
+      case Cmp::GT: {
+        for (auto for_lb :
+             afFor.getLowerBoundMap(baval.getArgNumber()).getResults())
+          if (valueCmp(Cmp::GT, for_lb, afFor.getLowerBoundsMap().getNumDims(),
+                       afFor.getLowerBoundsOperands(), val))
+            return true;
+        return false;
+      }
+      // \forall i \in [max(LB...), min(UB...)) >= k   => any(LB >= k)
+      case Cmp::GE: {
+        for (auto for_lb :
+             afFor.getLowerBoundMap(baval.getArgNumber()).getResults())
+          if (valueCmp(Cmp::GE, for_lb, afFor.getLowerBoundsMap().getNumDims(),
+                       afFor.getLowerBoundsOperands(), val))
+            return true;
+        return false;
+      }
+      }
+    }
+
+    if (scf::ForOp afFor =
+            dyn_cast<scf::ForOp>(baval.getOwner()->getParentOp())) {
+      if (baval.getArgNumber() == 0) {
+        auto for_lb = afFor.getLowerBound();
+        auto for_ub = afFor.getUpperBound();
+        switch (cmp) {
+        // \forall i \in [LB, UB) == k   => LB == k and UB == k+1
+        case Cmp::EQ: {
+          if (!valueCmp(Cmp::EQ, for_lb, val))
+            return false;
+          if (!val.isValue) {
+            if (!valueCmp(Cmp::EQ, for_ub, val.i_val + 1))
+              return false;
+            return true;
+          }
+          return false;
+        }
+        // \forall i \in [LB, UB) < k   => UB <= k
+        case Cmp::LT: {
+          return valueCmp(Cmp::LE, for_ub, val);
+        }
+        // \forall i \in [LB, UB) <= k   => UB-1 <= k  => UB <= k+1
+        case Cmp::LE: {
+          if (!val.isValue) {
+            return valueCmp(Cmp::LE, for_ub, val.i_val + 1);
+          }
+          return valueCmp(Cmp::LE, for_ub, val);
+        }
+        // \forall i \in [LB, UB) > k   => LB > k
+        case Cmp::GT: {
+          return valueCmp(Cmp::GT, for_lb, val);
+        }
+        // \forall i \in [LB, UB) >= k   => LB >= k
+        case Cmp::GE: {
+          return valueCmp(Cmp::GE, for_lb, val);
+        }
+        }
+      }
+    }
+
+    if (scf::ParallelOp afFor =
+            dyn_cast<scf::ParallelOp>(baval.getOwner()->getParentOp())) {
+      auto for_lb = afFor.getLowerBound()[baval.getArgNumber()];
+      auto for_ub = afFor.getUpperBound()[baval.getArgNumber()];
+      switch (cmp) {
+      // \forall i \in [LB, UB) == k   => LB == k and UB == k+1
+      case Cmp::EQ: {
+        if (!valueCmp(Cmp::EQ, for_lb, val))
+          return false;
+        if (!val.isValue) {
+          if (!valueCmp(Cmp::EQ, for_ub, val.i_val + 1))
+            return false;
+          return true;
+        }
+        return false;
+      }
+      // \forall i \in [LB, UB) < k   => UB <= k
+      case Cmp::LT: {
+        return valueCmp(Cmp::LE, for_ub, val);
+      }
+      // \forall i \in [LB, UB) <= k   => UB-1 <= k  => UB <= k+1
+      case Cmp::LE: {
+        if (!val.isValue) {
+          return valueCmp(Cmp::LE, for_ub, val.i_val + 1);
+        }
+        return valueCmp(Cmp::LE, for_ub, val);
+      }
+      // \forall i \in [LB, UB) > k   => LB > k
+      case Cmp::GT: {
+        return valueCmp(Cmp::GT, for_lb, val);
+      }
+      // \forall i \in [LB, UB) >= k   => LB >= k
+      case Cmp::GE: {
+        return valueCmp(Cmp::GE, for_lb, val);
+      }
+      }
+    }
+  }
+  if (val.isValue && val.v_val == bval) {
+    switch (cmp) {
+    case Cmp::EQ:
+      return true;
+    case Cmp::LT:
+      return false;
+    case Cmp::LE:
+      return true;
+    case Cmp::GT:
+      return false;
+    case Cmp::GE:
+      return true;
+    }
+  }
+  return false;
+}
+
+bool valueCmp(Cmp cmp, AffineExpr expr, size_t numDim, ValueRange operands,
+              ValueOrInt val) {
+
+  if (auto opd = expr.dyn_cast<AffineConstantExpr>()) {
+    switch (cmp) {
+    case Cmp::EQ:
+      return val == opd.getValue();
+    case Cmp::LT:
+      return val > opd.getValue();
+    case Cmp::LE:
+      return val >= opd.getValue();
+    case Cmp::GT:
+      return val < opd.getValue();
+    case Cmp::GE:
+      return val <= opd.getValue();
+    }
+  }
+  if (auto opd = expr.dyn_cast<AffineDimExpr>()) {
+    return valueCmp(cmp, operands[opd.getPosition()], val);
+  }
+  if (auto opd = expr.dyn_cast<AffineSymbolExpr>()) {
+    return valueCmp(cmp, operands[opd.getPosition() + numDim], val);
+  }
+
+  if (auto bop = expr.dyn_cast<AffineBinaryOpExpr>()) {
+    if (bop.getKind() == AffineExprKind::Add) {
+      switch (cmp) {
+      case Cmp::EQ:
+        return (valueCmp(cmp, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(cmp, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, val));
+      case Cmp::LT:
+        return (valueCmp(cmp, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(Cmp::LE, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(Cmp::LE, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, val)) ||
+               (valueCmp(Cmp::LE, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(cmp, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(Cmp::LE, bop.getRHS(), numDim, operands, val));
+      case Cmp::LE:
+        return (valueCmp(cmp, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(cmp, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, val));
+      case Cmp::GT:
+        return (valueCmp(cmp, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(Cmp::GE, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(Cmp::GE, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, val)) ||
+               (valueCmp(Cmp::GE, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(cmp, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(Cmp::GE, bop.getRHS(), numDim, operands, val));
+      case Cmp::GE:
+        return (valueCmp(cmp, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(cmp, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(cmp, bop.getRHS(), numDim, operands, val));
+      }
+    }
+    if (bop.getKind() == AffineExprKind::Mul && val == 0) {
+      switch (cmp) {
+      case Cmp::EQ:
+        return (valueCmp(cmp, bop.getLHS(), numDim, operands, val) ||
+                valueCmp(cmp, bop.getRHS(), numDim, operands, val));
+      case Cmp::LT:
+        return (valueCmp(Cmp::LT, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(Cmp::GT, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(Cmp::GT, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(Cmp::LT, bop.getRHS(), numDim, operands, val));
+      case Cmp::LE:
+        return valueCmp(Cmp::EQ, bop.getLHS(), numDim, operands, val) ||
+               valueCmp(Cmp::EQ, bop.getRHS(), numDim, operands, val) ||
+               ((valueCmp(Cmp::GE, bop.getLHS(), numDim, operands, 0) &&
+                 valueCmp(Cmp::LE, bop.getRHS(), numDim, operands, val)) ||
+                (valueCmp(Cmp::LE, bop.getLHS(), numDim, operands, 0) &&
+                 valueCmp(Cmp::GE, bop.getRHS(), numDim, operands, val)));
+      case Cmp::GT:
+        return (valueCmp(Cmp::LT, bop.getLHS(), numDim, operands, val) &&
+                valueCmp(Cmp::LT, bop.getRHS(), numDim, operands, 0)) ||
+               (valueCmp(Cmp::GT, bop.getLHS(), numDim, operands, 0) &&
+                valueCmp(Cmp::GT, bop.getRHS(), numDim, operands, val));
+      case Cmp::GE:
+        return valueCmp(Cmp::EQ, bop.getLHS(), numDim, operands, val) ||
+               valueCmp(Cmp::EQ, bop.getRHS(), numDim, operands, val) ||
+               ((valueCmp(Cmp::GE, bop.getLHS(), numDim, operands, 0) &&
+                 valueCmp(Cmp::GE, bop.getRHS(), numDim, operands, val)) ||
+                (valueCmp(Cmp::LE, bop.getLHS(), numDim, operands, 0) &&
+                 valueCmp(Cmp::LE, bop.getRHS(), numDim, operands, val)));
+      }
+    }
+  }
+  return false;
+}
+
+bool isReadOnly(Operation *op) {
+  bool hasRecursiveEffects = op->hasTrait<OpTrait::HasRecursiveMemoryEffects>();
+  if (hasRecursiveEffects) {
+    for (Region &region : op->getRegions()) {
+      for (auto &block : region) {
+        for (auto &nestedOp : block)
+          if (!isReadOnly(&nestedOp))
+            return false;
+      }
+    }
+    return true;
+  }
+
+  // If the op has memory effects, try to characterize them to see if the op
+  // is trivially dead here.
+  if (auto effectInterface = dyn_cast<MemoryEffectOpInterface>(op)) {
+    // Check to see if this op either has no effects, or only allocates/reads
+    // memory.
+    SmallVector<MemoryEffects::EffectInstance, 1> effects;
+    effectInterface.getEffects(effects);
+    if (!llvm::all_of(effects, [op](const MemoryEffects::EffectInstance &it) {
+          return isa<MemoryEffects::Read>(it.getEffect());
+        })) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+}
+
+bool isReadNone(Operation *op) {
+  bool hasRecursiveEffects = op->hasTrait<OpTrait::HasRecursiveMemoryEffects>();
+  if (hasRecursiveEffects) {
+    for (Region &region : op->getRegions()) {
+      for (auto &block : region) {
+        for (auto &nestedOp : block)
+          if (!isReadNone(&nestedOp))
+            return false;
+      }
+    }
+    return true;
+  }
+
+  // If the op has memory effects, try to characterize them to see if the op
+  // is trivially dead here.
+  if (auto effectInterface = dyn_cast<MemoryEffectOpInterface>(op)) {
+    // Check to see if this op either has no effects, or only allocates/reads
+    // memory.
+    SmallVector<MemoryEffects::EffectInstance, 1> effects;
+    effectInterface.getEffects(effects);
+    if (llvm::any_of(effects, [op](const MemoryEffects::EffectInstance &it) {
+          return isa<MemoryEffects::Read>(it.getEffect()) ||
+                 isa<MemoryEffects::Write>(it.getEffect());
+        })) {
+      return false;
+    }
+    return true;
+  }
+  return false;
 }
