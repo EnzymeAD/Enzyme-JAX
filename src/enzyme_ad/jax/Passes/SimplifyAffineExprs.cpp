ce7bf5d077d450743ca48674fa8c76ee2203ac53
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2025-02-22 15:56:56 +0900

    Go back to AffineExpr

--- src/enzyme_ad/jax/Passes/SimplifyAffineExprs.cpp
+++ src/enzyme_ad/jax/Passes/SimplifyAffineExprs.cpp
@@ -10,8 +10,11 @@
 #include "src/enzyme_ad/jax/Dialect/Ops.h"
 
 #include <isl/aff.h>
+#include <isl/ast.h>
+#include <isl/ast_build.h>
 #include <isl/constraint.h>
 #include <isl/ctx.h>
+#include <isl/id.h>
 #include <isl/local_space.h>
 #include <isl/map.h>
 #include <isl/mat.h>
@@ -20,7 +23,11 @@
 #include <isl/space_type.h>
 #include <isl/val.h>
 
-#define DEBUG_TYPE "llvm-to-memref-access"
+extern "C" {
+#include <isl_ast_build_expr.h>
+}
+
+#define DEBUG_TYPE "simplify-affine-exprs"
 
 namespace mlir {
 namespace enzyme {
@@ -113,40 +120,244 @@ std::tuple<isl_set *, FlatAffineValueConstraints> getDomain(isl_ctx *ctx,
           cst};
 }
 
-isl_aff *getIslAff(AffineExpr expr, isl_local_space *ls, isl_ctx *ctx,
-                   llvm::MapVector<unsigned, unsigned> posMap) {
-  if (auto bo = dyn_cast<AffineBinaryOpExpr>(expr)) {
-    isl_aff *lhs = getIslAff(bo.getLHS(), ls, ctx, posMap);
-    isl_aff *rhs = getIslAff(bo.getRHS(), ls, ctx, posMap);
-    switch (bo.getKind()) {
-    case mlir::AffineExprKind::Add:
-      return isl_aff_add(lhs, rhs);
-    case mlir::AffineExprKind::CeilDiv:
-      return isl_aff_ceil(isl_aff_div(lhs, rhs));
-    case mlir::AffineExprKind::FloorDiv:
-      return isl_aff_floor(isl_aff_div(lhs, rhs));
-    case mlir::AffineExprKind::Mod:
-      return isl_aff_mod_val(lhs, isl_aff_get_constant_val(rhs));
-    case mlir::AffineExprKind::Mul:
-      return isl_aff_mul(lhs, rhs);
-    default:
-      LLVM_DEBUG(llvm::dbgs()
-                 << "Unhandled kind " << (unsigned)bo.getKind() << "\n");
+using PosMapTy = llvm::MapVector<unsigned, unsigned>;
+struct AffineExprToIslAffConverter {
+  PosMapTy dimPosMap;
+  PosMapTy symPosMap;
+  isl_aff *getIslAff(AffineExpr expr, isl_local_space *ls, isl_ctx *ctx) {
+    if (auto bo = dyn_cast<AffineBinaryOpExpr>(expr)) {
+      isl_aff *lhs = getIslAff(bo.getLHS(), ls, ctx);
+      isl_aff *rhs = getIslAff(bo.getRHS(), ls, ctx);
+      switch (bo.getKind()) {
+      case mlir::AffineExprKind::Add:
+        return isl_aff_add(lhs, rhs);
+      case mlir::AffineExprKind::CeilDiv:
+        return isl_aff_ceil(isl_aff_div(lhs, rhs));
+      case mlir::AffineExprKind::FloorDiv:
+        return isl_aff_floor(isl_aff_div(lhs, rhs));
+      case mlir::AffineExprKind::Mod:
+        return isl_aff_mod_val(lhs, isl_aff_get_constant_val(rhs));
+      case mlir::AffineExprKind::Mul:
+        return isl_aff_mul(lhs, rhs);
+      default:
+        LLVM_DEBUG(llvm::dbgs()
+                   << "Unhandled kind " << (unsigned)bo.getKind() << "\n");
+        return nullptr;
+      }
+    } else if (auto c = dyn_cast<AffineConstantExpr>(expr)) {
+      return isl_aff_val_on_domain(isl_local_space_copy(ls),
+                                   isl_val_int_from_si(ctx, c.getValue()));
+    } else if (auto dim = dyn_cast<AffineDimExpr>(expr)) {
+      unsigned pos = dimPosMap[dim.getPosition()];
+      return isl_aff_var_on_domain(isl_local_space_copy(ls), isl_dim_set, pos);
+    } else if (auto sym = dyn_cast<AffineSymbolExpr>(expr)) {
+      unsigned pos = symPosMap[sym.getPosition()];
+      return isl_aff_var_on_domain(isl_local_space_copy(ls), isl_dim_param,
+                                   pos);
+    }
+    LLVM_DEBUG(llvm::dbgs() << "Unhandled expr " << expr << "\n");
+    return nullptr;
+  }
+};
+
+struct IslToAffineExprConverter {
+  MLIRContext *mlirContext;
+  unsigned symOffset;
+  PosMapTy dimPosMap;
+  PosMapTy symPosMap;
+
+  AffineExpr createOpBin(__isl_take isl_ast_expr *Expr) {
+    AffineExpr LHS, RHS, Res;
+    isl_ast_op_type OpType;
+    assert(isl_ast_expr_get_type(Expr) == isl_ast_expr_op &&
+           "isl ast expression not of type isl_ast_op");
+    assert(isl_ast_expr_get_op_n_arg(Expr) == 2 &&
+           "not a binary isl ast expression");
+
+    OpType = isl_ast_expr_get_op_type(Expr);
+
+    LHS = create(isl_ast_expr_get_op_arg(Expr, 0));
+    RHS = create(isl_ast_expr_get_op_arg(Expr, 1));
+
+    if (!LHS || !RHS) {
+      isl_ast_expr_free(Expr);
       return nullptr;
     }
-  } else if (auto c = dyn_cast<AffineConstantExpr>(expr)) {
-    return isl_aff_val_on_domain(isl_local_space_copy(ls),
-                                 isl_val_int_from_si(ctx, c.getValue()));
-  } else if (auto dim = dyn_cast<AffineDimExpr>(expr)) {
-    unsigned pos = posMap[dim.getPosition()];
-    return isl_aff_var_on_domain(isl_local_space_copy(ls), isl_dim_set, pos);
-  } else if (auto sym = dyn_cast<AffineSymbolExpr>(expr)) {
-    unsigned pos = posMap[dim.getPosition()];
-    return isl_aff_var_on_domain(isl_local_space_copy(ls), isl_dim_param, pos);
+
+    switch (OpType) {
+    default:
+      llvm_unreachable("This is no binary isl ast expression");
+    case isl_ast_op_add:
+      Res = LHS + RHS;
+      break;
+    case isl_ast_op_sub:
+      Res = LHS - RHS;
+      break;
+    case isl_ast_op_mul:
+      Res = (LHS * RHS);
+      break;
+    case isl_ast_op_div:
+      Res = LHS.floorDiv(RHS);
+      break;
+    case isl_ast_op_pdiv_q: // Dividend is non-negative
+      Res = LHS.floorDiv(RHS);
+      break;
+    case isl_ast_op_fdiv_q: { // Round towards -infty
+      Res = LHS.floorDiv(RHS);
+      break;
+    }
+    case isl_ast_op_pdiv_r: // Dividend is non-negative
+      Res = LHS % RHS;
+      break;
+    case isl_ast_op_zdiv_r: // Result only compared against zero
+      Res = LHS % RHS;
+      break;
+    }
+    isl_ast_expr_free(Expr);
+    return Res;
   }
-  LLVM_DEBUG(llvm::dbgs() << "Unhandled expr " << expr << "\n");
-  return nullptr;
-}
+
+  AffineExpr createOpUnary(__isl_take isl_ast_expr *Expr) {
+    assert(isl_ast_expr_get_op_type(Expr) == isl_ast_op_minus &&
+           "Unsupported unary operation");
+
+    AffineExpr V = create(isl_ast_expr_get_op_arg(Expr, 0));
+
+    isl_ast_expr_free(Expr);
+    return -V;
+  }
+
+  AffineExpr createOp(__isl_take isl_ast_expr *Expr) {
+    assert(isl_ast_expr_get_type(Expr) == isl_ast_expr_op &&
+           "Expression not of type isl_ast_expr_op");
+    switch (isl_ast_expr_get_op_type(Expr)) {
+    case isl_ast_op_error:
+    case isl_ast_op_cond:
+    case isl_ast_op_call:
+    case isl_ast_op_member:
+      break;
+    case isl_ast_op_access:
+      break;
+    case isl_ast_op_max:
+    case isl_ast_op_min:
+      break;
+    case isl_ast_op_add:
+    case isl_ast_op_sub:
+    case isl_ast_op_mul:
+    case isl_ast_op_div:
+    case isl_ast_op_fdiv_q: // Round towards -infty
+    case isl_ast_op_pdiv_q: // Dividend is non-negative
+    case isl_ast_op_pdiv_r: // Dividend is non-negative
+    case isl_ast_op_zdiv_r: // Result only compared against zero
+      return createOpBin(Expr);
+    case isl_ast_op_minus:
+      return createOpUnary(Expr);
+    case isl_ast_op_select:
+      break;
+    case isl_ast_op_and:
+    case isl_ast_op_or:
+      break;
+    case isl_ast_op_and_then:
+    case isl_ast_op_or_else:
+      break;
+    case isl_ast_op_eq:
+    case isl_ast_op_le:
+    case isl_ast_op_lt:
+    case isl_ast_op_ge:
+    case isl_ast_op_gt:
+      break;
+    case isl_ast_op_address_of:
+      break;
+    }
+    isl_ast_expr_free(Expr);
+    return nullptr;
+  }
+
+  APInt APIntFromVal(__isl_take isl_val *Val) {
+    uint64_t *Data;
+    int NumChunks;
+    const static int ChunkSize = sizeof(uint64_t);
+
+    assert(isl_val_is_int(Val) && "Only integers can be converted to APInt");
+
+    NumChunks = isl_val_n_abs_num_chunks(Val, ChunkSize);
+    Data = (uint64_t *)malloc(NumChunks * ChunkSize);
+    isl_val_get_abs_num_chunks(Val, ChunkSize, Data);
+    int NumBits = CHAR_BIT * ChunkSize * NumChunks;
+    APInt A(NumBits, NumChunks, Data);
+
+    // As isl provides only an interface to obtain data that describes the
+    // absolute value of an isl_val, A at this point always contains a positive
+    // number. In case Val was originally negative, we expand the size of A by
+    // one and negate the value (in two's complement representation). As a
+    // result, the new value in A corresponds now with Val.
+    if (isl_val_is_neg(Val)) {
+      A = A.zext(A.getBitWidth() + 1);
+      A = -A;
+    }
+
+    // isl may represent small numbers with more than the minimal number of
+    // bits. We truncate the APInt to the minimal number of bits needed to
+    // represent the signed value it contains, to ensure that the bitwidth is
+    // always minimal.
+    if (A.getSignificantBits() < A.getBitWidth())
+      A = A.trunc(A.getSignificantBits());
+
+    free(Data);
+    isl_val_free(Val);
+    return A;
+  }
+
+  AffineExpr createInt(__isl_take isl_ast_expr *Expr) {
+    assert(isl_ast_expr_get_type(Expr) == isl_ast_expr_int &&
+           "Expression not of type isl_ast_expr_int");
+    isl_val *Val;
+    APInt APValue;
+    Val = isl_ast_expr_get_val(Expr);
+    APValue = APIntFromVal(Val);
+
+    AffineExpr V = getAffineConstantExpr(APValue.getSExtValue(), mlirContext);
+    isl_ast_expr_free(Expr);
+    return V;
+  }
+
+  AffineExpr createId(__isl_take isl_ast_expr *Expr) {
+    assert(isl_ast_expr_get_type(Expr) == isl_ast_expr_id &&
+           "Expression not of type isl_ast_expr_ident");
+
+    isl_id *Id;
+    AffineExpr V;
+
+    Id = isl_ast_expr_get_id(Expr);
+
+    unsigned id = (uintptr_t)isl_id_get_user(Id);
+    id = id - 1;
+    if (id < symOffset)
+      V = getAffineDimExpr(dimPosMap[id], mlirContext);
+    else
+      V = getAffineSymbolExpr(symPosMap[id - symOffset], mlirContext);
+
+    isl_id_free(Id);
+    isl_ast_expr_free(Expr);
+
+    return V;
+  }
+
+  AffineExpr create(__isl_take isl_ast_expr *Expr) {
+    switch (isl_ast_expr_get_type(Expr)) {
+    case isl_ast_expr_error:
+      break;
+    case isl_ast_expr_op:
+      return createOp(Expr);
+    case isl_ast_expr_int:
+      return createInt(Expr);
+    case isl_ast_expr_id:
+      return createId(Expr);
+    }
+    isl_ast_expr_free(Expr);
+    return nullptr;
+  }
+};
 
 struct SimplifyAffineExprsPass
     : public enzyme::impl::SimplifyAffineExprsPassBase<
@@ -160,15 +371,29 @@ struct SimplifyAffineExprsPass
     op->walk([&](AffineLoadOp load) {
       LLVM_DEBUG(llvm::dbgs() << "Got domain\n");
       auto [domain, cst] = getDomain(ctx, load);
-      isl_set_dump(domain);
+      LLVM_DEBUG(isl_set_dump(domain));
       AffineMap map = load.getMap();
       AffineValueMap avm(map, load.getOperands(), {});
-      llvm::MapVector<unsigned, unsigned> posMap;
-      for (unsigned i = 0; i < cst.getNumVars(); i++) {
-        for (unsigned origDim = 0; origDim < map.getNumInputs(); origDim++) {
+      PosMapTy dimPosMap;
+      PosMapTy dimPosMapReverse;
+      PosMapTy symPosMap;
+      PosMapTy symPosMapReverse;
+      for (unsigned i = 0; i < cst.getNumDimVars(); i++) {
+        for (unsigned origDim = 0; origDim < map.getNumDims(); origDim++) {
           Value dim = avm.getOperand(origDim);
           if (cst.getValue(i) == dim) {
-            posMap[origDim] = i;
+            dimPosMap[origDim] = i;
+            dimPosMapReverse[i] = origDim;
+            break;
+          }
+        }
+      }
+      for (unsigned i = 0; i < cst.getNumSymbolVars(); i++) {
+        for (unsigned origSym = 0; origSym < map.getNumSymbols(); origSym++) {
+          Value dim = avm.getOperand(origSym + map.getNumDims());
+          if (cst.getValue(i) == dim) {
+            symPosMap[origSym] = i;
+            symPosMapReverse[i] = origSym;
             break;
           }
         }
@@ -176,20 +401,42 @@ struct SimplifyAffineExprsPass
 
       isl_space *space =
           isl_space_set_alloc(ctx, cst.getNumSymbolVars(), cst.getNumDimVars());
+      for (unsigned i = 0; i < cst.getNumDimVars(); i++) {
+        isl_id *id = isl_id_alloc(ctx, "dim", (void *)(i + 1));
+        space = isl_space_set_dim_id(space, isl_dim_set, i, id);
+      }
+      unsigned symOffset = cst.getNumDimVars();
+      for (unsigned i = 0; i < cst.getNumSymbolVars(); i++) {
+        isl_id *id = isl_id_alloc(ctx, "sym", (void *)(symOffset + i + 1));
+        space = isl_space_set_dim_id(space, isl_dim_set, i, id);
+      }
+
+      isl_ast_build *build =
+          isl_ast_build_from_context(isl_set_universe(isl_space_copy(space)));
+      AffineExprToIslAffConverter m2i{dimPosMap, symPosMap};
+      IslToAffineExprConverter i2m{op->getContext(), symOffset,
+                                   dimPosMapReverse, symPosMapReverse};
       for (unsigned i = 0; i < map.getNumResults(); i++) {
-        AffineExpr expr = map.getResult(i);
-        LLVM_DEBUG(llvm::dbgs() << "Handling AffineExpr\n" << expr << "\n");
+        AffineExpr mlirExpr = map.getResult(i);
+        LLVM_DEBUG(llvm::dbgs() << "Handling AffineExpr\n" << mlirExpr << "\n");
         isl_local_space *ls = isl_local_space_from_space(isl_space_copy(space));
         LLVM_DEBUG(llvm::dbgs() << "Got aff\n");
-        isl_aff *aff = getIslAff(expr, ls, ctx, posMap);
-        isl_aff_dump(aff);
+        isl_aff *aff = m2i.getIslAff(mlirExpr, ls, ctx);
+        LLVM_DEBUG(isl_aff_dump(aff));
         ls = isl_local_space_free(ls);
-        LLVM_DEBUG(llvm::dbgs() << "Gisted aff\n");
         aff = isl_aff_gist(aff, isl_set_copy(domain));
-        isl_aff_dump(aff);
+        LLVM_DEBUG(llvm::dbgs() << "Gisted aff\n");
+        LLVM_DEBUG(isl_aff_dump(aff));
+        isl_ast_expr *expr = isl_ast_expr_from_aff(aff, build);
+        LLVM_DEBUG(llvm::dbgs() << "ast expr\n");
+        LLVM_DEBUG(isl_ast_expr_dump(expr));
+        LLVM_DEBUG(llvm::dbgs() << "Back to AffineExpr\n");
+        AffineExpr newMlirExpr = i2m.create(expr);
+        LLVM_DEBUG(llvm::dbgs() << newMlirExpr << "\n");
       }
       space = isl_space_free(space);
       domain = isl_set_free(domain);
+      build = isl_ast_build_free(build);
     });
   }
 };
