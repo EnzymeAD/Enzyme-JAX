fae09a8e83589d69723ebd4aa47a4a0d1095f9f1
William Moses <wmoses@google.com>
2025-03-24 15:12:57 -0500

    Lower cpu into exit (#522)
    * Lower cpu into exit
    
    * fix
    
    * fmt

--- src/enzyme_ad/jax/Passes/LowerJIT.cpp
+++ src/enzyme_ad/jax/Passes/LowerJIT.cpp
@@ -810,6 +810,15 @@ CompileCall(SymbolTableCollection &symbolTable, mlir::Location loc,
     id++;
     PassManager pm(submod.getContext());
     if (numGPUModule == 0) {
+      SmallVector<Operation *> toErase;
+      submod.walk([&](LLVM::InlineAsmOp asmop) {
+        if (asmop.getAsmString() == "exit;") {
+          toErase.push_back(asmop);
+        }
+      });
+      for (auto op : toErase) {
+        op->erase();
+      }
       pm.addPass(createLowerAffinePass());
       if (openmp)
         pm.addPass(createConvertSCFToOpenMPPass());
