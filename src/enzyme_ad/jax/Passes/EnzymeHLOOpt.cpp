649f020c490825836c3daa6ba10211d572b52211
William S. Moses <gh@wsmoses.com>
2025-03-29 22:28:07 -0500

    fmt

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -291,27 +291,27 @@ struct DynamicUpdateSliceElim final
   }
 };
 
-struct DUSDUS final
-    : OpRewritePattern<mlir::stablehlo::DynamicUpdateSliceOp> {
+struct DUSDUS final : OpRewritePattern<mlir::stablehlo::DynamicUpdateSliceOp> {
   using OpRewritePattern::OpRewritePattern;
 
   LogicalResult matchAndRewrite(mlir::stablehlo::DynamicUpdateSliceOp dus,
                                 PatternRewriter &rewriter) const override {
-    auto dus2 = dus.getOperand().getDefiningOp<stablehlo::DynamicUpdateSliceOp>();
-    
+    auto dus2 =
+        dus.getOperand().getDefiningOp<stablehlo::DynamicUpdateSliceOp>();
+
     if (!dus2)
       return failure();
 
     if (dus.getUpdate().getType() != dus2.getUpdate().getType())
       return failure();
 
-    for (auto &&[start1, start2] : llvm::zip_equal(dus.getStartIndices(), dus2.getStartIndices())) {
+    for (auto &&[start1, start2] :
+         llvm::zip_equal(dus.getStartIndices(), dus2.getStartIndices())) {
       if (start1 != start2)
         return failure();
     }
-    rewriter.modifyOpInPlace(dus, [&](){
-      dus.getOperandMutable().set(dus2.getOperand());
-    });
+    rewriter.modifyOpInPlace(
+        dus, [&]() { dus.getOperandMutable().set(dus2.getOperand()); });
     return success();
   }
 };
