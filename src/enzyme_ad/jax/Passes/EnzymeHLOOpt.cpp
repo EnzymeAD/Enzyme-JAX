131e99c48c4b3ad3b1627fd51fac64a98dc6ac72
William S. Moses <gh@wsmoses.com>
2025-04-05 18:18:16 -0500

    fix

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -12862,20 +12862,20 @@ template <typename ST> struct SumToConv : public OpRewritePattern<ST> {
     SmallVector<int64_t> permutation;
     if (T.getShape().size() > 2) {
       if (newOffsetDim != 0 && newOffsetDim != T.getShape().size() - 1) {
-        for (int i=0; i<T.getShape().size(); i++)
+        for (int i = 0; i < T.getShape().size(); i++)
           permutation.push_back(i);
         permutation[newOffsetDim] = 0;
         permutation[0] = newOffsetDim;
-        input = rewriter.create<stablehlo::TransposeOp>(
-            op.getLoc(), input, permutation);
+        input = rewriter.create<stablehlo::TransposeOp>(op.getLoc(), input,
+                                                        permutation);
         newOffsetDim = 0;
       }
       if (newOffsetDim == 0) {
         auto RT = cast<RankedTensorType>(input.getType());
         pre_reshape = RT;
         reshapeOffsetDim = newOffsetDim;
-        int64_t newDims[3] = { RT.getShape()[newOffsetDim], 1, 1 };
-        for (int i=1; i<RT.getShape().size(); i++) {
+        int64_t newDims[3] = {RT.getShape()[newOffsetDim], 1, 1};
+        for (int i = 1; i < RT.getShape().size(); i++) {
           newDims[1] *= RT.getShape()[i];
         }
         input = rewriter.create<stablehlo::ReshapeOp>(
@@ -12887,8 +12887,8 @@ template <typename ST> struct SumToConv : public OpRewritePattern<ST> {
         auto RT = cast<RankedTensorType>(input.getType());
         pre_reshape = RT;
         reshapeOffsetDim = newOffsetDim;
-        int64_t newDims[3] = { 1, 1, RT.getShape()[newOffsetDim] };
-        for (int i=0; i<RT.getShape().size()-1; i++) {
+        int64_t newDims[3] = {1, 1, RT.getShape()[newOffsetDim]};
+        for (int i = 0; i < RT.getShape().size() - 1; i++) {
           newDims[0] *= RT.getShape()[i];
         }
         input = rewriter.create<stablehlo::ReshapeOp>(
@@ -12971,12 +12971,14 @@ template <typename ST> struct SumToConv : public OpRewritePattern<ST> {
     if (conv.getType() != pre_reshape) {
       SmallVector<int64_t> post_shape = llvm::to_vector(pre_reshape.getShape());
       post_shape[reshapeOffsetDim] -= (lastidx - startidx);
-      RankedTensorType post_reshape = RankedTensorType::get(post_shape, pre_reshape.getElementType());
-      conv = rewriter.create<stablehlo::ReshapeOp>(op.getLoc(), post_reshape, conv);
+      RankedTensorType post_reshape =
+          RankedTensorType::get(post_shape, pre_reshape.getElementType());
+      conv = rewriter.create<stablehlo::ReshapeOp>(op.getLoc(), post_reshape,
+                                                   conv);
     }
     if (permutation.size())
-      conv = rewriter.create<stablehlo::TransposeOp>(
-            op.getLoc(), conv, permutation);
+      conv = rewriter.create<stablehlo::TransposeOp>(op.getLoc(), conv,
+                                                     permutation);
 
     rewriter.replaceOp(op, ValueRange{conv});
     return success();
