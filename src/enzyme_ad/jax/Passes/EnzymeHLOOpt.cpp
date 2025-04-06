24423e1bbd0e6eb9afea514ed31c9493571d9d2a
William S. Moses <gh@wsmoses.com>
2025-04-06 16:50:24 -0500

    Single use transpose reduce

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -5511,6 +5511,9 @@ struct TransposeReduce : public OpRewritePattern<mlir::stablehlo::TransposeOp> {
     if (!reduce)
       return failure();
 
+    if (!llvm::hasSingleElement(reduce.getUsers()))
+      return failure();
+
     unsigned resultNum = std::distance(
         reduce.getResults().begin(), llvm::find(reduce.getResults(), operand));
 
@@ -5555,6 +5558,7 @@ struct TransposeReduce : public OpRewritePattern<mlir::stablehlo::TransposeOp> {
         rewriter.replaceAllUsesWith(oldRes, newRes);
       }
     }
+    rewriter.eraseOp(reduce);
 
     return success();
   }
