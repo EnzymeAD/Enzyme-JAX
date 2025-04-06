e2dd52fb6c5f1fb1fd6bba0271e3f7be07017a31
William S. Moses <gh@wsmoses.com>
2025-04-05 20:07:34 -0500

    Add while licm parameter

--- src/enzyme_ad/jax/TransformOps/GenerateApplyPatterns.cpp
+++ src/enzyme_ad/jax/TransformOps/GenerateApplyPatterns.cpp
@@ -144,10 +144,11 @@ LogicalResult parseTransform(OpBuilder &builder, Location loc,
       state.addAttribute("benefit", builder.getI64IntegerAttr(benefit));
     if (parameter != -1) {
       if (opName == "no_nan_add_sub_simplify" || opName == "while_simplify" ||
-          opName == "slice_licm" || opName == "dus_licm" ||
-          opName == "pad_licm" || opName == "elementwise_licm" ||
-          opName == "concatenate_licm" || opName == "broadcastindim_licm" ||
-          opName == "reshape_licm" || opName == "transpose_licm")
+          opName == "while_licm" || opName == "slice_licm" ||
+          opName == "dus_licm" || opName == "pad_licm" ||
+          opName == "elementwise_licm" || opName == "concatenate_licm" ||
+          opName == "broadcastindim_licm" || opName == "reshape_licm" ||
+          opName == "transpose_licm")
         state.addAttribute("parameter", builder.getBoolAttr(parameter));
       else
         state.addAttribute("parameter", builder.getI64IntegerAttr(parameter));
