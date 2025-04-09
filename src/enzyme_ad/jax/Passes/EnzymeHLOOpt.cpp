4725e0c1ceaa69b8610ad1040c80db5e2e1469f0
jumerckx <31353884+jumerckx@users.noreply.github.com>
2025-04-09 23:11:41 +0200

    formatting

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -8374,7 +8374,8 @@ struct DUSSliceSimplify final
         });
 
     LLVM_DEBUG(
-        for (auto [idx, operandSize, updateSize] : llvm::zip_equal(
+        for (auto [idx, operandSize, updateSize]
+             : llvm::zip_equal(
                  newDusIndices,
                  preSliceOperand.getType().cast<RankedTensorType>().getShape(),
                  preSliceUpdate.getType()
