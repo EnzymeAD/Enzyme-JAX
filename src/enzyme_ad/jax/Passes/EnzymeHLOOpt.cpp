aad56055e3123b088f6dc0948b4c94950154afbc
William S. Moses <gh@wsmoses.com>
2025-04-13 17:30:24 -0500

    fix

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -1464,7 +1464,7 @@ concat_to_dus_slice_common(PatternRewriter &rewriter, Location loc,
   if (!shard) {
     if (auto opSharding = mlir::sdy::getSharding(operand)) {
       TensorShardingAttr opShardings[] = {opSharding};
-      shard = TensorShardingPerValueAttr::get(extend.getContext(), opShardings);
+      shard = sdy::TensorShardingPerValueAttr::get(extend.getContext(), opShardings);
     }
   }
 
