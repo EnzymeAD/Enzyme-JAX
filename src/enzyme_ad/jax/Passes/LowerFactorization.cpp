455ea7d4b335b7f269d0391703e7d758dc2b2bac
Avik Pal <avikpal@mit.edu>
2025-04-30 23:48:00 -0500

    feat: tpu lowering

--- src/enzyme_ad/jax/Passes/LowerFactorization.cpp
+++ src/enzyme_ad/jax/Passes/LowerFactorization.cpp
@@ -25,6 +25,16 @@ namespace enzyme {
 using namespace mlir;
 using namespace mlir::enzyme;
 
+template <typename T> Attribute makeAttr(mlir::Type elemType, T val) {
+  if (auto TT = dyn_cast<RankedTensorType>(elemType))
+    return SplatElementsAttr::get(
+        TT, ArrayRef(makeAttr<T>(TT.getElementType(), val)));
+  if (isa<FloatType>(elemType))
+    return FloatAttr::get(elemType, val);
+  else
+    return IntegerAttr::get(elemType, val);
+}
+
 struct LUFactorizationOpLowering
     : public OpRewritePattern<enzymexla::LUFactorizationOp> {
 
@@ -35,6 +45,43 @@ struct LUFactorizationOpLowering
 
   LogicalResult matchAndRewrite(enzymexla::LUFactorizationOp op,
                                 PatternRewriter &rewriter) const override {
+    auto input = op.getOperand();
+    auto inputShape = cast<RankedTensorType>(input.getType()).getShape();
+    auto inputRank = inputShape.size();
+    auto inputElementType =
+        cast<RankedTensorType>(input.getType()).getElementType();
+
+    const int64_t m = inputShape[inputRank - 2];
+    const int64_t n = inputShape[inputRank - 1];
+    const int64_t numBatchDims = inputRank - 2;
+    auto inputType = input.getType();
+
+    auto indexType =
+        cast<RankedTensorType>(op.getResult(1).getType()).getElementType();
+
+    auto infoType = RankedTensorType::get({}, indexType);
+
+    SmallVector<int64_t> pivotShape;
+    for (int i = 0; i < numBatchDims; i++) {
+      pivotShape.push_back(inputShape[i]);
+    }
+    pivotShape.push_back(std::min(m, n));
+    auto pivotType = RankedTensorType::get(pivotShape, indexType);
+
+    SmallVector<int64_t> permutationShape;
+    for (int i = 0; i < numBatchDims; i++) {
+      permutationShape.push_back(inputShape[i]);
+    }
+    permutationShape.push_back(m);
+    auto permutationType = RankedTensorType::get(permutationShape, indexType);
+
+    if (numBatchDims > 0 && (backend == "cuda" || backend == "cpu")) {
+      // TODO: Implement batched LU factorizations???
+      return rewriter.notifyMatchFailure(
+          op,
+          "Batched LU factorizations not yet implemented for " + backend + ".");
+    }
+
     if (backend == "cpu") {
       return rewriter.notifyMatchFailure(
           op, "CPU backend lowering not yet implemented.");
@@ -42,8 +89,70 @@ struct LUFactorizationOpLowering
       return rewriter.notifyMatchFailure(
           op, "CUDA backend lowering not yet implemented.");
     } else if (backend == "tpu") {
-      return rewriter.notifyMatchFailure(
-          op, "TPU backend lowering not yet implemented.");
+      // TPU returns (LU, pivots, permutation). info isn't returned. based on
+      // how JAX operates, I am assuming info = 0 when there is a nan in the
+      // output.
+      auto customCall = rewriter.create<stablehlo::CustomCallOp>(
+          op.getLoc(), TypeRange{inputType, pivotType, permutationType},
+          ValueRange{input}, rewriter.getStringAttr("LUFactorization"),
+          /*has_side_effect*/ nullptr,
+          /*backend_config*/ nullptr,
+          /*api_version*/ nullptr,
+          /*calledcomputations*/ nullptr,
+          /*operand_layouts*/ nullptr,
+          /*result_layouts*/ nullptr,
+          /*output_operand_aliases*/ nullptr);
+
+      // LAPACK returns 1-indexed pivots, while XLA returns 0-indexed pivots. We
+      // make it consistent with LAPACK by adding 1 to the pivots.
+      auto pivots1Indexed = rewriter.create<stablehlo::AddOp>(
+          op.getLoc(),
+          rewriter.create<stablehlo::ConstantOp>(
+              op.getLoc(), pivotType,
+              cast<ElementsAttr>(makeAttr(pivotType, 1))),
+          customCall.getResult(1));
+
+      auto isFinite = rewriter.create<stablehlo::IsFiniteOp>(
+          op.getLoc(), customCall.getResult(0));
+
+      SmallVector<int64_t> reductionDims;
+      for (int i = 0; i < inputRank; i++) {
+        reductionDims.push_back(i);
+      }
+      auto initValType = RankedTensorType::get({}, rewriter.getI1Type());
+      auto initVal = rewriter.create<stablehlo::ConstantOp>(
+          op.getLoc(), initValType,
+          cast<ElementsAttr>(makeAttr(initValType, 1)));
+
+      auto allFinite = rewriter.create<stablehlo::ReduceOp>(
+          op.getLoc(), initValType, ValueRange{isFinite.getResult()},
+          ValueRange{initVal}, rewriter.getDenseI64ArrayAttr(reductionDims));
+
+      {
+        OpBuilder::InsertionGuard guard(rewriter);
+        auto &region = allFinite.getBody();
+        auto *block = rewriter.createBlock(
+            &region, {}, {initValType, initValType},
+            {op.getLoc(), op.getLoc()});
+
+        rewriter.setInsertionPointToStart(block);
+        auto lhs = block->getArgument(0);
+        auto rhs = block->getArgument(1);
+        auto andOp = rewriter.create<stablehlo::AndOp>(op.getLoc(), lhs, rhs);
+
+        rewriter.create<stablehlo::ReturnOp>(op.getLoc(),
+                                             ValueRange{andOp.getResult()});
+      }
+
+      auto info = rewriter.create<stablehlo::ConvertOp>(
+          op.getLoc(), infoType, allFinite.getResult(0));
+
+      rewriter.replaceAllUsesWith(op.getResult(0), customCall.getResult(0));
+      rewriter.replaceAllUsesWith(op.getResult(1), pivots1Indexed);
+      rewriter.replaceAllUsesWith(op.getResult(2), info);
+      rewriter.eraseOp(op);
+
+      return success();
     } else {
       return rewriter.notifyMatchFailure(op, "Unknown backend " + backend);
     }
