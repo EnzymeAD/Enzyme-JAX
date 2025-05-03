ee3aa78038a7142449119ccfec98fb13c68f4e74
William S. Moses <gh@wsmoses.com>
2025-05-03 12:34:15 -0500

    fix inbounds

--- src/enzyme_ad/jax/Passes/LowerJIT.cpp
+++ src/enzyme_ad/jax/Passes/LowerJIT.cpp
@@ -732,7 +732,7 @@ CompileCall(SymbolTableCollection &symbolTable, mlir::Location loc,
   for (auto arg : op.getArguments()) {
     LLVM::GEPArg args[1] = {arg.getArgNumber()};
     auto gep = builder.create<LLVM::GEPOp>(loc, ptrty, ptrty, buffers, args,
-                                           mlir::LLVM::GEPNoWrapFlags(true));
+                                           mlir::LLVM::GEPNoWrapFlags(mlir::LLVM::GEPNoWrapFlags::inbounds));
     auto argTy = arg.getType();
     if (auto AT = dyn_cast<LLVM::LLVMArrayType>(argTy)) {
       argTy = AT.getElementType();
