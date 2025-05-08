d692e703747ddcf9eea866ef038e516db78113ed
William S. Moses <gh@wsmoses.com>
2025-05-08 00:12:50 -0500

    Fix compilation errors

--- src/enzyme_ad/jax/Passes/LLVMToAffineAccess.cpp
+++ src/enzyme_ad/jax/Passes/LLVMToAffineAccess.cpp
@@ -1449,7 +1449,6 @@ convertLLVMToAffineAccess(Operation *op,
   IndexConverter ic;
 
   SmallVector<std::unique_ptr<AffineAccessBuilder>> accessBuilders;
-  bool failed = false;
   auto handleOp = [&](Operation *op, PtrVal addr) {
     LLVM_DEBUG(llvm::dbgs() << "Building affine access for " << op
                             << " for address " << addr << "\n");
@@ -1457,10 +1456,7 @@ convertLLVMToAffineAccess(Operation *op,
         std::make_unique<AffineAccessBuilder>(op, legalizeSymbols));
     AffineAccessBuilder &aab = *accessBuilders.back();
     auto dl = dataLayoutAnalysis.getAtOrAbove(op);
-    auto res = aab.build(dl, addr);
-    if (!res.succeeded()) {
-      failed = true;
-    }
+    (void)aab.build(dl, addr);
   };
   op->walk([&](LLVM::StoreOp store) {
     PtrVal addr = store.getAddr();
@@ -1470,8 +1466,6 @@ convertLLVMToAffineAccess(Operation *op,
     PtrVal addr = load.getAddr();
     handleOp(load, addr);
   });
-  if (failed)
-    return failure();
   // TODO should also gather other mem operations such as memory intrinsics
   // TODO should we shrink the scope to where no other memory operations
   // exist?
