a6205205740604b7693e8ccc66009eeb3d44c7ff
William Moses <wmoses@google.com>
2025-05-08 01:36:33 -0500

    Fix sort to be a valid partial order (#923)
    * Fix sort to be a valid partial order
    
    * fmt

--- src/enzyme_ad/jax/Implementations/StableHLOAutoDiffOpInterfaceImpl.cpp
+++ src/enzyme_ad/jax/Implementations/StableHLOAutoDiffOpInterfaceImpl.cpp
@@ -80,6 +80,106 @@ static inline Operation *createAddRegion(Operation *op) {
   return op;
 }
 
+bool blockCmp(Block *a, Block *b);
+
+bool opCmp(Operation *a, Operation *b) {
+  if (a == b)
+    return false;
+
+  // Ancestors are less than their descendants.
+  if (a->isProperAncestor(b)) {
+    return true;
+  } else if (b->isProperAncestor(a->getParentOp())) {
+    return false;
+  }
+
+  // Move a and b to be direct descendents of the same op
+  while (!a->getParentOp()->isAncestor(b))
+    a = a->getParentOp();
+
+  while (!b->getParentOp()->isAncestor(a))
+    b = b->getParentOp();
+
+  assert(a->getParentOp() == b->getParentOp());
+
+  if (a->getBlock() == b->getBlock()) {
+    return a->isBeforeInBlock(b);
+  } else {
+    return blockCmp(a->getBlock(), b->getBlock());
+  }
+}
+
+bool regionCmp(Region *a, Region *b) {
+  if (a == b)
+    return false;
+
+  // Ancestors are less than their descendants.
+  if (a->getParentOp()->isProperAncestor(b->getParentOp())) {
+    return true;
+  } else if (b->getParentOp()->isProperAncestor(a->getParentOp())) {
+    return false;
+  }
+
+  if (a->getParentOp() == b->getParentOp()) {
+    return a->getRegionNumber() < b->getRegionNumber();
+  }
+  return opCmp(a->getParentOp(), b->getParentOp());
+}
+
+bool blockCmp(Block *a, Block *b) {
+  if (a == b)
+    return false;
+
+  // Ancestors are less than their descendants.
+  if (a->getParent()->isProperAncestor(b->getParent())) {
+    return true;
+  } else if (b->getParent()->isProperAncestor(a->getParent())) {
+    return false;
+  }
+
+  if (a->getParent() == b->getParent()) {
+    // If the blocks are in the same region, then the first one in
+    // the region is less than the second one.
+    for (auto &bb : *b->getParent()) {
+      if (&bb == a)
+        return true;
+    }
+    return false;
+  }
+
+  return regionCmp(a->getParent(), b->getParent());
+}
+
+// This function returns whether a < b
+bool valueCmp(Value a, Value b) {
+  // Equal values are not less than each other.
+  if (a == b)
+    return false;
+
+  auto ba = dyn_cast<BlockArgument>(a);
+  auto bb = dyn_cast<BlockArgument>(b);
+  // Define block arguments are less than non-block arguments.
+  if (ba && !bb)
+    return true;
+  if (!ba && bb)
+    return false;
+  if (ba && bb) {
+    if (ba.getOwner() == bb.getOwner()) {
+      return ba.getArgNumber() < bb.getArgNumber();
+    }
+    return blockCmp(ba.getOwner(), bb.getOwner());
+  }
+
+  OpResult ra = cast<OpResult>(a);
+  OpResult rb = cast<OpResult>(b);
+
+  if (ra.getOwner() == rb.getOwner()) {
+    return ra.getResultNumber() < rb.getResultNumber();
+  } else {
+    return opCmp(ra.getOwner(), rb.getOwner());
+  }
+}
+
 Operation *cloneWithNewResultTypes(Operation *op, OpBuilder &builder,
                                    IRMapping &mapper,
                                    TypeRange newResultTypes) {
@@ -2493,38 +2593,8 @@ private:
     // Refine cached values based on some heuristics
     auto newCacheVec = newCaches.takeVector();
 
-    {
-      // sort caches to provide determinism.
-      auto sorter = [](const Value &a, const Value &b) -> bool {
-        if (a == b)
-          return true;
-
-        auto ba = dyn_cast<BlockArgument>(a);
-        auto bb = dyn_cast<BlockArgument>(b);
-        if (ba && bb) {
-          if (ba.getOwner() == bb.getOwner())
-            return ba.getArgNumber() < bb.getArgNumber();
-
-          return ba.getOwner()->getParent()->isProperAncestor(
-              bb.getOwner()->getParent());
-        }
-
-        if (ba)
-          return true;
-
-        if (bb)
-          return false;
-
-        OpResult ra = cast<OpResult>(a);
-        OpResult rb = cast<OpResult>(b);
-
-        if (ra.getParentBlock() == rb.getParentBlock())
-          return ra.getDefiningOp()->isBeforeInBlock(rb.getDefiningOp());
-
-        return ra.getParentRegion()->isProperAncestor(rb.getParentRegion());
-      };
-      std::sort(newCacheVec.begin(), newCacheVec.end(), sorter);
-    };
+    // sort caches to provide determinism.
+    llvm::sort(newCacheVec.begin(), newCacheVec.end(), valueCmp);
 
     for (Value newCache : newCacheVec) {
       SetVector<Value> candidates;
