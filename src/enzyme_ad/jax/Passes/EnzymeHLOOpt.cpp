50e3cf0aed67fbeb58396d7f57299fdcb4ba9337
Avik Pal <avikpal@mit.edu>
2025-05-16 23:16:13 -0500

    chore: fmt

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -19492,8 +19492,7 @@ struct TriangularSolveRealAdjoint
       return rewriter.notifyMatchFailure(
           op, "`transpose_a` is not TRANSPOSE_ADJOINT");
 
-    if (isa<ComplexType>(
-            cast<RankedTensorType>(op.getType()).getElementType()))
+    if (isa<ComplexType>(cast<RankedTensorType>(op.getType()).getElementType()))
       return rewriter.notifyMatchFailure(op, "can't apply to complex numbers");
 
     rewriter.replaceOpWithNewOp<stablehlo::TriangularSolveOp>(
