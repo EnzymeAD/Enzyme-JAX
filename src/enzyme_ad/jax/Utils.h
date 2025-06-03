3eccead75cd0308b25287bc9b6dfd91feb9457ac
Avik Pal <avikpal@mit.edu>
2025-06-03 11:24:15 -0400

    feat: implement matchers for complex numbers + simplification patterns (#1012)
    * feat: implement matchers for complex numbers + simplification patterns
    
    * chore: run fmt
    
    * feat: move to no_nan_mul_simplify
    
    * test: update tests
    
    * feat: more simplify

--- src/enzyme_ad/jax/Utils.h
+++ src/enzyme_ad/jax/Utils.h
@@ -40,5 +40,84 @@ template <> inline Attribute makeAttr(mlir::Type elemType, llvm::APFloat val) {
   return FloatAttr::get(elemType, val);
 }
 
+// matcher for complex numbers. should probably be upstreamed at some point.
+// https://github.com/llvm/llvm-project/blob/be6fc0092e44c7fa3981639cbfe692c78a5eb418/mlir/include/mlir/IR/Matchers.h#L162
+struct constant_complex_value_binder {
+  FloatAttr::ValueType *bindRealValue;
+  FloatAttr::ValueType *bindImagValue;
+
+  constant_complex_value_binder(FloatAttr::ValueType *realValue,
+                                FloatAttr::ValueType *imagValue)
+      : bindRealValue(realValue), bindImagValue(imagValue) {}
+
+  bool match(Attribute attr) const {
+    if (auto splatAttr = dyn_cast<SplatElementsAttr>(attr)) {
+      return match(splatAttr.getSplatValue<Attribute>());
+    }
+
+    auto arrayAttr = dyn_cast<ArrayAttr>(attr);
+    if (!arrayAttr || arrayAttr.size() != 2)
+      return false;
+
+    auto realAttr = dyn_cast<FloatAttr>(arrayAttr.getValue()[0]);
+    auto imagAttr = dyn_cast<FloatAttr>(arrayAttr.getValue()[1]);
+
+    if (!realAttr || !imagAttr)
+      return false;
+
+    *bindRealValue = realAttr.getValue();
+    *bindImagValue = imagAttr.getValue();
+    return true;
+  }
+
+  bool match(Operation *op) const {
+    Attribute attr;
+    if (!mlir::detail::constant_op_binder<Attribute>(&attr).match(op))
+      return false;
+
+    Type type = op->getResult(0).getType();
+    if (isa<ComplexType, VectorType, RankedTensorType>(type))
+      return match(attr);
+
+    return false;
+  }
+};
+
+struct constant_complex_predicate_matcher {
+  bool (*predicate)(const APFloat &, const APFloat &);
+
+  bool match(Attribute attr) const {
+    APFloat realValue(APFloat::Bogus());
+    APFloat imagValue(APFloat::Bogus());
+    return constant_complex_value_binder(&realValue, &imagValue).match(attr) &&
+           predicate(realValue, imagValue);
+  }
+
+  bool match(Operation *op) const {
+    APFloat realValue(APFloat::Bogus());
+    APFloat imagValue(APFloat::Bogus());
+    return constant_complex_value_binder(&realValue, &imagValue).match(op) &&
+           predicate(realValue, imagValue);
+  }
+};
+
+inline constant_complex_predicate_matcher m_AnyZeroComplex() {
+  return {[](const APFloat &realValue, const APFloat &imagValue) {
+    return realValue.isZero() && imagValue.isZero();
+  }};
+}
+
+inline constant_complex_predicate_matcher m_AnyZeroRealComplex() {
+  return {[](const APFloat &realValue, const APFloat &imagValue) {
+    return realValue.isZero();
+  }};
+}
+
+inline constant_complex_predicate_matcher m_AnyZeroImagComplex() {
+  return {[](const APFloat &realValue, const APFloat &imagValue) {
+    return imagValue.isZero();
+  }};
+}
+
 } // namespace enzyme
 } // namespace mlir
