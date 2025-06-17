f10e0e2f45fe3f0b0b3b11d0b880726d99ab4955
William Moses <wmoses@google.com>
2025-06-17 09:49:42 -0700

    Fix while dataflow (#1054)
    * Fix while dataflow
    
    * fmt
    
    * Add test

--- src/enzyme_ad/jax/Implementations/StableHLOAutoDiffOpInterfaceImpl.cpp
+++ src/enzyme_ad/jax/Implementations/StableHLOAutoDiffOpInterfaceImpl.cpp
@@ -2284,9 +2284,13 @@ struct ADDataFlowWhileOp
     if (&srt.getCond() == term->getParentRegion())
       return {};
     SmallVector<Value> sv;
-    for (auto &&[res, arg] : llvm::zip(srt.getResults(), term->getOperands())) {
-      if (arg == v)
+    for (auto &&[res, arg, barg] :
+         llvm::zip(srt.getResults(), term->getOperands(),
+                   srt.getBody().front().getArguments())) {
+      if (arg == v) {
         sv.push_back(res);
+        sv.push_back(barg);
+      }
     }
     return sv;
   }
