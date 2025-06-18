c3cc7ed8a9ce729157a2eed58d9bc1d957b42023
William S. Moses <gh@wsmoses.com>
2025-06-17 19:03:41 -0500

    Fix infinite compile affinecfg

--- src/enzyme_ad/jax/Passes/Passes.h
+++ src/enzyme_ad/jax/Passes/Passes.h
@@ -50,7 +50,7 @@ void populateLLVMToControlFlowConversionPatterns(RewritePatternSet &patterns);
 
 void fully2ComposeAffineMapAndOperands(
     mlir::PatternRewriter &rewriter, mlir::AffineMap *map,
-    llvm::SmallVectorImpl<mlir::Value> *operands, mlir::DominanceInfo &DI);
+    llvm::SmallVectorImpl<mlir::Value> *operands, mlir::DominanceInfo &DI, llvm::SmallVectorImpl<mlir::Operation*>* insertedOps = nullptr);
 bool isValidIndex(mlir::Value val);
 
 struct ValueOrInt {
