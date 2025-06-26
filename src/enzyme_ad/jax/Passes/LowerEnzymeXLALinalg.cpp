da52519348a5ad6d5901d2d4ce72717b806423fe
William S. Moses <gh@wsmoses.com>
2025-06-26 12:55:59 -0400

    remove unused vars

--- src/enzyme_ad/jax/Passes/LowerEnzymeXLALinalg.cpp
+++ src/enzyme_ad/jax/Passes/LowerEnzymeXLALinalg.cpp
@@ -121,7 +121,6 @@ struct LUFactorizationOpLowering
     auto pivotType = cast<RankedTensorType>(op.getResult(1).getType());
     auto pivotRank = pivotType.getRank();
     auto permutationType = cast<RankedTensorType>(op.getResult(2).getType());
-    auto permutationRank = permutationType.getRank();
     auto infoType = cast<RankedTensorType>(op.getResult(3).getType());
     auto infoRank = infoType.getRank();
 
@@ -187,8 +186,8 @@ struct LUFactorizationOpLowering
             op.getLoc(), llvmBlasIntType,
             rewriter.getIntegerAttr(blasIntType, n));
 
-        auto mStore = rewriter.create<LLVM::StoreOp>(op.getLoc(), mVal, mPtr);
-        auto nStore = rewriter.create<LLVM::StoreOp>(op.getLoc(), nVal, nPtr);
+        rewriter.create<LLVM::StoreOp>(op.getLoc(), mVal, mPtr);
+        rewriter.create<LLVM::StoreOp>(op.getLoc(), nVal, nPtr);
 
         rewriter.create<LLVM::CallOp>(op.getLoc(), TypeRange{},
                                       SymbolRefAttr::get(ctx, lapackFn),
@@ -789,8 +788,6 @@ struct QRFactorizationOpLowering
     auto inputRank = static_cast<int64_t>(inputShape.size());
     auto inputElementType = inputType.getElementType();
 
-    const int64_t m = inputShape[inputRank - 2];
-    const int64_t n = inputShape[inputRank - 1];
     const int64_t numBatchDims = inputRank - 2;
 
     if (numBatchDims > 0) {
@@ -943,10 +940,6 @@ struct QRFactorizationOpLowering
     auto shape_input = type_input.getShape();
     auto rank_input = static_cast<int64_t>(shape_input.size());
 
-    const int64_t m = shape_input[rank_input - 2];
-    const int64_t n = shape_input[rank_input - 1];
-    const int64_t numBatchDims = rank_input - 2;
-
     auto type_tau = cast<RankedTensorType>(op.getResult(1).getType());
     auto rank_tau = type_tau.getRank();
 
@@ -1001,10 +994,6 @@ struct QRFactorizationOpLowering
     auto type_input = cast<RankedTensorType>(input.getType());
     auto shape_input = type_input.getShape();
     auto rank_input = static_cast<int64_t>(shape_input.size());
-
-    const int64_t m = shape_input[rank_input - 2];
-    const int64_t n = shape_input[rank_input - 1];
-    const int64_t numBatchDims = rank_input - 2;
 
     // emit `stablehlo.custom_call` to `@Qr` kernel from XLA
     auto type_tau = cast<RankedTensorType>(op.getResult(1).getType());
