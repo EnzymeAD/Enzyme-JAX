90eb0bbd1949388c795c37277ff40d8a988d5a96
William S. Moses <gh@wsmoses.com>
2025-06-27 01:35:57 -0500

    canonicalize before tollvm

--- src/enzyme_ad/jax/raise.cpp
+++ src/enzyme_ad/jax/raise.cpp
@@ -101,7 +101,7 @@ extern "C" std::string runLLVMToMLIRRoundTrip(std::string input) {
       } else {
 	pass_pipeline += ",parallel-serialization,";
       }
-      pass_pipeline += "convert-polygeist-to-llvm{backend=";
+      pass_pipeline += "canonicalize,convert-polygeist-to-llvm{backend=";
       pass_pipeline += backend;
       pass_pipeline += "},strip-"
       "gpu-info,gpu-"
