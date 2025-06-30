25feef7b0a962f5a0fe0451333bce1ac43d7c1be
William S. Moses <gh@wsmoses.com>
2025-06-30 17:16:56 -0500

    Nondom lift

--- src/enzyme_ad/jax/Passes/AffineCFG.cpp
+++ src/enzyme_ad/jax/Passes/AffineCFG.cpp
@@ -5137,6 +5137,17 @@ public:
         return rewriter.notifyMatchFailure(
             loadOp, "non-pure operation on the path (V2)");
       }
+       for (auto op : toLift) {
+      for (auto operand : op->getOperands()) {
+        if (auto ba = dyn_cast<BlockArgument>(operand)) {
+          if (!dominance.dominates(ba, conditional)) {
+            return rewriter.notifyMatchFailure(
+                loadOp,
+                "block argument requirement not part dominating conditional");
+          }
+        }
+      }
+    }
 
       SmallVector<std::unique_ptr<Region>> regions;
       regions.emplace_back(new Region);
@@ -5173,6 +5184,20 @@ public:
     } else {
       for (auto i = 0; i < conditional->getNumResults(); i++)
         resultsNeeded.insert(i);
+    }
+    
+    for (auto op : toLift) {
+      llvm::errs() << "op: " << *op << "\n";
+      for (auto operand : op->getOperands()) {
+        if (auto ba = dyn_cast<BlockArgument>(operand)) {
+          llvm::errs() << " ba: " << ba << "\n";
+          if (!dominance.dominates(ba, conditional)) {
+            return rewriter.notifyMatchFailure(
+                loadOp,
+                "block argument requirement not part dominating conditional");
+          }
+        }
+      }
     }
 
     auto cloneIntoBlock = [&](unsigned blockNum) {
