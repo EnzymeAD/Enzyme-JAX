55c525aff4986497e923a95d6110974052679c81
William Moses <wmoses@google.com>
2025-08-17 15:28:12 +0200

    Declare visibility without linking capi twice (#1314)

--- src/enzyme_ad/jax/gpu.cc
+++ src/enzyme_ad/jax/gpu.cc
@@ -1,7 +1,21 @@
 #include "xla/ffi/api/ffi.h"
 #include "xla/ffi/ffi_api.h"
 
-#include "mlir-c/Support.h"
+#if (defined(_WIN32) || defined(__CYGWIN__)) &&                                \
+    !defined(MLIR_CAPI_ENABLE_WINDOWS_DLL_DECLSPEC)
+// Visibility annotations disabled.
+#define MLIR_CAPI_EXPORTED
+#elif defined(_WIN32) || defined(__CYGWIN__)
+// Windows visibility declarations.
+#if MLIR_CAPI_BUILDING_LIBRARY
+#define MLIR_CAPI_EXPORTED __declspec(dllexport)
+#else
+#define MLIR_CAPI_EXPORTED __declspec(dllimport)
+#endif
+#else
+// Non-windows: use visibility attributes.
+#define MLIR_CAPI_EXPORTED __attribute__((visibility("default")))
+#endif
 
 template <bool withError> struct CallInfo;
 
