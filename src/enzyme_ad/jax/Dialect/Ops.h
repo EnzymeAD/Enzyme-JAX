edaef4cac440e24fc6609aaf7b4dc2e7819114a0
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-09-21 21:03:59 +0200

    Update JAX to commit da0053fca01db7192d1ba2baef660eba0fd13497 (#1400)
    * Update JAX to commit da0053fca01db7192d1ba2baef660eba0fd13497
    
    Diff: https://github.com/jax-ml/jax/compare/f989fdbf2cd1bb47a3c618b2a102d01b4386e81d...da0053fca01db7192d1ba2baef660eba0fd13497
    
    * fix
    
    * Update ENZYME_COMMIT to a new hash
    
    * Update Python rules import in BUILD file
    
    * Update JAX_COMMIT to new commit hash
    
    * move patches
    
    * fx?
    
    * rocm patch
    
    * fmt
    
    * fix
    
    * pystar
    
    * fmt
    
    ---------
    
    Co-authored-by: enzyme-ci-bot[bot] <78882869+enzyme-ci-bot[bot]@users.noreply.github.com>
    Co-authored-by: William S. Moses <gh@wsmoses.com>
    Co-authored-by: William Moses <wmoses@google.com>

--- src/enzyme_ad/jax/Dialect/Ops.h
+++ src/enzyme_ad/jax/Dialect/Ops.h
@@ -19,7 +19,6 @@
 #include "mlir/IR/SymbolTable.h"
 #include "mlir/Interfaces/CallInterfaces.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
-#include "mlir/Interfaces/CopyOpInterface.h"
 #include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/MemorySlotInterfaces.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
