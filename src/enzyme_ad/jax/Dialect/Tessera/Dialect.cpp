dca7483ddced1a60d02f7a8e16c177e94d43e645
jessicacotturone21 <jac18@illinois.edu>
2025-09-24 15:15:04 -0500

    Created Tessera dialect files

---
+++ src/enzyme_ad/jax/Dialect/Tessera/Dialect.cpp
@@ -0,0 +1,15 @@
+#include "Dialect.h"
+
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#include "src/enzyme_ad/jax/Dialect/Tessera/TesseraDialect.cpp.inc"
+
+
+// Initialize the dialect
+void mlir::enzyme::tessera::TesseraDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "src/enzyme_ad/jax/Dialect/Tessera/TesseraOps.cpp.inc"
+      >();
+}
---
+++ src/enzyme_ad/jax/Dialect/Tessera/Dialect.h
@@ -0,0 +1,20 @@
+#ifndef ENZYME_AD_JAX_DIALECT_TESSERA_DIALECT_H
+#define ENZYME_AD_JAX_DIALECT_TESSERA_DIALECT_H
+
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Region.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/IR/Types.h"
+
+// Include the dialect
+#include "src/enzyme_ad/jax/Dialect/Tessera/TesseraDialect.h.inc"
+
+// Operations
+#define GET_OP_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Tessera/TesseraOps.h.inc"
+
+#endif // ENZYME_AD_JAX_DIALECT_TESSERA_DIALECT_H
