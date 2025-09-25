6a8fec3671b4624b36985c6d61baed97a9effb5e
jessicacotturone21 <144174576+jessicacotturone21@users.noreply.github.com>
2025-09-25 16:14:41 -0500

    Tessera Dialect (#1422)
    * Created Tessera dialect files
    
    * Registered Tessera dialect with Enzyme and defined Bazel build targets
    
    * Added missing files
    
    * Fmt1
    
    * more format
    
    * fmt3

---
+++ src/enzyme_ad/jax/Dialect/Tessera/Dialect.h
@@ -0,0 +1,24 @@
+#ifndef ENZYME_AD_JAX_DIALECT_TESSERA_DIALECT_H
+#define ENZYME_AD_JAX_DIALECT_TESSERA_DIALECT_H
+
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Region.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/IR/Types.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include "mlir/Interfaces/FunctionInterfaces.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+// Include the dialect
+#include "src/enzyme_ad/jax/Dialect/Tessera/TesseraDialect.h.inc"
+
+// Operations
+#define GET_OP_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Tessera/TesseraOps.h.inc"
+
+#endif // ENZYME_AD_JAX_DIALECT_TESSERA_DIALECT_H
--- src/enzyme_ad/jax/RegistryUtils.cpp
+++ src/enzyme_ad/jax/RegistryUtils.cpp
@@ -89,6 +89,7 @@
 #include "src/enzyme_ad/jax/Passes/Passes.h"
 
 #include "src/enzyme_ad/jax/Dialect/Distributed/Dialect.h"
+#include "src/enzyme_ad/jax/Dialect/Tessera/Dialect.h"
 
 #include "shardy/dialect/sdy/ir/dialect.h"
 #include "shardy/dialect/sdy/ir/utils.h"
@@ -206,6 +207,7 @@ void registerDialects(mlir::DialectRegistry &registry) {
   registry.insert<mlir::enzyme::EnzymeDialect>();
   registry.insert<mlir::enzymexla::EnzymeXLADialect>();
   registry.insert<mlir::enzyme::distributed::DistributedDialect>();
+  registry.insert<mlir::enzyme::tessera::TesseraDialect>();
   registry.insert<mlir::sdy::SdyDialect>();
   registry.insert<mlir::ub::UBDialect>();
   registry.insert<mlir::triton::TritonDialect>();
