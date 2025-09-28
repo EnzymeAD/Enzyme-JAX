a871adee36388efc768ba9983d7814b203417225
Avik Pal <avikpal@mit.edu>
2025-09-27 21:14:14 -0400

    feat: update AutoBatching passes to be CheckedRewrite (#1434)

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -25,6 +25,7 @@
 #include "mlir/Pass/PassManager.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "shardy/dialect/sdy/ir/utils.h"
+#include "src/enzyme_ad/jax/CheckedRewrite.h"
 #include "src/enzyme_ad/jax/Dialect/Dialect.h"
 #include "src/enzyme_ad/jax/Dialect/Ops.h"
 #include "src/enzyme_ad/jax/Passes/EnzymeHLOPatterns.h"
@@ -304,86 +305,6 @@ public:
 
     return res;
   }
-};
-
-LogicalResult failIfDynamicShape(Operation *op, PatternRewriter &rewriter) {
-  for (auto type : op->getResultTypes()) {
-    auto rType = dyn_cast<RankedTensorType>(type);
-    if (!rType || !rType.hasStaticShape())
-      return rewriter.notifyMatchFailure(
-          op, "unsupported dynamic shape for output.");
-  }
-
-  for (auto type : op->getOperandTypes()) {
-    auto rType = dyn_cast<RankedTensorType>(type);
-    if (!rType || !rType.hasStaticShape())
-      return rewriter.notifyMatchFailure(
-          op, "unsupported dynamic shape for input.");
-  }
-
-  return success();
-}
-
-LogicalResult failIfFuncOpInterfaceHasAttr(Operation *op, StringRef attrName,
-                                           PatternRewriter &rewriter) {
-  if (auto func = op->getParentOfType<FunctionOpInterface>()) {
-    if (func->hasAttrOfType<UnitAttr>(attrName))
-      return rewriter.notifyMatchFailure(op, "disabled by attribute.");
-  }
-
-  return success();
-}
-
-static constexpr StringRef kDisablePatternAttrName =
-    "enzymexla.disable_hlo_opts";
-
-template <typename OpTy, typename Child>
-struct CheckedOpRewritePattern : public OpRewritePattern<OpTy> {
-  using Base = OpRewritePattern<OpTy>;
-  using Base::Base;
-
-  LogicalResult
-  matchAndRewrite(OpTy op, PatternRewriter &rewriter) const override final {
-    LogicalResult res =
-        failIfFuncOpInterfaceHasAttr(op, kDisablePatternAttrName, rewriter);
-    if (res.failed())
-      return res;
-
-    if (!((Child *)this)->supportsDynamicShapes()) {
-      LogicalResult res = failIfDynamicShape(op, rewriter);
-      if (res.failed())
-        return res;
-    }
-
-    return ((Child *)this)->matchAndRewriteImpl(op, rewriter);
-  }
-
-  bool supportsDynamicShapes() { return false; }
-};
-
-template <template <typename> class TraitType, typename Child>
-struct CheckedOpTraitRewritePattern : public OpTraitRewritePattern<TraitType> {
-  using Base = OpTraitRewritePattern<TraitType>;
-  using Base::Base;
-
-  LogicalResult
-  matchAndRewrite(Operation *op,
-                  PatternRewriter &rewriter) const override final {
-    LogicalResult res =
-        failIfFuncOpInterfaceHasAttr(op, kDisablePatternAttrName, rewriter);
-    if (res.failed())
-      return res;
-
-    if (!((Child *)this)->supportsDynamicShapes()) {
-      auto res = failIfDynamicShape(op, rewriter);
-      if (res.failed())
-        return res;
-    }
-
-    return ((Child *)this)->matchAndRewriteImpl(op, rewriter);
-  }
-
-  bool supportsDynamicShapes() { return false; }
 };
 
 template <typename OpTy, typename Child>
