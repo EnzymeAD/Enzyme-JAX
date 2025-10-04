2bb06597763cc11f17cd42ace86e76234ecd8a1a
Avik Pal <avikpal@mit.edu>
2025-10-03 22:59:52 -0500

    fix: symbolref

--- src/enzyme_ad/jax/Passes/LowerKernel.cpp
+++ src/enzyme_ad/jax/Passes/LowerKernel.cpp
@@ -47,6 +47,23 @@ using namespace enzymexla;
 
 using namespace stablehlo;
 
+SymbolRefAttr SymRefAttrReplacingFunctionName(SymbolRefAttr origSymRef,
+                                              StringRef newName) {
+  auto newNameRef = FlatSymbolRefAttr::get(origSymRef.getContext(), newName);
+  auto nestedRefsAttr = origSymRef.getNestedReferences();
+  if (nestedRefsAttr.size() == 0) {
+    return newNameRef;
+  }
+
+  auto rootRef = origSymRef.getRootReference();
+  SmallVector<FlatSymbolRefAttr> nestedRefs;
+  for (int i = 0; i < nestedRefsAttr.size() - 1; i++) {
+    nestedRefs.push_back(nestedRefsAttr[i]);
+  }
+  nestedRefs.push_back(newNameRef);
+  return SymbolRefAttr::get(origSymRef.getContext(), rootRef, nestedRefs);
+}
+
 bool CompileGPUKernel(SymbolTableCollection &symbolTable, mlir::Location loc,
                       FunctionOpInterface op, size_t gridx, size_t gridy,
                       size_t gridz, size_t blockx, size_t blocky, size_t blockz,
@@ -228,21 +245,13 @@ bool CompileGPUKernel(SymbolTableCollection &symbolTable, mlir::Location loc,
 
   OpBuilder rewriter(kcall);
 
-  auto rootRef = kcall.getFn().getRootReference();
-  SmallVector<FlatSymbolRefAttr> nestedRefs;
-  auto nestedRefsAttr = kcall.getFn().getNestedReferences();
-  for (int i = 0; i < nestedRefsAttr.size() - 1; i++) {
-    nestedRefs.push_back(nestedRefsAttr[i]);
-  }
-  nestedRefs.push_back(FlatSymbolRefAttr::get(kcall.getContext(), callName));
-  auto symRef = SymbolRefAttr::get(kcall.getContext(), rootRef, nestedRefs);
-
   auto replacement = rewriter.create<enzymexla::JITCallOp>(
-      kcall.getLoc(), kcall.getResultTypes(), symRef, kcall.getInputs(),
-      kcall.getBackendConfigAttr(), kcall.getOperandLayoutsAttr(),
-      kcall.getResultLayoutsAttr(), kcall.getArgAttrsAttr(),
-      kcall.getResAttrsAttr(), kcall.getOutputOperandAliasesAttr(),
-      kcall.getXlaSideEffectFreeAttr());
+      kcall.getLoc(), kcall.getResultTypes(),
+      SymRefAttrReplacingFunctionName(kcall.getFn(), callName),
+      kcall.getInputs(), kcall.getBackendConfigAttr(),
+      kcall.getOperandLayoutsAttr(), kcall.getResultLayoutsAttr(),
+      kcall.getArgAttrsAttr(), kcall.getResAttrsAttr(),
+      kcall.getOutputOperandAliasesAttr(), kcall.getXlaSideEffectFreeAttr());
   kcall.replaceAllUsesWith(replacement);
   kcall.erase();
   return true;
@@ -404,22 +413,14 @@ bool CompileCPUKernel(SymbolTableCollection &symbolTable, mlir::Location loc,
     idxOp.erase();
   });
 
-  auto rootRef = kcall.getFn().getRootReference();
-  SmallVector<FlatSymbolRefAttr> nestedRefs;
-  auto nestedRefsAttr = kcall.getFn().getNestedReferences();
-  for (int i = 0; i < nestedRefsAttr.size() - 1; i++) {
-    nestedRefs.push_back(nestedRefsAttr[i]);
-  }
-  nestedRefs.push_back(FlatSymbolRefAttr::get(kcall.getContext(), callName));
-  auto symRef = SymbolRefAttr::get(kcall.getContext(), rootRef, nestedRefs);
-
   OpBuilder rewriter(kcall);
   auto replacement = rewriter.create<enzymexla::JITCallOp>(
-      kcall.getLoc(), kcall.getResultTypes(), symRef, kcall.getInputs(),
-      kcall.getBackendConfigAttr(), kcall.getOperandLayoutsAttr(),
-      kcall.getResultLayoutsAttr(), kcall.getArgAttrsAttr(),
-      kcall.getResAttrsAttr(), kcall.getOutputOperandAliasesAttr(),
-      kcall.getXlaSideEffectFreeAttr());
+      kcall.getLoc(), kcall.getResultTypes(),
+      SymRefAttrReplacingFunctionName(kcall.getFn(), callName),
+      kcall.getInputs(), kcall.getBackendConfigAttr(),
+      kcall.getOperandLayoutsAttr(), kcall.getResultLayoutsAttr(),
+      kcall.getArgAttrsAttr(), kcall.getResAttrsAttr(),
+      kcall.getOutputOperandAliasesAttr(), kcall.getXlaSideEffectFreeAttr());
   kcall.replaceAllUsesWith(replacement);
   kcall.erase();
   return true;
