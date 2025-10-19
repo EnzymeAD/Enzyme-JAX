832961bf8ab7d816fab35651d8210b9df4633413
William S. Moses <gh@wsmoses.com>
2025-10-19 03:02:34 -0400

    fmt

--- src/enzyme_ad/jax/gpu.cc
+++ src/enzyme_ad/jax/gpu.cc
@@ -82,8 +82,8 @@ XLA_FFI_Error *initialize(XLA_FFI_CallFrame *call_frame) {
 
   auto *execution_state = reinterpret_cast<xla::ffi::ExecutionState *>(
       internal_api->XLA_FFI_INTERNAL_ExecutionState_Get(ctx));
-  (void)execution_state->Set(
-      xla::ffi::TypeRegistry::GetTypeId<CuFuncWrapper>(), cufunc, noop);
+  (void)execution_state->Set(xla::ffi::TypeRegistry::GetTypeId<CuFuncWrapper>(),
+                             cufunc, noop);
 
   return nullptr;
 }
