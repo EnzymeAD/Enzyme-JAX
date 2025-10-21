fab19e328e6e1e5e3e6824168766f013c385e1a4
Jacob Peng <jacobmpeng@gmail.com>
2025-10-20 21:53:14 -0500

    removal interface for enzymexla.gpu_wrapper

--- src/enzyme_ad/jax/Implementations/XLADerivatives.h
+++ src/enzyme_ad/jax/Implementations/XLADerivatives.h
@@ -10,7 +10,6 @@
 
 namespace mlir {
 namespace enzyme {
-void registerEnzymeXLADialectAutoDiffInterface(mlir::DialectRegistry &registry);
 void registerMHLODialectAutoDiffInterface(mlir::DialectRegistry &registry);
 void registerStableHLODialectAutoDiffInterface(mlir::DialectRegistry &registry);
 void registerCHLODialectAutoDiffInterface(mlir::DialectRegistry &registry);
@@ -20,7 +19,6 @@ void registerTritonExtDialectAutoDiffInterface(mlir::DialectRegistry &registry);
 
 static inline void
 registerXLAAutoDiffInterfaces(mlir::DialectRegistry &registry) {
-  registerEnzymeXLADialectAutoDiffInterface(registry);
   registerMHLODialectAutoDiffInterface(registry);
   registerStableHLODialectAutoDiffInterface(registry);
   registerCHLODialectAutoDiffInterface(registry);
