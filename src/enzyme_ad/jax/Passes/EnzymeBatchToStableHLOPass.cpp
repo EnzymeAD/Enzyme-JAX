5db2b113cca4d4c95d66f04e01d3de96168442bc
Vimarsh Sathia <vsathia2@illinois.edu>
2025-11-02 17:52:18 -0600

    Add ConcatOp lowering, fix lit test Need to find another cafe to work...

--- src/enzyme_ad/jax/Passes/EnzymeBatchToStableHLOPass.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeBatchToStableHLOPass.cpp
@@ -9,11 +9,11 @@
 // This file implements a pass to print the MLIR module
 //===----------------------------------------------------------------------===//
 
-#include "src/enzyme_ad/jax/Passes/Passes.h"
-#include "stablehlo/dialect/StablehloOps.h"
 #include "Enzyme/MLIR/Dialect/Dialect.h"
 #include "Enzyme/MLIR/Dialect/Ops.h"
+#include "src/enzyme_ad/jax/Passes/Passes.h"
 #include "src/enzyme_ad/jax/Utils.h"
+#include "stablehlo/dialect/StablehloOps.h"
 
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Transforms/DialectConversion.h"
@@ -29,16 +29,67 @@ using namespace mlir;
 using namespace mlir::enzyme;
 using namespace enzyme;
 namespace {
+
+struct ExtractOpConversion : public OpConversionPattern<enzyme::ExtractOp> {
+  using OpConversionPattern<enzyme::ExtractOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(enzyme::ExtractOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto outTy = op.getOutput().getType();
+    // stablehlo always has tensor type
+    auto outRankTy = dyn_cast<RankedTensorType>(outTy);
+    auto rank = outRankTy.getRank();
+    return failure(); 
+    // stablehlo.dynamic_slice op
+  }
+};
+
+struct ConcatOpConversion : public OpConversionPattern<enzyme::ConcatOp> {
+  using OpConversionPattern<enzyme::ConcatOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(enzyme::ConcatOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    SmallVector<Value> inputs = op.getInputs();
+    if (inputs.empty())
+      return failure();
+
+    auto firstInTy = inputs.front().getType();
+
+    // stablehlo always has tensor type
+    // reshape each input to 1xinput_rank and concatenate on dim=0
+
+    SmallVector<Value> expandedInputs;
+    for (Value in : inputs) {
+      auto inRankTy = cast<RankedTensorType>(in.getType());
+      auto inShape = inRankTy.getShape();
+      SmallVector<int64_t> newInShape = {1};
+      newInShape.append(inShape.begin(), inShape.end());
+      auto newInTy = inRankTy.clone(newInShape);
+      Value newInput = rewriter.create<stablehlo::ReshapeOp>(
+          op->getLoc(), newInTy, in, op->getAttrs());
+      expandedInputs.push_back(newInput);
+    }
+
+    // concatenate on dim=0
+    rewriter.replaceOpWithNewOp<stablehlo::ConcatenateOp>(
+        op, op->getResultTypes(), expandedInputs, /*dim=*/0);
+    return success();
+  }
+};
 struct EnzymeBatchToStableHLOPass
     : public enzyme::impl::EnzymeBatchToStableHLOPassBase<
           EnzymeBatchToStableHLOPass> {
   void runOnOperation() override {
     MLIRContext *context = &getContext();
     RewritePatternSet patterns(context);
+    patterns.add<ConcatOpConversion, ExtractOpConversion>(context);
+
     ConversionTarget target(*context);
     target.addLegalDialect<stablehlo::StablehloDialect>();
     target.addLegalDialect<enzyme::EnzymeDialect>();
-    target.addIllegalOp<enzyme::ConcatOp, enzyme::ExtractOp>();
+    target.addIllegalOp<enzyme::ConcatOp>();
 
     if (failed(applyPartialConversion(getOperation(), target,
                                       std::move(patterns)))) {
