0d87cd5e346e212841bb7c79138a9abcb61dac0f
Avik Pal <avikpal@mit.edu>
2025-11-02 20:36:13 -0500

    fix: guaranteed analysis bfs queue was busted (#1538)
    * fix: guaranteed analysis bfs queue was busted
    
    * chore: fmt
    
    * fix: try making things deterministic
    
    * chore: bazel format
    
    * fix: address review comments
    
    * fix: revert to SmallPtrSet
    
    * chore: run fmt
    
    * fix: reuse found value

--- src/enzyme_ad/jax/Utils.h
+++ src/enzyme_ad/jax/Utils.h
@@ -7,6 +7,8 @@
 #include "mlir/IR/Types.h"
 #include "llvm/ADT/APFloat.h"
 #include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/MapVector.h"
+#include "llvm/ADT/SetVector.h"
 #include "llvm/ADT/SmallVector.h"
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
@@ -354,7 +356,7 @@ public:
 
     // Map of operations we have seen before. The target of the map[o] is a list
     // of sub-queries, that if all true prove that `o` is no-nan.
-    DenseMap<Operation *, SmallPtrSet<Operation *, 2>> seen;
+    llvm::MapVector<Operation *, llvm::SmallPtrSet<Operation *, 2>> seen;
 
     // Inverse of seen. A map of operations `p` we still need to prove, to a
     // list of values that require `p` to be proven.
@@ -454,10 +456,12 @@ public:
       case State::PENDING: {
         assert(localtodo.size());
         assert(seen.find(cur) == seen.end());
-        SmallPtrSet<Operation *, 2> set(localtodo.begin(), localtodo.end());
-        for (auto v : set) {
+        for (auto v : localtodo) {
           reverseSeen[v].push_back(cur);
+          todo.push_back(v);
         }
+        llvm::SmallPtrSet<Operation *, 2> set(localtodo.begin(),
+                                              localtodo.end());
         seen[cur] = std::move(set);
         break;
       }
@@ -475,11 +479,15 @@ public:
       });
     }
 
-    assert(opCache.find(op) != opCache.end());
-    rewriter.modifyOpInPlace(op, [&]() {
-      op->setAttr(attrName, BoolAttr::get(op->getContext(), true));
-    });
-    return true;
+    auto found = opCache.find(op);
+    if (found != opCache.end()) {
+      bool guaranteed = found->second;
+      rewriter.modifyOpInPlace(op, [&]() {
+        op->setAttr(attrName, BoolAttr::get(op->getContext(), guaranteed));
+      });
+      return guaranteed;
+    }
+    return false;
   }
 
   bool guaranteed(stablehlo::ConstantOp constOp, PatternRewriter &rewriter) {
