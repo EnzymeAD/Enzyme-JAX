b0906477b87cb240f13fd925bc53a31142262d77
Avik Pal <avikpal@mit.edu>
2025-11-18 16:51:05 -0600

    feat: value properties check

--- src/enzyme_ad/jax/Analysis/StructuredMatrixAnalysis.cpp
+++ src/enzyme_ad/jax/Analysis/StructuredMatrixAnalysis.cpp
@@ -6,6 +6,8 @@
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 
 #include "mlir/Support/LLVM.h"
@@ -22,8 +24,16 @@ namespace structure_analysis {
 //===----------------------------------------------------------------------===//
 
 StructuredSparsityPattern::StructuredSparsityPattern(Value v) {
+  if (auto blockArg = dyn_cast<BlockArgument>(v)) {
+    // TODO: If block arg is annotated with a pattern we should parse that
+    setUnknown(); // be pessimistic by default
+    return;
+  }
+
   llvm::errs() << "TODO: structured sparsity pattern not implemented for " << v
                << "\n";
+  setUnknown();
+  return;
 }
 
 void StructuredSparsityPattern::initializeBandwidths() {
@@ -188,7 +198,126 @@ void StructuredSparsityPattern::print(raw_ostream &os) const {
 //===----------------------------------------------------------------------===//
 
 ValueProperties::ValueProperties(Value v) {
-  llvm::errs() << "TODO: value properties not implemented for " << v << "\n";
+  if (auto blockArg = dyn_cast<BlockArgument>(v)) {
+    // TODO: If block arg is annotated with a pattern we should parse that
+    setFlags(0); // be pessimistic by default
+    return;
+  }
+
+  auto vTy = cast<RankedTensorType>(v.getType());
+  if (!vTy.hasStaticShape() || vTy.getRank() != 2)
+    return;
+  auto vShape = vTy.getShape();
+  if (vShape[0] != vShape[1]) // TODO: should we allow rectangular matrices?
+    return;
+
+  DenseElementsAttr denseAttr;
+  if (matchPattern(v, m_Constant(&denseAttr))) {
+    auto props = getPropertiesFromDenseAttr(denseAttr);
+    setFlags(props.getFlags());
+    llvm::errs() << "v: " << v << " properties: ";
+    this->print(llvm::errs());
+    llvm::errs() << "\n";
+    return;
+  }
+
+  // TODO: symmetric checks Utils.cpp:688
+
+  // TODO: broadcasted scalar
+
+  // TODO: unit diagonal
+  //       - iota scatter with constant
+
+  llvm::errs() << "(Not implemented) v: " << v << " properties: ";
+  this->print(llvm::errs());
+  llvm::errs() << "\n";
+  return;
+}
+
+ValueProperties
+ValueProperties::getPropertiesFromDenseAttr(DenseElementsAttr attr) {
+  ValueProperties props;
+
+  if (attr.isSplat()) {
+    auto val = attr.getSplatValue<Attribute>();
+    if (utils::isOne(val))
+      props.set(ValueProperty::UnitDiagonal);
+
+    props.set(ValueProperty::BroadcastedScalar);
+    props.set(ValueProperty::Symmetric);
+    props.set(ValueProperty::Hermitian);
+    return props;
+  }
+
+  auto type = dyn_cast<RankedTensorType>(attr.getType());
+  if (!type)
+    return props;
+
+  auto shape = type.getShape();
+  int64_t nrows = shape[0];
+  int64_t ncols = shape[1];
+  if (nrows != ncols)
+    return props;
+
+  if (isUnitDiagonal(attr, nrows, ncols))
+    props.set(ValueProperty::UnitDiagonal);
+
+  auto [isSymmetric, isHermitian] = isSymmetricOrHermitian(attr, nrows, ncols);
+  if (isSymmetric)
+    props.set(ValueProperty::Symmetric);
+  if (isHermitian)
+    props.set(ValueProperty::Hermitian);
+
+  return props;
+}
+
+template <typename T>
+bool isUnitDiagonalImpl(DenseElementsAttr attr, int64_t nrows, int64_t ncols) {
+  auto values = attr.getValues<T>().begin();
+  for (int64_t i = 0; i < std::min(nrows, ncols); i++) {
+    if (!utils::isOne(values[i]))
+      return false;
+  }
+  return true;
+}
+
+bool ValueProperties::isUnitDiagonal(DenseElementsAttr attr, int64_t nrows,
+                                     int64_t ncols) {
+  if (isa<IntegerType>(attr.getElementType())) {
+    return isUnitDiagonalImpl<APInt>(attr, nrows, ncols);
+  } else if (isa<FloatType>(attr.getElementType())) {
+    return isUnitDiagonalImpl<APFloat>(attr, nrows, ncols);
+  }
+  return false;
+}
+
+template <typename T>
+std::tuple<int64_t, int64_t> isSymmetricOrHermitianImpl(DenseElementsAttr attr,
+                                                        int64_t nrows,
+                                                        int64_t ncols) {
+  auto values = attr.getValues<T>().begin();
+  for (int64_t i = 0; i < nrows; i++) {
+    for (int64_t j = i + 1; j < ncols; j++) {
+      auto a = *(values + i * ncols + j);
+      auto b = *(values + j * ncols + i);
+      if (!utils::areEqual(a, b)) {
+        return {false, false}; // TODO: check for hermitian
+      }
+    }
+  }
+
+  return {true, false}; // TODO: check for hermitian
+}
+
+std::tuple<int64_t, int64_t>
+ValueProperties::isSymmetricOrHermitian(DenseElementsAttr attr, int64_t nrows,
+                                        int64_t ncols) {
+  if (isa<IntegerType>(attr.getElementType())) {
+    return isSymmetricOrHermitianImpl<APInt>(attr, nrows, ncols);
+  } else if (isa<FloatType>(attr.getElementType())) {
+    return isSymmetricOrHermitianImpl<APFloat>(attr, nrows, ncols);
+  }
+  return {false, false};
 }
 
 ValueProperties ValueProperties::meet(const ValueProperties &lhs,
@@ -303,7 +432,14 @@ void StructuredMatrixAnalysis::setToEntryState(
 LogicalResult StructuredMatrixAnalysis::visitOperation(
     Operation *op, ArrayRef<const StructuredMatrixLattice *> operands,
     ArrayRef<StructuredMatrixLattice *> results) {
+
   llvm::errs() << "Visiting operation " << *op << "\n";
+  for (auto operand : operands) {
+    llvm::errs() << "    operand: ";
+    operand->getValue().print(llvm::errs());
+    llvm::errs() << "\n";
+  }
+  llvm::errs() << "\n";
 
   return success();
 }
--- test/lit_tests/structured/symmetric.mlir
+++ test/lit_tests/structured/symmetric.mlir
@@ -2,7 +2,9 @@
 
 module {
   func.func @symmetric(%arg0: tensor<2x2xf32>) -> tensor<2x2xf32> {
-    %cst = stablehlo.constant dense<0.000000e+00> : tensor<2x2xf32>
+    %c0 = stablehlo.constant dense<0.000000e+00> : tensor<2x2xf32>
+    %c1 = stablehlo.constant dense<1.000000e+00> : tensor<2x2xf32>
+    %cst = stablehlo.add %c0, %c1 : tensor<2x2xf32>
     %0 = stablehlo.transpose %arg0, dims = [1, 0] : (tensor<2x2xf32>) -> tensor<2x2xf32>
     %1 = stablehlo.add %0, %arg0 : tensor<2x2xf32>
     %2 = stablehlo.transpose %cst, dims = [1, 0] : (tensor<2x2xf32>) -> tensor<2x2xf32>
