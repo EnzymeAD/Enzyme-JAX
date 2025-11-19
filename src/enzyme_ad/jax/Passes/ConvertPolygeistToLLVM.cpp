dfa7ab2473691427e15c8ca6859178e10cceeba8
Jacob Peng <jacobmpeng@gmail.com>
2025-11-19 13:33:50 -0600

    cudaFree fix with addr space

--- src/enzyme_ad/jax/Passes/ConvertPolygeistToLLVM.cpp
+++ src/enzyme_ad/jax/Passes/ConvertPolygeistToLLVM.cpp
@@ -3244,8 +3244,6 @@ private:
       };
       LLVM::CallOp::create(rewriter, loc, freeFunc.value(), args);
     } else if (backend.starts_with("xla")) {
-      auto ptrty = LLVM::LLVMPointerType::get(rewriter.getContext());
-
       // handle, ptr
       Type tys[] = {ptrty, ptrty};
 
