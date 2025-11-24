049f971ac5c4e9760a454173c02f379f4398a2d7
Roman Lee <rplee7@gmail.com>
2025-11-24 14:08:07 -0600

    MPICommRankOpLowering pass stuff

--- src/enzyme_ad/jax/Passes/LowerEnzymeXLAMPI.cpp
+++ src/enzyme_ad/jax/Passes/LowerEnzymeXLAMPI.cpp
@@ -29,12 +29,100 @@ using namespace mlir;
 struct MPICommRankOpLowering
     : public OpRewritePattern<enzymexla::MPICommRankOp> {
 
-  using OpRewritePattern::OpRewritePattern;
+  std::string backend;
+  MPICommRankOpLowering(std::string backend, MLIRContext *context, PatternBenefit benefit = 1)
+      : OpRewritePattern(context, benefit), backend(backend) {}
 
   LogicalResult matchAndRewrite(enzymexla::MPICommRankOp op,
                                 PatternRewriter &rewriter) const override {
+    // auto ctx = op->getContext();
+    // LLVMTypeConverter typeConverter(ctx);
+
+    auto comm = op.getOperand();
+    if (backend == "cpu") {
+      // auto moduleOp = op->getParentOfType<ModuleOp>();
+      // static int64_t fnNum = 0;
+
+      // // auto blasIntType = rewriter.getIntegerType(blasIntWidth);
+      // // auto llvmBlasIntType = typeConverter.convertType(blasIntType);
+      // auto llvmPtrType = LLVM::LLVMPointerType::get(ctx);
+      // auto llvmVoidPtrType = LLVM::LLVMVoidType::get(ctx);
+
+      // std::string fn = "MPI_Comm_Rank";
+      
+      // std::string fnName = fn + "wrapper_" + std::to_string(fnNum);
+      // fnNum++;
+      // {
+      // }
+    } else {
+      return rewriter.notifyMatchFailure(op, "Backend not supported: " + backend);
+    }
+
   }
+
+
+
+
 };
+
+// //===----------------------------------------------------------------------===//
+// // CommRankOpLowering
+// //===----------------------------------------------------------------------===//
+
+// struct CommRankOpLowering : public ConvertOpToLLVMPattern<mpi::CommRankOp> {
+//   using ConvertOpToLLVMPattern::ConvertOpToLLVMPattern;
+
+//   LogicalResult
+//   matchAndRewrite(mpi::CommRankOp op, OpAdaptor adaptor,
+//                   ConversionPatternRewriter &rewriter) const override {
+//     // get some helper vars
+//     Location loc = op.getLoc();
+//     MLIRContext *context = rewriter.getContext();
+//     Type i32 = rewriter.getI32Type();
+
+//     // ptrType `!llvm.ptr`
+//     Type ptrType = LLVM::LLVMPointerType::get(context);
+
+//     // grab a reference to the global module op:
+//     auto moduleOp = op->getParentOfType<ModuleOp>();
+
+//     auto mpiTraits = MPIImplTraits::get(moduleOp);
+//     // get communicator
+//     Value comm = mpiTraits->castComm(loc, rewriter, adaptor.getComm());
+
+//     // LLVM Function type representing `i32 MPI_Comm_rank(ptr, ptr)`
+//     auto rankFuncType =
+//         LLVM::LLVMFunctionType::get(i32, {comm.getType(), ptrType});
+//     // get or create function declaration:
+//     LLVM::LLVMFuncOp initDecl = getOrDefineFunction(
+//         moduleOp, loc, rewriter, "MPI_Comm_rank", rankFuncType);
+
+//     // replace with function call
+//     auto one = LLVM::ConstantOp::create(rewriter, loc, i32, 1);
+//     auto rankptr = LLVM::AllocaOp::create(rewriter, loc, ptrType, i32, one);
+//     auto callOp = LLVM::CallOp::create(rewriter, loc, initDecl,
+//                                        ValueRange{comm, rankptr.getRes()});
+
+//     // load the rank into a register
+//     auto loadedRank =
+//         LLVM::LoadOp::create(rewriter, loc, i32, rankptr.getResult());
+
+//     // if retval is checked, replace uses of retval with the results from the
+//     // call op
+//     SmallVector<Value> replacements;
+//     if (op.getRetval())
+//       replacements.push_back(callOp.getResult());
+
+//     // replace all uses, then erase op
+//     replacements.push_back(loadedRank.getRes());
+//     rewriter.replaceOp(op, replacements);
+
+//     return success();
+//   }
+// };
+
+// //===----------------------------------------------------------------------===//
+
 
 struct LowerEnzymeXLAMPIPass
     : public enzyme::impl::LowerEnzymeXLAMPIPassBase<
@@ -45,7 +133,7 @@ struct LowerEnzymeXLAMPIPass
     auto context = getOperation()->getContext();
     RewritePatternSet patterns(context);
 
-    patterns.add<MPICommRankOpLowering>(context);
+    patterns.add<MPICommRankOpLowering>(backend, context);
 
     GreedyRewriteConfig config;
     if (failed(applyPatternsAndFoldGreedily(getOperation(), std::move(patterns),
