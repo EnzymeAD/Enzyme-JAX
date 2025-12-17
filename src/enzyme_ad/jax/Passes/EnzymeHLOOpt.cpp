5e2b9515edd90ea40fe5bb120c917f7b8bab5d48
sbrantq <scharfrichterq@gmail.com>
2025-12-17 17:38:41 -0600

    fix

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -6910,7 +6910,7 @@ struct AndSimplify
       return success();
     }
 
-    // false & x -> x
+    // false & x -> false
     for (auto v : op.getOperands()) {
       if (matchPattern(v, m_Zero())) {
         rewriter.replaceOp(op, v);
@@ -6920,7 +6920,7 @@ struct AndSimplify
 
     // true & x -> x
     for (int i = 0; i < 2; i++) {
-      if (matchPattern(op.getOperand(i), m_One())) {
+      if (matchPattern(op.getOperand(i), m_AllOnes())) {
         rewriter.replaceOp(op, op.getOperand(1 - i));
         return success();
       }
@@ -6945,7 +6945,7 @@ struct OrSimplify
 
     // true | x -> true
     for (auto v : op.getOperands()) {
-      if (matchPattern(v, m_One())) {
+      if (matchPattern(v, m_AllOnes())) {
         rewriter.replaceOp(op, v);
         return success();
       }
@@ -6980,7 +6980,7 @@ struct XorSimplify
 
     // true ^ x -> not x
     for (int i = 0; i < 2; i++) {
-      if (matchPattern(op.getOperand(i), m_One())) {
+      if (matchPattern(op.getOperand(i), m_AllOnes())) {
         rewriter.replaceOpWithNewOp<stablehlo::NotOp>(op, op.getOperand(1 - i));
         return success();
       }
