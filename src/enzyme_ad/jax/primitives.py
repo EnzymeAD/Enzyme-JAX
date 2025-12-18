e5741f548770d852ab4c9ab410acb51644239d1f
Avik Pal <avikpal@mit.edu>
2025-12-18 13:44:21 -0500

    Revert "feat: dynamic_slice elementwise simplification (#1787)" (#1802)
    This reverts commit 0734438c27bac04ff05ebadfd37f782336dc54b7.

--- src/enzyme_ad/jax/primitives.py
+++ src/enzyme_ad/jax/primitives.py
@@ -321,7 +321,6 @@ def optimization_passes(
         "dus_dynamic_slice_simplify",
         "while_dus_ds_simplify",
         "reshape_slice_reshape",
-        "dynamic_slice_elementwise",
         "dot_general_remove_batch_dimensions",
         "delete_dims_reduce",
         "reduce_delete_dims",
