fix: scatter reverse mode derivative rules (#1824)
* fix: scatter reverse mode derivative rules

* feat: support add/mul with const arg

* test: enable ad in jaxmd tests

* test: add scatter reverse ad tests

test/bench_vs_xla.py
@@ -234,11 +234,7 @@ class ConstScatter(EnzymeJaxTest):
         N = 1024**2
         self.ins = [jnp.full(N, 2.7)]
         self.dins = [jnp.full(N, 3.1)]
-        self.douts = (self.dins[0],)
-
-        # TODO: support multiply for scatter reverse mode
-        self.revfilter = lambda _: []
-        # self.revfilter = justjax
+        self.douts = (jnp.array(5.0),)
 
 
 class ScatterSum(EnzymeJaxTest):