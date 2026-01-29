d760803e84517a52645cd7a78281e916846c9695
snonk <scq@mit.edu>
2026-01-29 11:32:24 -0600

    fix ==

--- src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
+++ src/enzyme_ad/jax/Passes/EnzymeHLOOpt.cpp
@@ -28437,7 +28437,7 @@ struct DotGeneralToSymm
 
     // can only replace with symm if either lhs or rhs is symmetric
     if (canApplySymmetricPattern(lhs, rewriter)) {
-      side == enzymexla::LapackSide::left;
+      side = enzymexla::LapackSide::left;
       if (rhsContractingDim == 0) {
         auto perm = rewriter.getDenseI64ArrayAttr({1, 0});
         rhs = stablehlo::TransposeOp::create(
@@ -28448,7 +28448,7 @@ struct DotGeneralToSymm
       }
 
     } else if (canApplySymmetricPattern(rhs, rewriter)) {
-      side == enzymexla::LapackSide::right;
+      side = enzymexla::LapackSide::right;
       if (lhsContractingDim == 0) {
         auto perm = rewriter.getDenseI64ArrayAttr({1, 0});
         lhs = stablehlo::TransposeOp::create(
