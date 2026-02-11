7ee60cfbbc8f4efd25186a5882234d6835914e94
William Moses <wmoses@google.com>
2026-02-11 17:50:21 -0600

    More info on the lockstep failure errors (#2073)

--- src/enzyme_ad/jax/Passes/AffineToStableHLORaising.cpp
+++ src/enzyme_ad/jax/Passes/AffineToStableHLORaising.cpp
@@ -1724,7 +1724,7 @@ static LogicalResult tryRaisingLockStepForOpToStableHLO(
   if (pc.options.dump_failed_lockstep) {
     llvm::errs() << " failed lockstep of for raise: " << *forOp << "\n";
   }
-  return forOp.emitError("Not lockstep executable");
+  return forOp.emitError("Not lockstep executable") << *forOp;
 }
 
 static LogicalResult
