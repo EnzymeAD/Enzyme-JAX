1d4c5b7c0adabaf894e00b18dfebbffaba07cc5c
Arjun Vedantham <arjunsv2@illinois.edu>
2026-02-16 19:53:00 -0600

    add a Cost type

--- src/enzyme_ad/jax/Dialect/Perfify/Dialect.cpp
+++ src/enzyme_ad/jax/Dialect/Perfify/Dialect.cpp
@@ -5,8 +5,15 @@
 
 #include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyDialect.cpp.inc"
 
+#define GET_TYPEDEF_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyTypes.cpp.inc"
+
 // Initialize the dialect
 void mlir::enzyme::perfify::PerfifyDialect::initialize() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyTypes.cpp.inc"
+      >();
   addOperations<
 #define GET_OP_LIST
 #include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyOps.cpp.inc"
--- src/enzyme_ad/jax/Dialect/Perfify/Dialect.h
+++ src/enzyme_ad/jax/Dialect/Perfify/Dialect.h
@@ -7,11 +7,15 @@
 #include "mlir/IR/DialectImplementation.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/Region.h"
+#include "mlir/IR/SymbolTable.h"
 #include "mlir/IR/Types.h"
 
 // Include the dialect
 #include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyDialect.h.inc"
 
+// Types
+#define GET_TYPEDEF_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyTypes.h.inc"
 // Operations
 #define GET_OP_CLASSES
 #include "src/enzyme_ad/jax/Dialect/Perfify/PerfifyOps.h.inc"
