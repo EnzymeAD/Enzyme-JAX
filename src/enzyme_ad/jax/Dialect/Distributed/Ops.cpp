907ebfbfbeaa1ca426cbb0ebba6be991f1ff0e28
Egan Johnson <egancj2@illinois.edu>
2026-02-24 16:49:45 -0600

    Decompose Collectives Ops

--- src/enzyme_ad/jax/Dialect/Distributed/Ops.cpp
+++ src/enzyme_ad/jax/Dialect/Distributed/Ops.cpp
@@ -160,6 +160,45 @@ LogicalResult RegionComputationOp::verify() {
   return mlir::success();
 }
 
+LogicalResult SubmeshCollectivePartsOp::inferReturnTypes(
+    MLIRContext *context, std::optional<Location> location, ValueRange operands,
+    DictionaryAttr attributes, OpaqueProperties properties, RegionRange regions,
+    SmallVectorImpl<Type> &inferredReturnTypes) {
+  SubmeshCollectivePartsOpAdaptor adaptor(operands, attributes, properties,
+                                          regions);
+  if (!adaptor.getSubmesh()) {
+    if (location)
+      mlir::emitError(*location) << "missing submesh operand";
+    return failure();
+  }
+
+  auto submeshDefOp = adaptor.getSubmesh().getDefiningOp<LogicalMeshOp>();
+  if (!submeshDefOp) {
+    if (location) {
+      mlir::emitError(*location)
+          << "requires submesh operand to be defined by LogicalMeshOp";
+    }
+    return failure();
+  }
+
+  FailureOr<int64_t> meshSize = getLogicalMeshSize(submeshDefOp);
+  if (failed(meshSize)) {
+    if (location)
+      mlir::emitError(*location) << "failed to determine submesh size";
+    return failure();
+  }
+
+  inferredReturnTypes.reserve(static_cast<size_t>(2 * (*meshSize)));
+  for (int64_t i = 0; i < *meshSize; ++i) {
+    inferredReturnTypes.push_back(CollectiveTokenType::get(context));
+  }
+  for (int64_t i = 0; i < *meshSize; ++i) {
+    inferredReturnTypes.push_back(CollectiveTokenType::get(context));
+  }
+
+  return success();
+}
+
 } // namespace mlir::enzyme::distributed
 #define GET_OP_CLASSES
 #include "src/enzyme_ad/jax/Dialect/Distributed/DistributedOps.cpp.inc"
