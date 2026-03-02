d2d583be44878b1afc887757f4b44541d7e305d8
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-03-02 15:31:51 +0100

    implement verifiers

---
+++ src/enzyme_ad/jax/Dialect/BLAS/Ops.cpp
@@ -0,0 +1,130 @@
+#include "Dialect.h"
+
+using namespace mlir;
+using namespace mlir::enzymexla::blas;
+
+LogicalResult SymmOp::verify() {
+  auto alpha = getAlpha();
+  auto A = getA();
+  auto B = getB();
+  auto beta = getBeta();
+  auto C = getC();
+  auto side = getSide();
+  auto uplo = getUplo();
+
+  auto type_alpha = cast<RankedTensorType>(alpha.getType());
+  auto type_A = cast<RankedTensorType>(A.getType());
+  auto type_B = cast<RankedTensorType>(B.getType());
+  auto type_beta = cast<RankedTensorType>(beta.getType());
+  auto type_C = cast<RankedTensorType>(C.getType());
+
+  auto eltype = type_alpha.getElementType();
+
+  if (eltype != type_A.getElementType() || eltype != type_B.getElementType() ||
+      eltype != type_beta.getElementType() || eltype != type_C.getElementType())
+    return emitOpError(
+        "alpha, beta, A, B, and C must all have the same element type");
+
+  if (!llvm::isa<FloatType>(eltype) && !llvm::isa<ComplexType>(eltype) &&
+      !llvm::isa<FloatType>(cast<ComplexType>(eltype).getElementType()))
+    return emitOpError("element type must be floating-point or complex type of "
+                       "floating-point element type");
+
+  if (alpha.getRank() != 0)
+    return emitOpError("alpha must be a scalar");
+
+  if (beta.getRank() != 0)
+    return emitOpError("beta must be a scalar");
+
+  // TODO check shapes of A, B, and C
+
+  if (!side.isValid())
+    return emitOpError("invalid side attribute value");
+  if (!uplo.isValid())
+    return emitOpError("invalid uplo attribute value");
+
+  return success();
+}
+
+LogicalResult SyrkOp::verify() {
+  auto alpha = getAlpha();
+  auto A = getA();
+  auto B = getB();
+  auto beta = getBeta();
+  auto C = getC();
+  auto uplo = getUplo();
+  auto output_uplo = getOutputUplo();
+  auto transpose = getTranspose();
+
+  auto type_alpha = cast<RankedTensorType>(alpha.getType());
+  auto type_A = cast<RankedTensorType>(A.getType());
+  auto type_B = cast<RankedTensorType>(B.getType());
+  auto type_beta = cast<RankedTensorType>(beta.getType());
+  auto type_C = cast<RankedTensorType>(C.getType());
+
+  auto eltype = type_alpha.getElementType();
+
+  if (eltype != type_A.getElementType() || eltype != type_B.getElementType() ||
+      eltype != type_beta.getElementType() || eltype != type_C.getElementType())
+    return emitOpError(
+        "alpha, beta, A, B, and C must all have the same element type");
+
+  if (!llvm::isa<FloatType>(eltype) && !llvm::isa<ComplexType>(eltype) &&
+      !llvm::isa<FloatType>(cast<ComplexType>(eltype).getElementType()))
+    return emitOpError("element type must be floating-point or complex type of "
+                       "floating-point element type");
+
+  if (alpha.getRank() != 0)
+    return emitOpError("alpha must be a scalar");
+
+  if (beta.getRank() != 0)
+    return emitOpError("beta must be a scalar");
+
+  // TODO check shapes of A, B, and C
+
+  if (!uplo.isValid())
+    return emitOpError("invalid uplo attribute value");
+  if (!output_uplo.isValid())
+    return emitOpError("invalid output_uplo attribute value");
+  if (!transpose.isValid())
+    return emitOpError("invalid transpose attribute value");
+
+  return success();
+}
+
+LogicalResult TrmmOp::verify() {
+  auto alpha = getAlpha();
+  auto A = getA();
+  auto B = getB();
+  auto side = getSide();
+  auto uplo = getUplo();
+  auto transpose = getTranspose();
+
+  auto type_alpha = cast<RankedTensorType>(alpha.getType());
+  auto type_A = cast<RankedTensorType>(A.getType());
+  auto type_B = cast<RankedTensorType>(B.getType());
+
+  auto eltype = type_alpha.getElementType();
+
+  if (eltype != type_A.getElementType() || eltype != type_B.getElementType())
+    return emitOpError("alpha, A, and B must all have the same element type");
+
+  if (!llvm::isa<FloatType>(eltype) && !llvm::isa<ComplexType>(eltype) &&
+      !llvm::isa<FloatType>(cast<ComplexType>(eltype).getElementType()))
+    return emitOpError("element type must be floating-point or complex type of "
+                       "floating-point element type");
+
+  if (alpha.getRank() != 0)
+    return emitOpError("alpha must be a scalar");
+
+  // TODO check shapes of A and B
+
+  if (!side.isValid())
+    return emitOpError("invalid side attribute value");
+  if (!uplo.isValid())
+    return emitOpError("invalid uplo attribute value");
+  if (!transpose.isValid())
+    return emitOpError("invalid transpose attribute value");
+
+  return success();
+}
