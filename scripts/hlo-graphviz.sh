74d8b51e5b7f9e30bc8b32de935b10c61510da88
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-03-10 08:46:56 -0500

    fixes

--- scripts/hlo-graphviz.sh
+++ scripts/hlo-graphviz.sh
@@ -5,17 +5,18 @@
 export CC=clang
 
 usage() {
-    echo "Usage: $0 [-o <output path of dot file>] [-f <matching func.func name>] <input hlo file>"
+    echo "Usage: $0 [-o <output path of dot file>] [-f <matching func.func pattern>] <input hlo file>"
     exit 1
 }
 
 OUTPUT=""
-FUNC_NAME=""
-
-while getopts "o:f:" ; do
+PATTERN=""
+DEBUG=false
+while getopts o:f:d opt; do
     case $opt in
         o) OUTPUT="$OPTARG" ;;
-        f) FUNC_NAME="$OPTARG" ;;
+        f) PATTERN="$OPTARG" ;;
+        d) DEBUG=true ;;
         *) usage ;;
         :) echo "Option -$OPTARG requires an argument." >&2
            exit 1
@@ -25,22 +26,42 @@ done
 
 shift $((OPTIND - 1))
 INPUT="$1"
-if [[ -z "$INPUT" ]]; then
+if [ -z "$INPUT" ]; then
     usage
 fi
 
+HLO_TRANSLATE_CMD=./$(bazel cquery --output=starlark --starlark:expr=target.files_to_run.executable.path @xla//xla/hlo/tools:hlo-translate 2>/dev/null)
+MLIR_OPT_CMD=./$(bazel cquery --output=starlark --starlark:expr=target.files_to_run.executable.path //:enzymexlamlir-opt 2>/dev/null)
+
 # convert HLO to MLIR
-MLIR_CODE=$(bazel run --run_under="cd $PWD &&" @xla//xla/hlo/tools:hlo-translate -- --hlo-to-mlir $INPUT)
+TMP_HLO_TRANSLATE_CMD=$(mktemp)
+$HLO_TRANSLATE_CMD --hlo-to-mlir $INPUT -o $TMP_HLO_TRANSLATE_CMD
 
 # filter MLIR to only include the specified `func.func` if provided, otherwise include all
-if [ -n "$FUNC_NAME" ]; then
-    MLIR_CODE=$(echo "$MLIR_CODE" | awk '/func\.func private @${FUNC_NAME}/ {p=1} p {print; c+=gsub(/\{/,"{"); c-=gsub(/\}/,"}"); if(c==0) p=0}')
+TMP_AWK=$TMP_HLO_TRANSLATE_CMD
+if [ -n "$PATTERN" ]; then
+    TMP_AWK=$(mktemp)
+    awk '/func\.func '"$PATTERN"'/ {p=1} p {print; c+=gsub(/\{/,"{"); c-=gsub(/\}/,"}"); if(c==0) p=0}' $TMP_HLO_TRANSLATE_CMD > $TMP_AWK
 fi
 
 # convert MLIR to graphviz dot format
-echo "$MLIR_CODE" | bazel run --run_under="cd $PWD &&" //:enzymexlamlir-opt -- --view-op-graph 2>&1 >/dev/null
+TMP_MLIR_OPT_CMD=$(mktemp)
+$MLIR_OPT_CMD --view-op-graph $TMP_AWK 2>$TMP_MLIR_OPT_CMD >/dev/null
+
 if [ -n "$OUTPUT" ]; then
-    echo $MLIR_CODE > $OUTPUT
+    mv $TMP_MLIR_OPT_CMD $OUTPUT
 else
-    echo $MLIR_CODE
+    cat $TMP_MLIR_OPT_CMD
+fi
+
+# clean temporary files
+if [ "$DEBUG" = true ]; then
+    echo "Debug mode enabled, temporary files not removed:"
+    echo "  HLO to MLIR output: $TMP_HLO_TRANSLATE_CMD"
+    if [ -n "$FUNC_NAME" ]; then
+        echo "  Filtered MLIR output: $TMP_AWK"
+    fi
+    echo "  MLIR to Graphviz output: $TMP_MLIR_OPT_CMD"
+else
+    rm $TMP_HLO_TRANSLATE_CMD $TMP_AWK $TMP_MLIR_OPT_CMD
 fi
