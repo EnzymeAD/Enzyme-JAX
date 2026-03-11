15d4c5d85a33ea7ded94414da414289bc9244e5e
Egan Johnson <egancj2@illinois.edu>
2026-03-10 23:41:42 -0500

    Timing Pass

--- src/enzyme_ad/jax/Dialect/Distributed/CollectiveOps.cpp
+++ src/enzyme_ad/jax/Dialect/Distributed/CollectiveOps.cpp
@@ -38,4 +38,52 @@ LogicalResult SubmeshCollectivePartsOp::inferReturnTypes(
   return success();
 }
 
+mlir::Value CollectiveOp::getHandle() { return getToken(); }
+
+// Send, Recv, and SendRecv op interface
+llvm::SmallVector<mlir::Value> SendOp::happensAfter() {
+  return {}; // sending is first in the chain
+}
+llvm::SmallVector<mlir::Value> SendOp::simulatenousWith() {
+  return {getToken()}; // satisfies its send token
+}
+bool SendOp::concurrentWith(Operation *other) { return false; }
+llvm::SmallVector<mlir::Value> RecvOp::happensAfter() {
+  return {getToken()}; // receiving happens after the token is satisfied
+}
+llvm::SmallVector<mlir::Value> RecvOp::simulatenousWith() {
+  return {}; // receiving is last in the chain
+}
+bool RecvOp::concurrentWith(Operation *other) { return false; }
+
+// SendRecv behaves differently since it is a synchronous protocol
+llvm::SmallVector<mlir::Value> SendRecvOp::happensAfter() { return {}; }
+llvm::SmallVector<mlir::Value> SendRecvOp::simulatenousWith() {
+  // Need to walk the token back to its defining collective- it could be defined
+  // by a parts op.
+  return {resolveCollectiveTokenToRootCollective(getToken())}; // satisfies its send/recv token
+}
+bool SendRecvOp::concurrentWith(Operation *other) {
+  // Must commute with other SendRecvOps on the same token so long as there is
+  // no SSA dependency between them
+  if (auto otherSendRecv = dyn_cast<SendRecvOp>(other)) {
+    if (getToken() == otherSendRecv.getToken()) {
+      // Check for SSA dependencies
+      for (Value operand : getOperands()) {
+        if (llvm::is_contained(otherSendRecv.getOperands(), operand)) {
+          return false; // There is an SSA dependency, cannot commute
+        }
+      }
+      for (Value result : getOperation()->getResults()) {
+        if (llvm::is_contained(otherSendRecv.getOperation()->getResults(),
+                               result)) {
+          return false; // There is an SSA dependency, cannot commute
+        }
+      }
+      return true; // No SSA dependencies, can commute
+    }
+  }
+  return false; // Don't care about commuting with other ops
+}
+
 } // namespace mlir::enzyme::distributed
