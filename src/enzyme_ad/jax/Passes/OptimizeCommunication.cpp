33162be4463a0203722264efcb50117115a92787
William S. Moses <gh@wsmoses.com>
2026-03-14 17:16:07 -0500

    fix

--- src/enzyme_ad/jax/Passes/OptimizeCommunication.cpp
+++ src/enzyme_ad/jax/Passes/OptimizeCommunication.cpp
@@ -2157,9 +2157,10 @@ struct RotateCommOptimize : public OpRewritePattern<enzymexla::RotateOp> {
 };
 
 struct RotateSpmdOptimize : public OpRewritePattern<enzymexla::RotateOp> {
-
-  RotateSpmdOptimize(MLIRContext *context, PatternBenefit benefit = 1)
-      : OpRewritePattern(context, benefit) {}
+  int64_t bufferize;
+  RotateSpmdOptimize(int64_t bufferize, MLIRContext *context,
+                     PatternBenefit benefit = 1)
+      : OpRewritePattern(context, benefit), bufferize(bufferize) {}
   LogicalResult matchAndRewrite(enzymexla::RotateOp rotate,
                                 PatternRewriter &rewriter) const override {
     if (rotate->getParentOfType<sdy::ManualComputationOp>())
@@ -2191,7 +2192,7 @@ struct RotateSpmdOptimize : public OpRewritePattern<enzymexla::RotateOp> {
       std::string opaque =
           "dimension=" + std::to_string(rotateDimension) +
           ",left_amount=" + std::to_string(rotate.getAmount()) +
-          ",right_amount=0";
+          ",right_amount=0,bufferize=" + std::to_string(bufferize);
 
       auto fnSym = rewriter.getStringAttr("_SPMDInternalOp_MultiRotate");
 
@@ -2214,6 +2215,16 @@ struct RotateSpmdOptimize : public OpRewritePattern<enzymexla::RotateOp> {
       mlir::sdy::setShardings(ccall, sdy::TensorShardingPerValueAttr::get(
                                          rotate.getContext(), newShardings));
       rewriter.replaceOp(rotate, ValueRange(ccall->getResults()[0]));
+
+      Value neutral = ccall.getResult(rotate.getAmount());
+      DominanceInfo domInfo;
+      rewriter.replaceUsesWithIf(ccall.getOperands()[0], neutral,
+                                 [&](OpOperand &use) {
+                                   Operation *user = use.getOwner();
+                                   if (!domInfo.properlyDominates(ccall, user))
+                                     return false;
+                                   return true;
+                                 });
       return success();
     }
 
@@ -2224,7 +2235,8 @@ struct RotateSpmdOptimize : public OpRewritePattern<enzymexla::RotateOp> {
       // Our op is rotate left:
       std::string opaque =
           "dimension=" + std::to_string(rotateDimension) +
-          ",left_amount=0,right_amount=" + std::to_string(right_amount);
+          ",left_amount=0,right_amount=" + std::to_string(right_amount) +
+          ",bufferize=" + std::to_string(bufferize);
 
       auto fnSym = rewriter.getStringAttr("_SPMDInternalOp_MultiRotate");
 
@@ -2246,6 +2258,17 @@ struct RotateSpmdOptimize : public OpRewritePattern<enzymexla::RotateOp> {
       mlir::sdy::setShardings(ccall, sdy::TensorShardingPerValueAttr::get(
                                          rotate.getContext(), newShardings));
       rewriter.replaceOp(rotate, ValueRange(ccall->getResults()[right_amount]));
+
+      Value neutral = ccall.getResult(0);
+      DominanceInfo domInfo;
+      rewriter.replaceUsesWithIf(ccall.getOperands()[0], neutral,
+                                 [&](OpOperand &use) {
+                                   Operation *user = use.getOwner();
+                                   if (!domInfo.properlyDominates(ccall, user))
+                                     return false;
+                                   return true;
+                                 });
+
       return success();
     }
 
@@ -2275,10 +2298,10 @@ struct RotateSpmdOptimize : public OpRewritePattern<enzymexla::RotateOp> {
 
 struct MultiRotateCustomCallOptimize
     : public OpRewritePattern<enzymexla::MultiRotateOp> {
-
-  MultiRotateCustomCallOptimize(MLIRContext *context,
+  int64_t bufferize;
+  MultiRotateCustomCallOptimize(int64_t bufferize, MLIRContext *context,
                                 PatternBenefit benefit = 1)
-      : OpRewritePattern(context, benefit) {}
+      : OpRewritePattern(context, benefit), bufferize(bufferize) {}
   LogicalResult matchAndRewrite(enzymexla::MultiRotateOp rotate,
                                 PatternRewriter &rewriter) const override {
     if (rotate->getParentOfType<sdy::ManualComputationOp>())
@@ -2306,7 +2329,8 @@ struct MultiRotateCustomCallOptimize
     std::string opaque =
         "dimension=" + std::to_string(rotateDimension) +
         ",left_amount=" + std::to_string(rotate.getLeftAmount()) +
-        ",right_amount=" + std::to_string(rotate.getRightAmount());
+        ",right_amount=" + std::to_string(rotate.getRightAmount()) +
+        ",bufferize=" + std::to_string(bufferize);
 
     auto fnSym = rewriter.getStringAttr("_SPMDInternalOp_MultiRotate");
 
@@ -2386,8 +2410,9 @@ bool detectCrossShardPattern(Value operand, Operation *op,
 
 struct MultiSliceCustomCallOptimize
     : public OpRewritePattern<enzymexla::MultiSliceOp> {
-
-  MultiSliceCustomCallOptimize(MLIRContext *context, PatternBenefit benefit = 1)
+  int64_t bufferize;
+  MultiSliceCustomCallOptimize(int64_t bufferize, MLIRContext *context,
+                               PatternBenefit benefit = 1)
       : OpRewritePattern(context, benefit) {}
 
   std::string serializeDenseI64ArrayAttr(ArrayRef<int64_t> array) const {
@@ -2514,7 +2539,8 @@ struct MultiSliceCustomCallOptimize
                          ",amount=" + std::to_string(slice.getAmount()) +
                          ",start_indices=" + start_indices_str +
                          ",limit_indices=" + limit_indices_str +
-                         ",strides=" + strides_str;
+                         ",strides=" + strides_str +
+                         ",bufferize=" + std::to_string(bufferize);
 
     auto fnSym = rewriter.getStringAttr("_SPMDInternalOp_MultiSlice");
 
@@ -5017,7 +5043,8 @@ struct OptimizeCommunicationPass
                                        PatternBenefit(rotate_comm));
 
     if (rotate_spmd > 0)
-      patterns.add<RotateSpmdOptimize>(context, PatternBenefit(rotate_spmd));
+      patterns.add<RotateSpmdOptimize>((int)multi_buffer, context,
+                                       PatternBenefit(rotate_spmd));
 
     if (multirotate_spmd > 0)
       patterns.add<MultiRotateSpmdOptimize>(context,
@@ -5025,11 +5052,11 @@ struct OptimizeCommunicationPass
 
     if (multirotate_custom_call > 0)
       patterns.add<MultiRotateCustomCallOptimize>(
-          context, PatternBenefit(multirotate_custom_call));
+          multi_buffer, context, PatternBenefit(multirotate_custom_call));
 
     if (multislice_custom_call > 0)
       patterns.add<MultiSliceCustomCallOptimize>(
-          context, PatternBenefit(multislice_custom_call));
+          multi_buffer, context, PatternBenefit(multislice_custom_call));
 
     if (rotate_to_pad_comm > 0)
       patterns.add<RotateToPadCommOptimize>(context,
