e4fc28168d230fab6dcd7d1d4163e632d022ee1d
snonk <scq@mit.edu>
2026-04-01 09:41:14 -0500

    added CPU lowering

--- src/enzyme_ad/jax/Passes/LowerEnzymeXLALinalg.cpp
+++ src/enzyme_ad/jax/Passes/LowerEnzymeXLALinalg.cpp
@@ -45,7 +45,11 @@ struct TridiagonalSolveOpLowering
 
   LogicalResult matchAndRewrite(enzymexla::TridiagonalSolveOp op,
                                 PatternRewriter &rewriter) const override {
-    if (backend == "cpu") {
+
+    auto BType = cast<RankedTensorType>(op.getB().getType());
+    auto nBatchDims = BType.getRank() - 2;
+
+    if (backend == "cpu" && nBatchDims == 0) {
       return matchAndRewriteCPU(op, rewriter);
     } else if (backend == "cuda") {
       return matchAndRewriteCUDA(op, rewriter);
@@ -55,8 +59,156 @@ struct TridiagonalSolveOpLowering
 
   LogicalResult matchAndRewriteCPU(enzymexla::TridiagonalSolveOp op,
                                    PatternRewriter &rewriter) const {
-    // TODO
-    return matchAndRewriteFallback(op, rewriter);
+    auto ctx = op->getContext();
+    LLVMTypeConverter typeConverter(ctx);
+
+    Value dl = op.getDL();
+    Value d = op.getD();
+    Value du = op.getDU();
+    Value B = op.getB();
+
+    auto dlType = cast<RankedTensorType>(dl.getType());
+    auto dType = cast<RankedTensorType>(d.getType());
+    auto duType = cast<RankedTensorType>(dl.getType());
+    auto bType = cast<RankedTensorType>(B.getType());
+
+    if (!dlType || !dType || !duType || !bType)
+      return rewriter.notifyMatchFailure(
+          op, "operand types not ranked tensor types");
+
+    if (!dlType.hasRank() || !dType.hasRank() || !duType.hasRank() ||
+        !bType.hasRank())
+      return rewriter.notifyMatchFailure(op, "expected ranked tensor types");
+
+    if (dlType.getRank() != 1 || dType.getRank() != 1 ||
+        duType.getRank() != 1 || bType.getRank() > 2)
+      return rewriter.notifyMatchFailure(
+          op, "only 2D matrices supported for gtsv on CPU");
+
+    Type elementType = dlType.getElementType();
+    auto blasIntType = rewriter.getIntegerType(blasIntWidth);
+    auto intType = RankedTensorType::get({}, blasIntType);
+    auto uint8Type =
+        RankedTensorType::get({}, rewriter.getIntegerType(8, false));
+    auto llvmIntType = typeConverter.convertType(blasIntType);
+    auto llvmPtrType = LLVM::LLVMPointerType::get(ctx);
+    auto llvmVoidType = LLVM::LLVMVoidType::get(ctx);
+
+    std::string lapackFn;
+    if (auto prefix = lapackPrecisionPrefix(elementType)) {
+      lapackFn = "enzymexla_lapack_" + *prefix + "gtsv_";
+    } else {
+      op->emitOpError() << "Unsupported element type: " << elementType;
+      return rewriter.notifyMatchFailure(op, "unsupported element type");
+    }
+
+    auto moduleOp = op->getParentOfType<ModuleOp>();
+
+    if (!moduleOp.lookupSymbol<LLVM::LLVMFuncOp>(lapackFn)) {
+      OpBuilder::InsertionGuard guard(rewriter);
+      rewriter.setInsertionPointToStart(moduleOp.getBody());
+      auto funcType = LLVM::LLVMFunctionType::get(llvmVoidType,
+                                                  {
+                                                      llvmPtrType, // n
+                                                      llvmPtrType, // nrhs
+                                                      llvmPtrType, // dl
+                                                      llvmPtrType, // d
+                                                      llvmPtrType, // du
+                                                      llvmPtrType, // b
+                                                      llvmPtrType, // ldb
+                                                      llvmPtrType, // info
+                                                  },
+                                                  false);
+      rewriter.create<LLVM::LLVMFuncOp>(op.getLoc(), lapackFn, funcType,
+                                        LLVM::Linkage::External);
+    }
+
+    static int64_t fn_counter = 0;
+    std::string funcFnName =
+        blasFnWrapper + "wrapper_" + std::to_string(fn_counter++);
+
+    SmallVector<bool> isColMajorArrOperands(8, true);
+    SmallVector<int64_t> operandRanks = {0, 0, 1, 1, 1, 2, 0, 0};
+
+    SmallVector<bool> isColMajorArrResults(5, true);
+    SmallVector<int64_t> outputRanks = {1, 1, 1, 2, 0};
+
+    auto operandLayouts =
+        getSHLOLayout(rewriter, operandRanks, isColMajorArrOperands, 2);
+    auto resultLayouts =
+        getSHLOLayout(rewriter, outputRanks, isColMajorArrResults, 2);
+
+    SmallVector<Attribute> aliases = {
+        stablehlo::OutputOperandAliasAttr::get(ctx, {}, 2, {}), // dl
+        stablehlo::OutputOperandAliasAttr::get(ctx, {}, 3, {}), // d
+        stablehlo::OutputOperandAliasAttr::get(ctx, {}, 4, {}), // du
+        stablehlo::OutputOperandAliasAttr::get(ctx, {}, 5, {}), // b
+        stablehlo::OutputOperandAliasAttr::get(ctx, {}, 7, {}), // info
+    };
+
+    func::FuncOp shloFunc;
+
+    {
+      OpBuilder::InsertionGuard guard(rewriter);
+      rewriter.setInsertionPointToStart(moduleOp.getBody());
+
+      SmallVector<Type> argTypes = {
+          op.getDl().getType(),
+          op.getD().getType(),
+          op.getDu().getType(),
+          op.getB().getType(),
+      };
+      SmallVector<Type> retTypes = {op.getB().getType()};
+
+      auto calleeType = rewriter.getFunctionType(argTypes, retTypes);
+      shloFunc =
+          func::FuncOp::create(rewriter, op.getLoc(), funcFnName, calleeType);
+      shloFunc.setPrivate();
+
+      auto &entryBlock = *shloFunc.addEntryBlock();
+      rewriter.setInsertionPointToStart(&entryBlock);
+
+      auto dl = entryBlock.getArgument(0);
+      auto d = entryBlock.getArgument(1);
+      auto du = entryBlock.getArgument(2);
+      auto b = entryBlock.getArgument(3);
+
+      auto n = stablehlo::ConvertOp::create(
+          rewriter, op.getLoc(), intType,
+          stablehlo::GetDimensionSizeOp::create(rewriter, op.getLoc(), d, 0));
+      auto nrhs = stablehlo::ConvertOp::create(
+          rewriter, op.getLoc(), intType,
+          stablehlo::GetDimensionSizeOp::create(rewriter, op.getLoc(), B, 1));
+      auto ldb = stablehlo::ConvertOp::create(
+          rewriter, op.getLoc(), intType,
+          stablehlo::GetDimensionSizeOp::create(rewriter, op.getLoc(), B, 0));
+      auto info = stablehlo::ConstantOp::create(
+          rewriter, op.getLoc(), intType,
+          cast<ElementsAttr>(makeAttr(intType, -1)));
+
+      auto jitCall = enzymexla::JITCallOp::create(
+          rewriter, op.getLoc(),
+          TypeRange{op.getDl().getType(), op.getD().getType(),
+                    op.getDu().getType(), op.getB().getType(), intType},
+          mlir::FlatSymbolRefAttr::get(ctx, lapackFn),
+          ValueRange{n, nrhs, dl, d, du, b, ldb, info},
+          rewriter.getStringAttr(""),
+          /*operand_layouts=*/operandLayouts,
+          /*result_layouts=*/resultLayouts,
+          /*arg_attrs=*/nullptr,
+          /*res_attrs=*/nullptr,
+          /*output_operand_aliases=*/rewriter.getArrayAttr(aliases),
+          /*xla_side_effect_free=*/rewriter.getUnitAttr());
+
+      func::ReturnOp::create(rewriter, op.getLoc(),
+                             ValueRange{jitCall.getResult(3)}); // B
+    }
+    auto callOp = func::CallOp::create(rewriter, op.getLoc(), shloFunc,
+                                       ValueRange{dl, d, du, B});
+
+    rewriter.replaceOp(op, callOp);
+
+    return success();
   }
 
   LogicalResult matchAndRewriteCUDA(enzymexla::TridiagonalSolveOp op,
@@ -98,9 +250,9 @@ struct TridiagonalSolveOpLowering
             mlir::stablehlo::CustomCallApiVersion::API_VERSION_TYPED_FFI),
         /*calledcomputations*/ nullptr,
         /*operand_layouts*/
-        getSHLOLayout(rewriter, operandRanks, areColMajor, rank),
+        getSHLOLayout(rewriter, operandRanks, areColMajor, bRank),
         /*result_layouts*/
-        getSHLOLayout(rewriter, {rank}, SmallVector<bool>(rank, true), rank),
+        getSHLOLayout(rewriter, {bRank}, SmallVector<bool>(bRank, true), bRank),
         /*output_operand_aliases*/ aliases);
 
     Value result = customCall.getResult(0);
