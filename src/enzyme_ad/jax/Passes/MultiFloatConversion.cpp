440b4a7c82300332c3534130f530db9b899f8158
Billy Moses <gh@wsmoses.com>
2026-04-05 16:19:46 +0000

    Optimize SineOp and update sine_precise.mlir

--- src/enzyme_ad/jax/Passes/MultiFloatConversion.cpp
+++ src/enzyme_ad/jax/Passes/MultiFloatConversion.cpp
@@ -1354,6 +1354,8 @@ struct SineOpConversion : public OpConversionPattern<stablehlo::SineOp> {
     Value final_raw_lo = rewriter.create<stablehlo::SelectOp>(
         loc, is_sine_quad, res_sine_lo, res_cosine_lo);
 
+    bool isTuple = concatDimension == "tuple";
+
     // Apply quadrant sign
     auto twoIntAttr = rewriter.getIntegerAttr(rewriter.getI32Type(), 2);
     Value two_int = rewriter.create<stablehlo::ConstantOp>(
@@ -1363,25 +1365,54 @@ struct SineOpConversion : public OpConversionPattern<stablehlo::SineOp> {
     Value is_neg_quad = rewriter.create<stablehlo::CompareOp>(
         loc, quadrant_and_2, zero_int, stablehlo::ComparisonDirection::NE);
 
-    Value neg_final_hi = rewriter.create<stablehlo::NegOp>(loc, final_raw_hi);
-    Value neg_final_lo = rewriter.create<stablehlo::NegOp>(loc, final_raw_lo);
+    if (isTuple) {
+      Value neg_final_hi = rewriter.create<stablehlo::NegOp>(loc, final_raw_hi);
+      Value neg_final_lo = rewriter.create<stablehlo::NegOp>(loc, final_raw_lo);
 
-    Value final_hi = rewriter.create<stablehlo::SelectOp>(
-        loc, is_neg_quad, neg_final_hi, final_raw_hi);
-    Value final_lo = rewriter.create<stablehlo::SelectOp>(
-        loc, is_neg_quad, neg_final_lo, final_raw_lo);
+      Value final_hi = rewriter.create<stablehlo::SelectOp>(
+          loc, is_neg_quad, neg_final_hi, final_raw_hi);
+      Value final_lo = rewriter.create<stablehlo::SelectOp>(
+          loc, is_neg_quad, neg_final_lo, final_raw_lo);
 
-    // Apply original sign (lt_zero)
-    Value neg_final2_hi = rewriter.create<stablehlo::NegOp>(loc, final_hi);
-    Value neg_final2_lo = rewriter.create<stablehlo::NegOp>(loc, final_lo);
+      // Apply original sign (lt_zero)
+      Value neg_final2_hi = rewriter.create<stablehlo::NegOp>(loc, final_hi);
+      Value neg_final2_lo = rewriter.create<stablehlo::NegOp>(loc, final_lo);
 
-    Value res_h = rewriter.create<stablehlo::SelectOp>(loc, lt_zero,
-                                                       neg_final2_hi, final_hi);
-    Value res_l = rewriter.create<stablehlo::SelectOp>(loc, lt_zero,
-                                                       neg_final2_lo, final_lo);
+      Value res_h = rewriter.create<stablehlo::SelectOp>(loc, lt_zero,
+                                                         neg_final2_hi, final_hi);
+      Value res_l = rewriter.create<stablehlo::SelectOp>(loc, lt_zero,
+                                                         neg_final2_lo, final_lo);
 
-    Value packed = packLimbs(res_h, res_l, rewriter, loc, concatDimension);
-    rewriter.replaceOp(op, packed);
+      Value packed = packLimbs(res_h, res_l, rewriter, loc, concatDimension);
+      rewriter.replaceOp(op, packed);
+    } else {
+      Value final_raw = packLimbs(final_raw_hi, final_raw_lo, rewriter, loc, concatDimension);
+      auto fullType = cast<RankedTensorType>(final_raw.getType());
+      auto predType = RankedTensorType::get(fullType.getShape(), rewriter.getI1Type());
+
+      SmallVector<int64_t> broadcastDims;
+      for (int i = 0; i < fullType.getRank(); ++i) {
+        broadcastDims.push_back(i);
+      }
+      
+      // Broadcast is_neg_quad
+      Value bcast_is_neg_quad = rewriter.create<stablehlo::BroadcastInDimOp>(
+          loc, predType, is_neg_quad, rewriter.getDenseI64ArrayAttr(broadcastDims));
+          
+      Value neg_final = rewriter.create<stablehlo::NegOp>(loc, final_raw);
+      Value final_signed = rewriter.create<stablehlo::SelectOp>(
+          loc, fullType, bcast_is_neg_quad, neg_final, final_raw);
+          
+      // Broadcast lt_zero
+      Value bcast_lt_zero = rewriter.create<stablehlo::BroadcastInDimOp>(
+          loc, predType, lt_zero, rewriter.getDenseI64ArrayAttr(broadcastDims));
+          
+      Value neg_final2 = rewriter.create<stablehlo::NegOp>(loc, final_signed);
+      Value res = rewriter.create<stablehlo::SelectOp>(
+          loc, fullType, bcast_lt_zero, neg_final2, final_signed);
+          
+      rewriter.replaceOp(op, res);
+    }
     return success();
   }
 };
