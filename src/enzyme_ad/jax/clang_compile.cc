ae362f1b5395738fabd66ecf6583193eae969fcd
Antipath1 <62488245+Antipath1@users.noreply.github.com>
2026-04-28 17:17:30 +0000

    Use list instead to prevent segfault

--- src/enzyme_ad/jax/clang_compile.cc
+++ src/enzyme_ad/jax/clang_compile.cc
@@ -20,6 +20,8 @@
 #include <unistd.h>
 #include <vector>
 #include <regex>
+#include <string>
+#include <list>
 
 #include "clang/CodeGen/CodeGenAction.h"
 #include "llvm-c/Core.h"
@@ -106,38 +108,17 @@ using namespace llvm;
 
 class ArgumentList {
 private:
-  /// Helper storage.
-  llvm::SmallVector<llvm::SmallString<0>> Storage;
-  /// List of arguments
+  std::list<std::string> Storage;
   llvm::opt::ArgStringList Args;
 
 public:
-  /// Add argument.
-  ///
-  /// The element stored will not be owned by this.
   void push_back(const char *Arg) { Args.push_back(Arg); }
 
-  /// Add argument and ensure it will be valid before this passer's destruction.
-  ///
-  /// The element stored will be owned by this.
-  /*
-  template <typename... ArgTy> void emplace_back(ArgTy &&...Args) {
-    // Store as a string
-    std::string Buffer;
-    llvm::raw_string_ostream Stream(Buffer);
-    (Stream << ... << Args);
-    emplace_back(llvm::StringRef(Stream.str()));
-  }
-  */
-
-  void emplace_back(llvm::StringRef &&Arg) {
-    push_back(Storage.emplace_back(Arg).c_str());
+  void emplace_back(llvm::StringRef Arg) {
+    Storage.push_back(Arg.str());
+    Args.push_back(Storage.back().c_str());
   }
 
-  /// Return the underling argument list.
-  ///
-  /// The return value of this operation could be invalidated by subsequent
-  /// calls to push_back() or emplace_back().
   llvm::opt::ArgStringList &getArguments() { return Args; }
 };
 
