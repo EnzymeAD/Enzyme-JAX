a6eceff5a71c7b9f7bf76d9219737077b752cb00
Antipath1 <62488245+Antipath1@users.noreply.github.com>
2026-04-28 17:16:43 +0000

    Fix optimize_module and run_pass_pipeline absl stauts handling

--- src/enzyme_ad/jax/enzyme_call.cc
+++ src/enzyme_ad/jax/enzyme_call.cc
@@ -534,33 +534,41 @@ NB_MODULE(enzyme_call, m) {
 
   m.def("optimize_module",
         [](MlirModule cmod, const std::string &pass_pipeline) {
-          run_pass_pipeline(unwrap(cmod), pass_pipeline);
+          absl::Status status = run_pass_pipeline(unwrap(cmod), pass_pipeline);
+          if (!status.ok()) {
+            throw nanobind::value_error(status.ToString().c_str());
+          }
         });
-  m.def("run_pass_pipeline",
-        [](nanobind::object pyoldsyms, const std::string &mlir,
-           const std::string &pass_pipeline) {
-          auto pyargv = pyoldsyms.ptr();
-          std::vector<std::string> oldsyms;
-          assert(PySequence_Check(pyargv));
-          auto sz = PySequence_Size(pyargv);
-          for (Py_ssize_t i = 0; i < sz; ++i) {
-            PyObject *item = PySequence_GetItem(pyargv, i);
+
+  m.def("run_pass_pipeline", [](nanobind::object pyoldsyms,
+                                const std::string &mlir,
+                                const std::string &pass_pipeline) {
+    auto pyargv = pyoldsyms.ptr();
+    std::vector<std::string> oldsyms;
+    assert(PySequence_Check(pyargv));
+    auto sz = PySequence_Size(pyargv);
+    for (Py_ssize_t i = 0; i < sz; ++i) {
+      PyObject *item = PySequence_GetItem(pyargv, i);
 #if PY_VERSION_HEX < 0x03000000
-            auto argv = PyString_AsString(item);
+      auto argv = PyString_AsString(item);
 #else
       auto argv = PyUnicode_AsUTF8(item);
 #endif
-            Py_DECREF(item);
-            assert(argv);
-            oldsyms.emplace_back(argv);
+      Py_DECREF(item);
+      assert(argv);
+      oldsyms.emplace_back(argv);
 #if PY_VERSION_HEX < 0x03000000
-            free(argv);
+      free(argv);
 #else
       // should not free py3+
 #endif
-          }
-          return run_pass_pipeline(oldsyms, mlir, pass_pipeline);
-        });
+    }
+    auto result_or_err = run_pass_pipeline(oldsyms, mlir, pass_pipeline);
+    if (!result_or_err.ok()) {
+      throw nanobind::value_error(result_or_err.status().ToString().c_str());
+    }
+    return result_or_err.value();
+  });
 
   m.def("register_enzymexla_cpu_handler",
         []() { RegisterEnzymeXLACPUHandler(); });
