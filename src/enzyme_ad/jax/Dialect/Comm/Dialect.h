ef5ddec2aa5e6e7d38178e7f252bc64d4e098beb
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-05-08 16:43:27 -0500

    remove Ops.td, Types.td

--- src/enzyme_ad/jax/Dialect/Comm/Dialect.h
+++ src/enzyme_ad/jax/Dialect/Comm/Dialect.h
@@ -5,10 +5,4 @@
 
 #include "src/enzyme_ad/jax/Dialect/Comm/CommDialect.h.inc"
 
-#define GET_TYPEDEF_CLASSES
-#include "src/enzyme_ad/jax/Dialect/Comm/CommTypes.h.inc"
-
-#define GET_OP_CLASSES
-#include "src/enzyme_ad/jax/Dialect/Comm/CommOps.h.inc"
-
 #endif // ENZYMEXLA_COMM_DIALECT_H
