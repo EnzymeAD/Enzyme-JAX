81259276f76cb863f426fccc2fc111055b340f43
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-05-09 07:09:35 -0500

    register types and attrs

--- src/enzyme_ad/jax/Dialect/Comm/Dialect.cpp
+++ src/enzyme_ad/jax/Dialect/Comm/Dialect.cpp
@@ -1,5 +1,27 @@
 #include "Dialect.h"
 
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/DialectImplementation.h"
+
+#include "llvm/ADT/TypeSwitch.h"
+
 #include "src/enzyme_ad/jax/Dialect/Comm/CommDialect.cpp.inc"
 
-void mlir::enzymexla::comm::CommDialect::initialize() {}
+#include "src/enzyme_ad/jax/Dialect/Comm/MPIAttrEnums.cpp.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Comm/MPIAttrDefs.cpp.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Comm/MPITypes.cpp.inc"
+
+void mlir::enzymexla::comm::CommDialect::initialize() {
+  addAttributes<
+#define GET_ATTRDEF_LIST
+#include "src/enzyme_ad/jax/Dialect/Comm/MPIAttrDefs.cpp.inc"
+      >();
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "src/enzyme_ad/jax/Dialect/Comm/MPITypes.cpp.inc"
+      >();
+}
--- src/enzyme_ad/jax/Dialect/Comm/Dialect.h
+++ src/enzyme_ad/jax/Dialect/Comm/Dialect.h
@@ -5,4 +5,12 @@
 
 #include "src/enzyme_ad/jax/Dialect/Comm/CommDialect.h.inc"
 
+#include "src/enzyme_ad/jax/Dialect/Comm/MPIAttrEnums.h.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Comm/MPIAttrDefs.h.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "src/enzyme_ad/jax/Dialect/Comm/MPITypes.h.inc"
+
 #endif // ENZYMEXLA_COMM_DIALECT_H
