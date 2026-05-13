e3278dc99a8a560f47e7db76c6ae9eb029b87842
Billy Moses <wmoses@google.com>
2026-05-13 16:54:29 -0500

    Handle memref copy

--- src/enzyme_ad/jax/Passes/AffineToStableHLORaising.cpp
+++ src/enzyme_ad/jax/Passes/AffineToStableHLORaising.cpp
@@ -46,6 +46,19 @@
 #include <optional>
 
 namespace mlir {
+
+static Block *getAllocaBlock(Operation *op) {
+  Operation *currentOp = op;
+  while (Operation *parentOp = currentOp->getParentOp()) {
+    if (parentOp->mightHaveTrait<OpTrait::IsIsolatedFromAbove>() ||
+        parentOp->mightHaveTrait<OpTrait::AutomaticAllocationScope>()) {
+      return &currentOp->getParentRegion()->front();
+    }
+    currentOp = parentOp;
+  }
+  return nullptr;
+}
+
 namespace enzyme {
 #define GEN_PASS_DEF_AFFINETOSTABLEHLORAISING
 #include "src/enzyme_ad/jax/Passes/Passes.h.inc"
@@ -3453,6 +3466,10 @@ struct AffineToStableHLORaisingPass
                     ValueRange())
                     ->getResult(0);
 
+            Block *allocaBlock = getAllocaBlock(g);
+            assert(allocaBlock && "GPUWrapperOp must be inside an allocation scope");
+            OpBuilder::InsertionGuard guard(b);
+            b.setInsertionPointToStart(allocaBlock);
             auto res0 = memref::AllocaOp::create(
                 b, rewriteLocation(g.getLoc(), options.strip_llvm_debuginfo),
                 MT0);
