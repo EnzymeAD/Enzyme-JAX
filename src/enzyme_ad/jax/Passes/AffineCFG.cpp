2237bfc9e13d0518d8ff0d756d6e4946a5d6775c
xys-syx <xuyuansui@outlook.com>
2026-06-11 18:49:22 -0500

    Fix infinite loop in MoveSIToFPToAffine on non-decomposable operands (#2550)
    * fix infinite loop
    
    * sym == getOperand

--- src/enzyme_ad/jax/Passes/AffineCFG.cpp
+++ src/enzyme_ad/jax/Passes/AffineCFG.cpp
@@ -2251,7 +2251,24 @@ struct MoveSIToFPToAffine : public OpRewritePattern<arith::SIToFPOp> {
     auto *parentScope = scope->getParentOp();
     DominanceInfo DI(parentScope);
 
-    fully2ComposeAffineMapAndOperands(rewriter, &map, &operands, DI, scope);
+    SmallVector<Operation *> insertedOps;
+    fully2ComposeAffineMapAndOperands(rewriter, &map, &operands, DI, scope,
+                                      &insertedOps);
+
+    if (map.getNumResults() == 1 &&
+        map.getResult(0).getKind() == AffineExprKind::SymbolId &&
+        operands.size() == 1) {
+      Value sym = operands[0];
+      while (auto ic = sym.getDefiningOp<arith::IndexCastOp>())
+        sym = ic.getIn();
+      if (sym == ifOp.getOperand()) {
+        for (Operation *op : llvm::reverse(insertedOps))
+          if (op->use_empty())
+            rewriter.eraseOp(op);
+        return failure();
+      }
+    }
+
     affine::canonicalizeMapAndOperands(&map, &operands);
     map = recreateExpr(map);
 
