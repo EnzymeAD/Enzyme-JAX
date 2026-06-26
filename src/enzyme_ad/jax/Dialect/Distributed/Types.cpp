1971ee30770dfde190bca892ab498ced417e63ed
Egan Johnson <egancj2@illinois.edu>
2026-06-26 12:52:26 -0500

    Factor axis to new dialect

--- src/enzyme_ad/jax/Dialect/Distributed/Types.cpp
+++ src/enzyme_ad/jax/Dialect/Distributed/Types.cpp
@@ -1,41 +1,30 @@
 #include "Dialect.h"
 
+#include "src/enzyme_ad/jax/Dialect/Axis/Dialect.h"
+
 #include "llvm/Support/ErrorHandling.h"
 
 namespace mlir::enzyme::distributed {
 
-// Physical-axis alias semantics are intentionally not implemented yet.
-bool PhysicalCommAxisType::aliases(Value selfAxis, Value otherAxis) const {
-  (void)selfAxis;
-  (void)otherAxis;
-  llvm_unreachable(
-      "alias semantics are not implemented for PhysicalCommAxisType");
+bool PhysicalCommAxisType::aliases(Value ax1, Value ax2) const {
+  (void)ax1;
+  (void)ax2;
+  llvm::report_fatal_error(
+      "PhysicalCommAxisType::aliases is not implemented yet");
 }
 
-// Logical-mesh-axis alias semantics are intentionally not implemented yet.
-bool LogicalMeshAxisType::aliases(Value selfAxis, Value otherAxis) const {
-  (void)selfAxis;
-  (void)otherAxis;
-  llvm_unreachable(
-      "alias semantics are not implemented for LogicalMeshAxisType");
+bool LogicalMeshAxisType::aliases(Value ax1, Value ax2) const {
+  (void)ax1;
+  (void)ax2;
+  llvm::report_fatal_error(
+      "LogicalMeshAxisType::aliases is not implemented yet");
 }
 
 // Replication axes are modeled as always disjoint.
-bool ReplicationAxisType::aliases(Value selfAxis, Value otherAxis) const {
-  (void)selfAxis;
-  (void)otherAxis;
+bool ReplicationAxisType::aliases(Value ax1, Value ax2) const {
+  (void)ax1;
+  (void)ax2;
   return false;
-}
-
-// Tensor-axis equivalence is currently based on typed index-space coordinates.
-bool TensorAxisType::aliases(Value selfAxis, Value otherAxis) const {
-  (void)selfAxis;
-  auto other = dyn_cast<TensorAxisType>(otherAxis.getType());
-  assert(other && "TensorAxisType aliasing requires tensor axis values");
-  // TODO: include tensor object/value provenance once index-space identity
-  // is represented beyond the current type payload.
-  return getAxisIndex() == other.getAxisIndex() &&
-         getTensorType() == other.getTensorType();
 }
 
 } // namespace mlir::enzyme::distributed
