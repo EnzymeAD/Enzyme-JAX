0577c7d9b0fbdc44bf543dd7578cd3d3c6f09085
Arjun Vedantham <arjunsv2@illinois.edu>
2026-07-08 03:09:02 -0500

    fix output of SMT check

--- src/enzyme_ad/jax/Passes/Perfify/SimpleCycleAnalysis.cpp
+++ src/enzyme_ad/jax/Passes/Perfify/SimpleCycleAnalysis.cpp
@@ -35,7 +35,7 @@ struct SimpleCycleAnalysisPass
   llvm::DenseMap<mlir::Value, int64_t> args;
   std::unordered_map<HoareStates, int64_t> constant_costs;
   llvm::DenseMap<mlir::StringAttr, mlir::Region *> funcMap;
-  
+
   void runOnOperation() override {
     Operation *op = getOperation();
     mlir::AsmState state(op);
@@ -43,11 +43,12 @@ struct SimpleCycleAnalysisPass
     z3::solver solver(ctx);
     std::vector<z3::expr> cost_var;
     cost_var.push_back(solver.ctx().int_const("cost0"));
-    
+
     visitOperation(op, &state, solver, cost_var);
   }
 
-  void visitOperation(Operation *op, AsmState *state, z3::solver &solver, std::vector<z3::expr> &cost_var) {
+  void visitOperation(Operation *op, AsmState *state, z3::solver &solver,
+                      std::vector<z3::expr> &cost_var) {
     if (auto costOp = dyn_cast<CostOp>(op)) {
       if (!op->getAttrs().empty()) {
         NamedAttribute cost_attr = op->getAttrs()[0];
@@ -110,18 +111,17 @@ struct SimpleCycleAnalysisPass
         auto cost_map_res =
             cost_map.find(region_op->getName().getStringRef().str());
         if (cost_map_res != cost_map.end()) {
-          std::string cost_v = std::to_string(cost_var.size());
-          std::string fmt = cost_str + cost_v;
+          std::string cost_v = cost_str + std::to_string(cost_var.size());
           cost_var.push_back(
-              solver.ctx().int_const(fmt.c_str())); // create a new cost var
+              solver.ctx().int_const(cost_v.c_str())); // create a new cost var
           z3::expr post_cost = cost_var[cost_var.size() - 1];
           z3::expr pre_cost = cost_var[cost_var.size() - 2];
           int64_t cost_val = cost_map_res->second;
           z3::expr cost_expr =
-              (post_cost ==
-               pre_cost + solver.ctx().int_val(cost_val)); // increment per op cost
-          
-          solver.add(cost_expr);                           // add to solver
+              (post_cost == pre_cost + solver.ctx().int_val(
+                                           cost_val)); // increment per op cost
+
+          solver.add(cost_expr); // add to solver
         } else {
           llvm::outs() << "unknown cost for operation "
                        << region_op->getName().getStringRef().str() << "\n";
@@ -133,31 +133,43 @@ struct SimpleCycleAnalysisPass
         auto region_num = cmpOp->getParentRegion()->getRegionNumber();
         auto pre_post_cost =
             constant_costs.find(static_cast<HoareStates>(region_num));
-        z3::expr p = (cost_var[0] ==
-                      solver.ctx().int_val(pre_post_cost->second)); // precondition
-        z3::expr q = (cost_var[cost_var.size() - 1] !=
-                      solver.ctx().int_val(pre_post_cost->second)); // postcondition
+        z3::expr p =
+            (cost_var[0] ==
+             solver.ctx().int_val(pre_post_cost->second)); // precondition
+        z3::expr q =
+            (cost_var[cost_var.size() - 1] !=
+             solver.ctx().int_val(pre_post_cost->second)); // postcondition
         if (region_num == 0) {
           solver.add(p);
         } else {
-          solver.add(q); // safer to add the false?
+          solver.add(q); // if satisfiable -> assignment exists s.t. perf
+                         // counter doesn't equal expected value?
         }
       }
     } else if (auto assumeOp = dyn_cast<AssumeOp>(op)) {
       // todo: trigger traceup from the provided register argument, evaluate or
       // fetch the cmp set up the hoare triple here
-      llvm::outs() << (solver.check() == 1 ? "Met perf check!" : "Did not meet perf check") << "\n";
+      std::cout << solver << std::endl;
+      auto region_num = assumeOp->getParentRegion()->getRegionNumber();
+      auto check_res = solver.check();
+      llvm::outs() << (((region_num == 0 && check_res == 1) ||
+                        (region_num != 0 && check_res == 0))
+                           ? "Met perf check!"
+                           : "Did not meet perf check")
+                   << "\n"; // this should always be true
     }
     for (Region &region : op->getRegions())
       visitRegion(region, state, solver, cost_var);
   }
 
-  void visitRegion(Region &region, AsmState *state, z3::solver &solver, std::vector<z3::expr> &cost_var) {
+  void visitRegion(Region &region, AsmState *state, z3::solver &solver,
+                   std::vector<z3::expr> &cost_var) {
     for (Block &block : region.getBlocks())
       visitBlock(block, state, solver, cost_var);
   }
 
-  void visitBlock(Block &block, AsmState *state, z3::solver &solver, std::vector<z3::expr> &cost_var) {
+  void visitBlock(Block &block, AsmState *state, z3::solver &solver,
+                  std::vector<z3::expr> &cost_var) {
     for (Operation &op : block.getOperations())
       visitOperation(&op, state, solver, cost_var);
   }
