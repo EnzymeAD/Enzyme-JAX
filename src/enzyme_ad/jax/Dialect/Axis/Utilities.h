3eb76281a482deb362ec13f2713aeaca2f00b031
Egan Johnson <egancj2@illinois.edu>
2026-07-15 13:33:39 -0500

    Collective  parsing (verifier failure)

--- src/enzyme_ad/jax/Dialect/Axis/Utilities.h
+++ src/enzyme_ad/jax/Dialect/Axis/Utilities.h
@@ -6,13 +6,13 @@
 namespace mlir::enzyme::axis {
 
 // Returns the static extent for any canonical axis SSA value.
-int getAxisExtent(::mlir::Value axis);
+int getAxisExtent(::mlir::TypedValue<AxisTypeInterface> axis);
 
 // Returns the static extent for any factor SSA value.
-int getFactorExtent(::mlir::Value factor);
+int getFactorExtent(::mlir::TypedValue<AxisFactorType> factor);
 
 // Returns the static extent for any segment SSA value.
-int getSegmentExtent(::mlir::Value segment);
+int getSegmentExtent(::mlir::TypedValue<AxisSegmentType> segment);
 
 // Returns the defining op for a canonical axis SSA value.
 ::mlir::FailureOr<::mlir::Operation *> getAxisProvenanceOp(::mlir::Value axis);
@@ -28,6 +28,10 @@ getSegmentProvenanceAxis(::mlir::TypedValue<AxisSegmentType> segment);
 // Returns the factor list used to build a factor-product SSA value.
 ::mlir::FailureOr<::mlir::ValueRange>
 getProductProvenanceFactors(::mlir::TypedValue<FactorGroupType> factorProduct);
+
+// Returns the product of extents for a factor-product SSA value.
+::mlir::FailureOr<uint64_t>
+getFactorGroupExtent(::mlir::TypedValue<FactorGroupType> factorProduct);
 
 // Checks that factors are pairwise non-overlapping for one source axis.
 bool arePairwiseFactorsDisjoint(::mlir::Value lhsFactor,
@@ -59,6 +63,23 @@ bool areFactorsComplete(::mlir::Value axis, ::mlir::ValueRange factors);
 // Checks that segments exactly cover [0, axis extent) with no overlap or gaps.
 bool areSegmentsComplete(::mlir::Value axis, ::mlir::ValueRange segments);
 
+// Small utlity for extracting all factors from one or more factor groups.
+llvm::SmallVector<::mlir::Value>
+flattenGroupsToFactors(::mlir::ValueRange factorGroups);
+
+// Shortcut for calling distjoint on a flattened factor list
+bool areFactorGroupsDisjoint(::mlir::ValueRange factorGroups);
+
+// Given a shapeType with a known rank, returns a list of canonical axes for
+// each dimension of that shape. Use a builder without an insertion point and
+// an unknown location for ephemeral axes.
+llvm::SmallVector<::mlir::Value>
+createAxesForRankedShape(::mlir::Type shapeType, ::mlir::OpBuilder &builder,
+                         ::mlir::Location loc);
+// Creates a single factor for each axis, with full extent and stride 1.
+llvm::SmallVector<::mlir::Value> viewAxesAsFactors(::mlir::ValueRange axes,
+                                                   ::mlir::OpBuilder &builder,
+                                                   ::mlir::Location loc);
 } // namespace mlir::enzyme::axis
 
 #endif // ENZYME_AD_JAX_DIALECT_AXIS_UTILITIES_H
--- src/enzyme_ad/jax/Dialect/Distributed/Types.cpp
+++ src/enzyme_ad/jax/Dialect/Distributed/Types.cpp
@@ -14,10 +14,15 @@ bool PhysicalCommAxisType::aliases(Value ax1, Value ax2) const {
 }
 
 bool LogicalMeshAxisType::aliases(Value ax1, Value ax2) const {
-  (void)ax1;
-  (void)ax2;
-  llvm::report_fatal_error(
-      "LogicalMeshAxisType::aliases is not implemented yet");
+  // alias iff they are the same result of the same op
+  auto result1 = dyn_cast<OpResult>(ax1);
+  auto result2 = dyn_cast<OpResult>(ax2);
+  if (!result1 || !result2) {
+    llvm::report_fatal_error(
+        "LogicalMeshAxisType::aliases requires both axes to be OpResults");
+  }
+  return result1.getOwner() == result2.getOwner() &&
+         result1.getResultNumber() == result2.getResultNumber();
 }
 
 // Replication axes are modeled as always disjoint.
