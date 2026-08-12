5c916cd57f6b8e279ef83a41987cc93f76875f86
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-08-12 08:21:39 -0500

    export C functions

--- src/enzyme_ad/jax/xla_ffi/host/mpi_ffi.h
+++ src/enzyme_ad/jax/xla_ffi/host/mpi_ffi.h
@@ -1,16 +1,18 @@
 #pragma once
 
-extern "C" void enzymexla_set_mpi_comm_rank(void *);
-extern "C" void enzymexla_set_mpi_comm_size(void *);
-extern "C" void enzymexla_set_mpi_comm_split(void *);
-extern "C" void enzymexla_set_mpi_barrier(void *);
-extern "C" void enzymexla_set_mpi_send(void *);
-extern "C" void enzymexla_set_mpi_isend(void *);
-extern "C" void enzymexla_set_mpi_recv(void *);
-extern "C" void enzymexla_set_mpi_irecv(void *);
-extern "C" void enzymexla_set_mpi_wait(void *);
-extern "C" void enzymexla_set_mpi_waitall(void *);
-extern "C" void enzymexla_set_mpi_allreduce(void *);
-extern "C" void enzymexla_set_mpi_bcast(void *);
+#include "../export_macro.h"
 
-extern "C" size_t enzymexla_set_mpi_status_size();
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_comm_rank(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_comm_size(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_comm_split(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_barrier(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_send(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_isend(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_recv(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_irecv(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_wait(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_waitall(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_allreduce(void *);
+extern "C" MLIR_CAPI_EXPORTED void enzymexla_set_mpi_bcast(void *);
+
+extern "C" MLIR_CAPI_EXPORTED size_t enzymexla_set_mpi_status_size();
