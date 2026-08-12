b19e6a0f7999b26320d547da8d3a47f23e6be012
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-08-12 03:43:20 -0500

    inline `extern "C"`

--- src/enzyme_ad/jax/xla_ffi/host/mpi_ffi.h
+++ src/enzyme_ad/jax/xla_ffi/host/mpi_ffi.h
@@ -1,22 +1,14 @@
 #pragma once
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-void enzymexla_set_mpi_comm_rank(void *);
-void enzymexla_set_mpi_comm_size(void *);
-void enzymexla_set_mpi_comm_split(void *);
-void enzymexla_set_mpi_barrier(void *);
-void enzymexla_set_mpi_send(void *);
-void enzymexla_set_mpi_isend(void *);
-void enzymexla_set_mpi_recv(void *);
-void enzymexla_set_mpi_irecv(void *);
-void enzymexla_set_mpi_wait(void *);
-void enzymexla_set_mpi_waitall(void *);
-void enzymexla_set_mpi_allreduce(void *);
-void enzymexla_set_mpi_bcast(void *);
-
-#if defined(__cplusplus)
-}
-#endif
+extern "C" void enzymexla_set_mpi_comm_rank(void *);
+extern "C" void enzymexla_set_mpi_comm_size(void *);
+extern "C" void enzymexla_set_mpi_comm_split(void *);
+extern "C" void enzymexla_set_mpi_barrier(void *);
+extern "C" void enzymexla_set_mpi_send(void *);
+extern "C" void enzymexla_set_mpi_isend(void *);
+extern "C" void enzymexla_set_mpi_recv(void *);
+extern "C" void enzymexla_set_mpi_irecv(void *);
+extern "C" void enzymexla_set_mpi_wait(void *);
+extern "C" void enzymexla_set_mpi_waitall(void *);
+extern "C" void enzymexla_set_mpi_allreduce(void *);
+extern "C" void enzymexla_set_mpi_bcast(void *);
