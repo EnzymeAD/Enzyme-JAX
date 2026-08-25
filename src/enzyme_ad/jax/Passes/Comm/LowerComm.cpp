df6ff5c3e3115595eb8bf69f71742745ce339dbe
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-08-25 18:32:35 -0500

    WIP implement comm lowering pass to stablehlo

--- src/enzyme_ad/jax/Passes/Comm/LowerComm.cpp
+++ src/enzyme_ad/jax/Passes/Comm/LowerComm.cpp
@@ -1,6 +1,10 @@
+#include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
+#include "mlir/Conversion/LLVMCommon/TypeConverter.h"
 #include "src/enzyme_ad/jax/Dialect/Comm/Dialect.h"
 #include "src/enzyme_ad/jax/Dialect/Comm/Ops.h"
-#include "src/enzyme_ad/jax/Passes/Passes.h"
+#include "src/enzyme_ad/jax/Passes/Comm/Passes.h"
+// #include "stablehlo/dialect/Base.h"
+#include "stablehlo/dialect/StablehloOps.h"
 
 namespace mlir::comm {
 #define GEN_PASS_DEF_LOWERCOMMTOSTABLEHLOPASS
@@ -9,17 +13,321 @@ namespace mlir::comm {
 
 using namespace mlir;
 
-struct LowerCommPass
-    : public mlir::comm::impl::LowerCommPassBase<LowerCommPass> {
+struct CommMpiCommRankOpLowering
+    : public OpConversionPattern<comm::MpiCommRankOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiCommRankOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+    const TypeConverter *converter = getTypeConverter();
+
+    SmallVector<Type> converted_res_types;
+    if (failed(converter->convertTypes(op->getResultTypes(),
+                                       converted_res_types))) {
+      return failure();
+    }
+
+    SmallVector<Type> converted_arg_types;
+    if (failed(converter->convertTypes(op->getOperandTypes(),
+                                       converted_arg_types))) {
+      return failure();
+    }
+
+    // Replace the MpiCommRankOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, converted_res_types,
+        rewriter.getStringAttr("enzymexla_ffi_mpi_comm_rank"),
+        ValueRange{op.getComm()},
+        /*has_side_effect=*/false);
+
+    return success();
+  }
+};
+
+struct CommMpiCommSizeOpLowering
+    : public OpConversionPattern<comm::MpiCommSizeOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiCommSizeOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+    const TypeConverter *converter = getTypeConverter();
+
+    SmallVector<Type> converted_res_types;
+    if (failed(converter->convertTypes(op->getResultTypes(),
+                                       converted_res_types))) {
+      return failure();
+    }
+
+    SmallVector<Type> converted_arg_types;
+    if (failed(converter->convertTypes(op->getOperandTypes(),
+                                       converted_arg_types))) {
+      return failure();
+    }
+
+    // Replace the MpiCommSizeOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, converted_res_types,
+        rewriter.getStringAttr("enzymexla_ffi_mpi_comm_size"),
+        ValueRange{op.getComm()},
+        /*has_side_effect=*/false);
+
+    return success();
+  }
+};
+
+struct CommMpiCommSplitOpLowering
+    : public OpConversionPattern<comm::MpiCommSplitOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiCommSplitOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+    const TypeConverter *converter = getTypeConverter();
+
+    SmallVector<Type> converted_res_types;
+    if (failed(converter->convertTypes(op->getResultTypes(),
+                                       converted_res_types))) {
+      return failure();
+    }
+
+    SmallVector<Type> converted_arg_types;
+    if (failed(converter->convertTypes(op->getOperandTypes(),
+                                       converted_arg_types))) {
+      return failure();
+    }
+
+    // Replace the MpiCommSplitOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, converted_res_types,
+        rewriter.getStringAttr("enzymexla_ffi_mpi_comm_split"),
+        ValueRange{op.getComm(), op.getColor(), op.getKey()},
+        /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiBarrierOpLowering
+    : public OpConversionPattern<comm::MpiBarrierOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiBarrierOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiBarrierOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_barrier"),
+        ValueRange{op.getComm()}, /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiSendOpLowering : public OpConversionPattern<comm::MpiSendOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiSendOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiSendOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_send"),
+        ValueRange{op.getBuffer(), op.getDest(), op.getTag(), op.getComm()},
+        /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiIsendOpLowering : public OpConversionPattern<comm::MpiIsendOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiIsendOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiIsendOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_isend"),
+        ValueRange{op.getBuffer(), op.getDest(), op.getTag(), op.getComm()},
+        /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiRecvOpLowering : public OpConversionPattern<comm::MpiRecvOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiRecvOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiRecvOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_recv"),
+        ValueRange{op.getBuffer(), op.getSource(), op.getTag(), op.getComm()},
+        /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiIrecvOpLowering : public OpConversionPattern<comm::MpiIrecvOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiIrecvOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiIrecvOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_irecv"),
+        ValueRange{op.getBuffer(), op.getSource(), op.getTag(), op.getComm()},
+        /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiWaitOpLowering : public OpConversionPattern<comm::MpiWaitOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiWaitOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiWaitOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_wait"),
+        ValueRange{op.getRequest()}, /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct CommMpiWaitallOpLowering
+    : public OpConversionPattern<comm::MpiWaitallOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiWaitallOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiWaitallOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_waitall"),
+        ValueRange{op.getRequests()}, /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+// TODO
+// struct CommMpiAllreduceOpLowering
+//     : public OpConversionPattern<comm::MpiAllreduceOp> {
+//   using OpConversionPattern::OpConversionPattern;
+
+//   LogicalResult
+//   matchAndRewrite(comm::MpiAllreduceOp op, OpAdaptor adaptor,
+//                   ConversionPatternRewriter &rewriter) const override {
+//     auto context = op->getContext();
+
+//     // Replace the MpiAllreduceOp with a stablehlo.custom_call operation
+//     rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+//         op, TypeRange{},
+//         rewriter.getStringAttr("enzymexla_ffi_mpi_allreduce"),
+//         ValueRange{op.getSendbuf(), op.getReduceOp(), op.getComm()},
+//         /*has_side_effect=*/true);
+
+//     return success();
+//   }
+// };
+
+struct CommMpiBcastOpLowering : public OpConversionPattern<comm::MpiBcastOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiBcastOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+
+    // Replace the MpiBcastOp with a stablehlo.custom_call operation
+    rewriter.replaceOpWithNewOp<stablehlo::CustomCallOp>(
+        op, TypeRange{}, rewriter.getStringAttr("enzymexla_ffi_mpi_bcast"),
+        ValueRange{op.getInBuffer(), op.getRoot(), op.getComm()},
+        /*has_side_effect=*/true);
+
+    return success();
+  }
+};
+
+struct LowerCommToStablehloPass
+    : public mlir::comm::impl::LowerCommToStablehloPassBase<
+          LowerCommToStablehloPass> {
   using Base::Base;
 
   void runOnOperation() override {
     auto *context = getOperation()->getContext();
 
-    RewritePatternSet patterns(context);
+    ConversionTarget target(*context);
+    target.addLegalDialect<stablehlo::StablehloDialect>();
 
-    if (failed(applyPatternsAndFoldGreedily(getOperation(),
-                                            std::move(patterns)))) {
+    target.addIllegalOp<comm::MpiCommRankOp>();
+    target.addIllegalOp<comm::MpiCommSizeOp>();
+    target.addIllegalOp<comm::MpiCommSplitOp>();
+    target.addIllegalOp<comm::MpiBarrierOp>();
+    target.addIllegalOp<comm::MpiSendOp>();
+    target.addIllegalOp<comm::MpiIsendOp>();
+    target.addIllegalOp<comm::MpiRecvOp>();
+    target.addIllegalOp<comm::MpiIrecvOp>();
+    target.addIllegalOp<comm::MpiWaitOp>();
+    target.addIllegalOp<comm::MpiWaitallOp>();
+    target.addIllegalOp<comm::MpiAllreduceOp>();
+    target.addIllegalOp<comm::MpiBcastOp>();
+
+    TypeConverter converter;
+
+    // !comm.mpi.comm, !comm.mpi.request are pointer-like, so lower to
+    // tensor<i64>
+    auto ptr_tensor_type =
+        RankedTensorType::get({}, IntegerType::get(context, 64));
+    converter.addConversion([](comm::MpiComm type) { return ptr_tensor_type; });
+    converter.addConversion(
+        [](comm::MpiRequest type) { return ptr_tensor_type; });
+
+    // !comm.mpi.status is a opaque type, so lower to tensor<MPI_STATUS_SIZExi8>
+    converter.addConversion([](comm::MpiStatus type) {
+      return RankedTensorType::get({MPI_STATUS_SIZE},
+                                   IntegerType::get(type.getContext(), 8));
+    });
+
+    // lower comm.mpi ops to stablehlo.custom_call ops
+    RewritePatternSet patterns(context);
+    patterns.add<CommMpiCommRankOpLowering, CommMpiCommSizeOpLowering,
+                 CommMpiCommSplitOpLowering, CommMpiBarrierOpLowering,
+                 CommMpiSendOpLowering, CommMpiIsendOpLowering,
+                 CommMpiRecvOpLowering, CommMpiIrecvOpLowering,
+                 CommMpiWaitOpLowering, CommMpiWaitallOpLowering,
+                 /* CommMpiAllreduceOpLowering, */ CommMpiBcastOpLowering>(
+        converter, context);
+
+    if (failed(
+            applyFullConversion(getOperation(), target, std::move(patterns)))) {
       signalPassFailure();
     }
   }
