69d954f562b90c477388b0139ba0f91a72a2afa3
Egan Johnson <egancj2@illinois.edu>
2026-09-07 12:23:35 -0500

    Hopefully efficient decision replay

---
+++ src/enzyme_ad/jax/Passes/Distributed/ReplayTree.h
@@ -0,0 +1,91 @@
+#ifndef ENZYME_AD_JAX_PASSES_DISTRIBUTED_REPLAYTREE_H
+#define ENZYME_AD_JAX_PASSES_DISTRIBUTED_REPLAYTREE_H
+
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+namespace mlir::enzyme::distributed {
+enum ReplayQueryShortCircuit { Continue, Stop };
+
+/**
+ * For our search, we need to query a list of decisions at each leaf,
+ * and we need to copy leaves / grow the tree. If we do this nievely by:
+ *  - Replaying the decision list from root to leaf: O(n) traversal time
+ *    for each leaf
+ *  - Storing the complete state at each leaf: O(n) size per leaf,
+ *    O(n) time to copy
+ * So extending down from root in a linear chain in either approach would take
+ * O(n^2) time. (Worse if we are expanding a full tree, of course).
+ *
+ * We can do a hybrid checkpoint approach, though, where instead of depth D
+ * pointing to depth (D -1), we point to the parent differing only by taking the
+ * lowest-significance 1 bit and replacing it with a 0: i.e. a chain might go
+ * 0000 <-- 0100 <-- 0101, with a hop here compressing 4 and 1 steps
+ * respectively. This should give an overall traversal time of O(log(n)) "hops"
+ * and an ammortized O(1) (?) construction time for each leaf.
+ *
+ * However, despite reducing the number of hops, the overall decisions stored
+ * in the chain is still the same (in our 0101 example, we still walked over
+ * deltas compressing 5 steps). So this only gets us any benefit if we can
+ * internally do better than O(n) operations within each hop. An O(1) map
+ * lookup, for example, gets us an O(log(n)) query over the entire replay;
+ * an O(n) operation still creates an O(n) query over the entire replay.
+ *
+ * For this structure we require a DeltaType with an associative "apply"
+ * operation allowing one delta to be applied to another. Empty-construting this
+ * delta should give us an identity element.
+ */
+template <typename DeltaType> class ReplayTree {
+protected:
+  using SelfT = ReplayTree<DeltaType>;
+  DeltaType delta;
+  std::shared_ptr<const SelfT> parent;
+  uint32_t depth;
+
+  ReplayTree(std::shared_ptr<SelfT> predecessor, uint32_t depth)
+      : depth(depth), DeltaType() {
+    int stepsUp = llvm::countr_zero(depth);
+    std::vector<std::shared_ptr<SelfT>> toCompress;
+    for (int i = 0; i < stepsUp; ++i) {
+      toCompress.push_back(predecessor);
+      predecessor = predecessor->parent;
+    }
+    parent = predecessor;
+    for (auto it = toCompress.rbegin(); it != toCompress.rend(); ++it) {
+      // Replay onto the delta, in original order
+      delta.apply((*it)->delta);
+    }
+  }
+
+public:
+  // Define the necessary members and methods for the ReplayTree here.
+  static std::shared_ptr<SelfT> makeRoot() {
+    return std::make_shared<SelfT>(nullptr, 0);
+  }
+  static std::shared_ptr<SelfT> makeChild(std::shared_ptr<SelfT> predecessor) {
+    return std::make_shared<SelfT>(predecessor, predecessor->depth + 1);
+  }
+
+  // Any mutating operations to the replay tree can be done
+  // on this getDelta reference.
+  DeltaType &getDelta() { return delta; }
+
+  // Currently return-by-reference or state on the query object.
+  // Query must return a short-circuit status indicating whether to
+  // continue or stop, and should be associative and commutative with
+  // respect to the delta state. (Currently we look at the latest delta first)
+  template <typename Query, typename... Args>
+  void queryReplay(Query q, Args... args) const {
+    ReplayQueryShortCircuit status = q((const DeltaType)delta, args...);
+    if (status == Stop) {
+      return;
+    }
+    // Continue querying the parent if it exists
+    if (parent) {
+      parent->queryReplay(q, args...);
+    }
+  }
+};
+} // namespace mlir::enzyme::distributed
+#endif // ENZYME_AD_JAX_PASSES_DISTRIBUTED_REPLAYTREE_H
--- src/enzyme_ad/jax/Passes/Distributed/SearchStrategies.cpp
+++ src/enzyme_ad/jax/Passes/Distributed/SearchStrategies.cpp
@@ -1,21 +1,144 @@
 #include "src/enzyme_ad/jax/Passes/Distributed/Passes.h"
+#include "src/enzyme_ad/jax/Passes/Distributed/ReplayTree.h"
 
 #include "src/enzyme_ad/jax/Dialect/Axis/Utilities.h"
 #include "src/enzyme_ad/jax/Dialect/Distributed/Utilities.h"
 
+#include <memory>
+
 namespace mlir::enzyme::distributed {
+using namespace mlir::enzyme::axis;
 
 #define GEN_PASS_DEF_DISTRIBUTEDSEARCHSTRATEGIESPASS
 #include "src/enzyme_ad/jax/Passes/Distributed/Passes.h.inc"
 
+/**
+ * Plan for this pass:
+ * To discover efficient distribution plans, search over mappings from logical
+ * axes to either physical device axes, temporal pipelines (TODO), or
+ * within-device axes (i.e. serialization, though we don't care if the device
+ * chooses to internally parallelize.) We use beam search for this:
+ *  - Ahead of search, freeze an order of axes to decide. This deduplicates
+ *    search effort, since different decision orders lead to the same result.
+ *    This should attempt to place "expensive" / impactful axes earlier in the
+ *    order heuristically.
+ *  - For an axis being decided, make decisions for subfactors at a time rather
+ *    than the whole axis at once, allowing hybrid strategies. Exception:
+ *    serialization decisions should take the entire remaining axis at once.
+ *  - Decide factors from outermost / least contiguous to innermost. This means
+ *    that we serialize local chunks of tensors. (Not a hard invariant: reshapes
+ *    can introduce strides)
+ *  - Currently doing fixed budget beam, may change in the future
+ *  - TODO: figure out how we can avoid invoking the device cost model for
+ *    identical / unchanged kernels. Should only need to recalculate kernels
+ *    and collectives affected by the latest decision.
+ *  - To avoid needing to mantain multiple modules (including IR mapping
+ *    bookeeping) we maintain a single module and replay decisions onto it.
+ *    during each lowering pass.
+ */
 namespace {
+
+// Attempt to hybridize replaying decision lists (O(n) per step, so O(n^2))
+// vs storing complete state at leaves (O(n) size per leaf, O(n) time to copy)
+// to a hybrid approach that ammortizes both to a (hopefully) more efficient
+// strategy.
+
+struct LogicalAxisWorklist {
+  // SSA value of the next logical axis to rewrite
+  TypedValue<LogicalMeshAxisType> axis;
+  std::shared_ptr<LogicalAxisWorklist> next;
+
+  static std::shared_ptr<LogicalAxisWorklist>
+  fromArray(const llvm::ArrayRef<TypedValue<LogicalMeshAxisType>> &axes) {
+    std::shared_ptr<LogicalAxisWorklist> current = nullptr;
+    for (auto it = axes.rbegin(); it != axes.rend(); ++it) {
+      auto node = std::make_shared<LogicalAxisWorklist>();
+      node->axis = *it;
+      node->next = current;
+      current = node;
+    }
+    return current;
+  }
+};
+
+// Collects all logical axes with users in the given module.
+static std::vector<TypedValue<LogicalMeshAxisType>>
+findAllLogicalAxes(ModuleOp moduleOp) {
+  std::vector<TypedValue<LogicalMeshAxisType>> logicalAxes;
+  moduleOp.walk([&](LogicalMeshAxesOp axesOp) {
+    // Check dead axes have been cleaned up so we don't have to search over them
+    for (Value axis : axesOp.getAxes()) {
+      bool hasUsers = !axis.use_empty();
+#ifndef NDEBUG
+      static bool remarked = false;
+      if (!remarked && !hasUsers) {
+        axesOp.emitWarning("Skipping dead logical axis: will unnecessarily "
+                           "increase search space. Skipping future remarks.");
+        remarked = true;
+      }
+#endif
+      if (hasUsers)
+        logicalAxes.push_back(cast<TypedValue<LogicalMeshAxisType>>(axis));
+    }
+  });
+  return logicalAxes;
+}
+
+struct AxisMappingReplayState {
+  llvm::DenseMap<TypedValue<LogicalMeshAxisType>,
+                 std::vector<TypedValue<AxisFactorType>>>
+      axisMapping;
+  void apply(const AxisMappingReplayState &other) {
+    for (auto &entry : other.axisMapping) {
+      auto &vec = axisMapping[entry.first];
+      vec.insert(vec.end(), entry.second.begin(), entry.second.end());
+    }
+  }
+};
+
+class AxisMappingReplayTree
+    : public mlir::enzyme::distributed::ReplayTree<AxisMappingReplayState> {
+public:
+  llvm::SmallVector<TypedValue<AxisFactorType>>
+  getBindings(TypedValue<LogicalMeshAxisType> axis) {
+    struct LookupOperator {
+      ReplayQueryShortCircuit
+      operator()(const AxisMappingReplayState &state,
+                 TypedValue<LogicalMeshAxisType> axis,
+                 llvm::SmallVector<TypedValue<AxisFactorType>> &result) {
+        auto it = state.axisMapping.find(axis);
+        if (it != state.axisMapping.end()) {
+          result.insert(result.end(), it->second.begin(), it->second.end());
+        }
+        return mlir::enzyme::distributed::Continue;
+      }
+    };
+    LookupOperator lookup;
+    llvm::SmallVector<TypedValue<AxisFactorType>> result;
+    queryReplay(lookup, axis, result);
+    return result;
+  }
+};
+
+struct StrategySearchNode {
+  std::shared_ptr<LogicalAxisWorklist> worklist;
+  ModuleOp searchedModule;
+  mlir::IRMapping moduleToOriginal;
+  mlir::IRMapping originalToModule;
+
+  ~StrategySearchNode() { searchedModule.erase(); }
+};
+
 struct DistributedSearchStrategiesPass
     : public impl::DistributedSearchStrategiesPassBase<
           DistributedSearchStrategiesPass> {
 
   void runOnOperation() override {
     ModuleOp moduleOp = getOperation();
-    (void)moduleOp;
+
+    auto logicalAxes = findAllLogicalAxes(moduleOp);
+    // TODO: order by importance
+    auto worklist = LogicalAxisWorklist::fromArray(logicalAxes);
   }
 };
 
