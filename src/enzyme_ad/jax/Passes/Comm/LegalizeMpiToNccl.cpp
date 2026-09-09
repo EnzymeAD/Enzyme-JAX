ead7d85808aee078b07492deacd549e93871d695
Roman Lee <rplee7@gmail.com>
2026-09-08 20:25:48 -0500

    sketch out runOnOperation

--- src/enzyme_ad/jax/Passes/Comm/LegalizeMpiToNccl.cpp
+++ src/enzyme_ad/jax/Passes/Comm/LegalizeMpiToNccl.cpp
@@ -12,16 +12,60 @@
 #include "stablehlo/dialect/StablehloOps.h"
 
 namespace mlir::comm {
-#define GEN_PASS_DEF_LEGALIZECOMMMPITONCCLPASS
+#define GEN_PASS_DEF_LEGALIZEMPITONCCLPASS
 #include "src/enzyme_ad/jax/Passes/Comm/Passes.h.inc"
 } // namespace mlir::comm
 
 using namespace mlir;
 
-struct LegalizeCommMpiToNcclPass
-    : public comm::impl::LegalizeCommMpiToNcclPassBase<LegalizeCommMpiToNcclPass> {
+struct LegalizeMpiCommRankOpToNccl
+    : public OpConversionPattern<comm::MpiCommRankOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(comm::MpiCommRankOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    auto context = op->getContext();
+  }
+}; // struct LegalizeMpiCommRankOpToNccl
+
+
+struct LegalizeMpiToNcclPass
+    : public comm::impl::LegalizeMpiToNcclPassBase<LegalizeMpiToNcclPass> {
   using Base::Base;
 
   void runOnOperation() override {
+    auto *context = getOperation()->getContext();
+
+    ConversionTarget target(*context);
+
+    target.addLegalDialect<stablehlo::StablehloDialect,
+                           enzymexla::EnzymeXLADialect,
+                           mlir::LLVM::LLVMDialect>();
+    target.addIllegalDialect<comm::CommDialect>();
+    target.addLegalOp<comm::NcclCommSplitOp, comm::NcclCommFinalizeOp,
+                      comm::NcclCommDestroyOp, comm::NcclCommAbortOp,
+                      comm::NcclCommCountOp, comm::NcclCommCuDeviceOp,
+                      comm::NcclCommUserRankOp, comm::NcclAllReduceOp,
+                      comm::NcclBroadcastOp, comm::NcclSendOp,
+                      comm::NcclRecvOp>();
+
+    comm::MpiToNcclTypeConverter converter;
+
+    target.addDynamicallyLegalOp<func::FuncOp>([&](func::FuncOp op) {
+      return converter.isSignatureLegal(op.getFunctionType());
+    });
+
+    RewritePatternSet patterns(context);
+
+    mlir::populateFunctionOpInterfaceTypeConversionPattern<func::FuncOp>(
+        patterns, converter);
+
+    patterns.add<LegalizeMpiCommRankOpToNccl>(converter, context);
+
+    if (failed(applyPartialConversion(getOperation(), target,
+                                      std::move(patterns)))) {
+      signalPassFailure();
+    }
   }
 };
