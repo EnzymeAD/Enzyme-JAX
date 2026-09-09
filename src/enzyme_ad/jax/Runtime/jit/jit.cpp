50b6742c91b45a24bc25fb52d76002635e5f1768
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-09-09 18:40:34 -0500

    Revert "fix `enzymexla::lookup_symbol`"
    This reverts commit eba54eb7f95cbde051a0b5e692e44983e9b24ec6.

--- src/enzyme_ad/jax/Runtime/jit/jit.cpp
+++ src/enzyme_ad/jax/Runtime/jit/jit.cpp
@@ -121,10 +121,10 @@ llvm::Expected<void *> enzymexla::lookup_symbol(const char *name) {
     return llvm::make_error<llvm::StringError>("JIT not initialized",
                                                llvm::inconvertibleErrorCode());
 
-  auto mangled_name = JIT->mangleAndIntern(name);
-  if (!MappedSymbols.contains(mangled_name))
+  auto addr = JIT->lookup(name);
+  if (!addr)
     return llvm::make_error<llvm::StringError>("Symbol not found",
                                                llvm::inconvertibleErrorCode());
 
-  return MappedSymbols[mangled_name].toPtr<void *>();
+  return addr->toPtr<void *>();
 }
