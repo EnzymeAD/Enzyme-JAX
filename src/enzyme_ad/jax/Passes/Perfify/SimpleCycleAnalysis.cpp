f3351b812cff66b988b1847c90486b706c418586
Arjun Vedantham <arjunsv2@illinois.edu>
2026-09-25 17:51:52 -0500

    Parse cost operations as regions, add symbolic/polynomial costs

--- src/enzyme_ad/jax/Passes/Perfify/SimpleCycleAnalysis.cpp
+++ src/enzyme_ad/jax/Passes/Perfify/SimpleCycleAnalysis.cpp
@@ -1,3 +1,4 @@
+#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/AsmState.h"
@@ -8,6 +9,7 @@
 #include "z3++.h"
 #include <cstdint>
 #include <iostream>
+#include <optional>
 #include <string>
 namespace mlir {
 namespace enzyme {
@@ -31,9 +33,9 @@ struct SimpleCycleAnalysisPass
           SimpleCycleAnalysisPass> {
   using SimpleCycleAnalysisPassBase::SimpleCycleAnalysisPassBase;
   mlir::Region *analysis_func;
-  std::unordered_map<std::string, int64_t> cost_map;
+  std::unordered_map<std::string, mlir::Region *> cost_map;
   llvm::DenseMap<mlir::Value, int64_t> args;
-  std::unordered_map<HoareStates, int64_t> constant_costs;
+  std::unordered_map<HoareStates, mlir::Region *> constant_costs;
   llvm::DenseMap<mlir::StringAttr, mlir::Region *> funcMap;
 
   void runOnOperation() override {
@@ -50,30 +52,15 @@ struct SimpleCycleAnalysisPass
   void visitOperation(Operation *op, AsmState *state, z3::solver &solver,
                       std::vector<z3::expr> &cost_var) {
     if (auto costOp = dyn_cast<CostOp>(op)) {
-      if (!op->getAttrs().empty()) {
-        NamedAttribute cost_attr = op->getAttrs()[0];
-        NamedAttribute op_attr = op->getAttrs()[1];
-        if (auto intAttr =
-                mlir::dyn_cast<mlir::IntegerAttr>(cost_attr.getValue())) {
-          if (auto strAttr =
-                  mlir::dyn_cast<mlir::StringAttr>(op_attr.getValue())) {
-            int64_t value = intAttr.getInt();
-            if (cost_map.find(strAttr.getValue().str()) == cost_map.end()) {
-              cost_map[strAttr.getValue().str()] = value;
-            }
-          }
-        }
+      std::string target_op = costOp.getTargetOp().str();
+      if (cost_map.find(target_op) == cost_map.end()) {
+        cost_map[target_op] =
+            &costOp.getBody(); // store the body of the region for eval
       }
     } else if (auto con_cost = dyn_cast<ConstantCostOp>(op)) {
-      if (!op->getAttrs().empty()) {
-        NamedAttribute est_cost = op->getAttrs()[0];
-        if (auto intAttr =
-                mlir::dyn_cast<mlir::IntegerAttr>(est_cost.getValue())) {
-          HoareStates pre_post = static_cast<HoareStates>(
-              con_cost->getParentRegion()->getRegionNumber());
-          constant_costs.insert({pre_post, intAttr.getInt()});
-        }
-      }
+      HoareStates pre_post = static_cast<HoareStates>(
+          con_cost->getParentRegion()->getRegionNumber());
+      constant_costs.insert({pre_post, &con_cost.getBody()});
     } else if (auto arg = dyn_cast<ArgOp>(op)) {
       NamedAttribute arg_attr = op->getAttrs()[0];
       if (auto intAttr =
@@ -102,7 +89,6 @@ struct SimpleCycleAnalysisPass
     } else if (auto func = dyn_cast<mlir::func::FuncOp>(op)) {
       funcMap[func.getNameAttr()] = &func.getBody();
     } else if (auto funcCost = dyn_cast<FnCostOp>(op)) {
-      // TODO: Make this symbolic
       llvm::SmallVector<mlir::Operation *> allOps;
 
       analysis_func->walk(
@@ -116,10 +102,9 @@ struct SimpleCycleAnalysisPass
               solver.ctx().int_const(cost_v.c_str())); // create a new cost var
           z3::expr post_cost = cost_var[cost_var.size() - 1];
           z3::expr pre_cost = cost_var[cost_var.size() - 2];
-          int64_t cost_val = cost_map_res->second;
+          z3::expr op_cost = evaluateCostRegion(cost_map_res->second, solver);
           z3::expr cost_expr =
-              (post_cost == pre_cost + solver.ctx().int_val(
-                                           cost_val)); // increment per op cost
+              (post_cost == pre_cost + op_cost); // increment per op cost
 
           solver.add(cost_expr); // add to solver
         } else {
@@ -132,54 +117,48 @@ struct SimpleCycleAnalysisPass
       auto region_num = cmpOp->getParentRegion()->getRegionNumber();
       auto pre_post_cost =
           constant_costs.find(static_cast<HoareStates>(region_num));
-      z3::expr p = solver.ctx().bool_val(true);
-      z3::expr q =
-          solver.ctx().bool_val(false); // should return false if nothing else?
-      if (region_num == 0) {
-        if (pred == CmpPredicate::eq) {
-          p = z3::expr(
-              cost_var[0] ==
-              solver.ctx().int_val(pre_post_cost->second)); // precondition
-        } else if (pred == CmpPredicate::ne) {
-          p = z3::expr(cost_var[0] !=
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::lt) {
-          p = z3::expr(cost_var[0] <
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::le) {
-          p = z3::expr(cost_var[0] <=
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::gt) {
-          p = z3::expr(cost_var[0] >
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::ge) {
-          p = z3::expr(cost_var[0] >=
-                       solver.ctx().int_val(pre_post_cost->second));
-        }
-        solver.add(p);
+      if (pre_post_cost == constant_costs.end()) {
+        llvm::outs() << "no constant cost registered for conditions region "
+                     << region_num << "\n";
       } else {
-        if (pred == CmpPredicate::eq) {
-          q = z3::expr(
-              cost_var[cost_var.size() - 1] !=
-              solver.ctx().int_val(pre_post_cost->second)); // postcondition
-        } else if (pred == CmpPredicate::ne) {
-          q = z3::expr(cost_var[cost_var.size() - 1] ==
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::lt) {
-          q = z3::expr(cost_var[cost_var.size() - 1] >=
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::le) {
-          q = z3::expr(cost_var[cost_var.size() - 1] >
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::gt) {
-          q = z3::expr(cost_var[cost_var.size() - 1] <=
-                       solver.ctx().int_val(pre_post_cost->second));
-        } else if (pred == CmpPredicate::ge) {
-          q = z3::expr(cost_var[cost_var.size() - 1] <
-                       solver.ctx().int_val(pre_post_cost->second));
+        z3::expr expected_cost =
+            evaluateCostRegion(pre_post_cost->second, solver);
+        z3::expr p = solver.ctx().bool_val(true);
+        z3::expr q = solver.ctx().bool_val(
+            false); // should return false if nothing else?
+        if (region_num == 0) {
+          if (pred == CmpPredicate::eq) {
+            p = z3::expr(cost_var[0] == expected_cost); // precondition
+          } else if (pred == CmpPredicate::ne) {
+            p = z3::expr(cost_var[0] != expected_cost);
+          } else if (pred == CmpPredicate::lt) {
+            p = z3::expr(cost_var[0] < expected_cost);
+          } else if (pred == CmpPredicate::le) {
+            p = z3::expr(cost_var[0] <= expected_cost);
+          } else if (pred == CmpPredicate::gt) {
+            p = z3::expr(cost_var[0] > expected_cost);
+          } else if (pred == CmpPredicate::ge) {
+            p = z3::expr(cost_var[0] >= expected_cost);
+          }
+          solver.add(p);
+        } else {
+          if (pred == CmpPredicate::eq) {
+            q = z3::expr(cost_var[cost_var.size() - 1] !=
+                         expected_cost); // postcondition
+          } else if (pred == CmpPredicate::ne) {
+            q = z3::expr(cost_var[cost_var.size() - 1] == expected_cost);
+          } else if (pred == CmpPredicate::lt) {
+            q = z3::expr(cost_var[cost_var.size() - 1] >= expected_cost);
+          } else if (pred == CmpPredicate::le) {
+            q = z3::expr(cost_var[cost_var.size() - 1] > expected_cost);
+          } else if (pred == CmpPredicate::gt) {
+            q = z3::expr(cost_var[cost_var.size() - 1] <= expected_cost);
+          } else if (pred == CmpPredicate::ge) {
+            q = z3::expr(cost_var[cost_var.size() - 1] < expected_cost);
+          }
+          solver.add(q); // if satisfiable -> assignment exists s.t. perf
+                         // counter doesn't equal expected value?
         }
-        solver.add(q); // if satisfiable -> assignment exists s.t. perf
-                       // counter doesn't equal expected value?
       }
     } else if (auto assumeOp = dyn_cast<AssumeOp>(op)) {
 // todo: trigger traceup from the provided register argument, evaluate or
@@ -207,6 +186,52 @@ struct SimpleCycleAnalysisPass
     }
     for (Region &region : op->getRegions())
       visitRegion(region, state, solver, cost_var);
+  }
+
+  z3::expr evaluateCostRegion(mlir::Region *costRegion, z3::solver &solver) {
+    z3::context &ctx = solver.ctx();
+    llvm::DenseMap<mlir::Value, z3::expr> valueExprs;
+    std::optional<z3::expr> result;
+    for (mlir::Block &block : costRegion->getBlocks()) {
+      for (mlir::Operation &regionOp : block.getOperations()) {
+        std::optional<z3::expr> expr;
+        if (auto constOp = dyn_cast<arith::ConstantOp>(regionOp)) {
+          if (auto intAttr = dyn_cast<IntegerAttr>(constOp.getValue())) {
+            llvm::SmallString<40> valueStr;
+            intAttr.getValue().toString(valueStr, 10, false);
+            expr = ctx.int_val(valueStr.c_str());
+          }
+        } else if (auto symOp = dyn_cast<SymCostOp>(regionOp)) {
+          std::string symName = ("sym_" + symOp.getSymAttr().getValue()).str();
+          expr = ctx.int_const(symName.c_str());
+        } else if (isa<arith::AddIOp, arith::MulIOp>(regionOp)) {
+          auto lhs = valueExprs.find(regionOp.getOperand(0));
+          auto rhs = valueExprs.find(regionOp.getOperand(1));
+          if (lhs != valueExprs.end() && rhs != valueExprs.end()) {
+            expr = isa<arith::AddIOp>(regionOp) ? (lhs->second + rhs->second)
+                                                : (lhs->second * rhs->second);
+          } else {
+            llvm::outs() << "unresolvable operand in cost expression for "
+                         << regionOp.getName().getStringRef() << "\n";
+          }
+        } else if (auto yieldOp = dyn_cast<YieldOp>(regionOp)) {
+          if (mlir::Value yielded = yieldOp.getValue()) {
+            auto it = valueExprs.find(yielded);
+            if (it != valueExprs.end()) {
+              result = it->second;
+            }
+          }
+        } else {
+          llvm::outs() << "unsupported op in cost region: "
+                       << regionOp.getName().getStringRef() << "\n";
+        }
+        if (expr && regionOp.getNumResults() > 0) {
+          valueExprs.insert({regionOp.getResult(0), *expr});
+          result = expr;
+        }
+      }
+    }
+    return result.value_or(ctx.int_val(0));
   }
 
   void visitRegion(Region &region, AsmState *state, z3::solver &solver,
--- test/lit_tests/perfify/roundtrip.mlir
+++ test/lit_tests/perfify/roundtrip.mlir
@@ -13,36 +13,52 @@ module {
       return %res : i64
    }
 
+   perfify.define_symbol @C : i64
+
     perfify.assumptions { // operation in the dialect
      perfify.cost "arith.muli" {
-      arith.constant 3 // op
-      perfify.yield
+      %c = perfify.sym_cost @C : i64
+      %c2 = arith.constant 2
+      %sq = arith.muli %c, %c : i64
+      %ad = arith.addi %sq, %c2 : i64
+      perfify.yield %ad : i64
      }
      perfify.cost "func.return" {
-      arith.constant 0
+      %c2 = arith.constant 0
       perfify.yield
      }
      perfify.cost "scf.yield" {
-      arith.constant 0
+      %c2 = arith.constant 0
       perfify.yield
      }
      perfify.cost "arith.cmpi" {
-      arith.constant 0
+      %c2 = arith.constant 0
       perfify.yield
      }
      perfify.cost "scf.if" {
-      arith.constant 0
+      %c2 = arith.constant 0
       perfify.yield
      }
    
      perfify.conditions @foo true pre { // true here meaning verification is enabled
         %b0 = perfify.arg 0
-        %c0 = perfify.constant_cost 0 : !perfify.cost
+        %c0 = perfify.constant_cost {
+         arith.constant 0
+         perfify.yield
+        } : !perfify.cost
         %cmp = perfify.cmp eq, %c0, %b0
         perfify.assume %cmp
      } post {
         %cost = perfify.fn_cost : !perfify.cost // compute the value of the defined operation (func.return)
-        %c9 = perfify.constant_cost 9 : !perfify.cost // set up our cost as 9
+        %c9 = perfify.constant_cost {
+            %c = perfify.sym_cost @C : i64
+            %c2 = arith.constant 2
+            %sq = arith.muli %c, %c : i64
+            %ad = arith.addi %sq, %c2 : i64
+            %c3 = arith.constant 3
+            %ae = arith.muli %c3, %ad : i64
+            perfify.yield %ae : i64
+        } : !perfify.cost
         %cmp = perfify.cmp eq, %cost, %c9
         perfify.assume %cmp
      }
@@ -50,34 +66,61 @@ module {
 }
 
 // CHECK: module {
-// CHECK-NEXT:  func.func @foo(%arg0: i64, %arg1: i64) -> i64 {
-// CHECK-NEXT:    %0 = arith.cmpi eq, %arg0, %arg1 : i64
-// CHECK-NEXT:    %1 = scf.if %0 -> (i64) {
-// CHECK-NEXT:      %2 = arith.muli %arg1, %arg1 : i64
-// CHECK-NEXT:      %3 = arith.muli %2, %2 : i64
-// CHECK-NEXT:      %4 = arith.muli %3, %3 : i64
-// CHECK-NEXT:      scf.yield %4 : i64
-// CHECK-NEXT:    } else {
-// CHECK-NEXT:      scf.yield %arg1 : i64
-// CHECK-NEXT:    }
-// CHECK-NEXT:    return %1 : i64
-// CHECK-NEXT:  }
-// CHECK-NEXT:  perfify.assumptions {
-// CHECK-NEXT:    perfify.cost "arith.muli" 3 : i64
-// CHECK-NEXT:    perfify.cost "func.return" 0 : i64
-// CHECK-NEXT:    perfify.cost "scf.yield" 0 : i64
-// CHECK-NEXT:    perfify.cost "arith.cmpi" 0 : i64
-// CHECK-NEXT:    perfify.cost "scf.if" 0 : i64
-// CHECK-NEXT:    perfify.conditions @foo true pre {
-// CHECK-NEXT:      %0 = perfify.arg 0
-// CHECK-NEXT:      %1 = perfify.constant_cost 0 : !perfify.cost
-// CHECK-NEXT:      %2 = perfify.cmp eq, %1, %0
-// CHECK-NEXT:      perfify.assume %2 {satres = true}
-// CHECK-NEXT:    } post {
-// CHECK-NEXT:      %0 = perfify.fn_cost : !perfify.cost
-// CHECK-NEXT:      %1 = perfify.constant_cost 9 : !perfify.cost
-// CHECK-NEXT:      %2 = perfify.cmp eq, %0, %1
-// CHECK-NEXT:      perfify.assume %2 {satres = true}
-// CHECK-NEXT:    }
-// CHECK-NEXT:  }
+// CHECK-NEXT:   func.func @foo(%arg0: i64, %arg1: i64) -> i64 {
+// CHECK-NEXT:     %0 = arith.cmpi eq, %arg0, %arg1 : i64
+// CHECK-NEXT:     %1 = scf.if %0 -> (i64) {
+// CHECK-NEXT:       %2 = arith.muli %arg1, %arg1 : i64
+// CHECK-NEXT:       %3 = arith.muli %2, %2 : i64
+// CHECK-NEXT:       %4 = arith.muli %3, %3 : i64
+// CHECK-NEXT:       scf.yield %4 : i64
+// CHECK-NEXT:     } else {
+// CHECK-NEXT:       scf.yield %arg1 : i64
+// CHECK-NEXT:     }
+// CHECK-NEXT:     return %1 : i64
+// CHECK-NEXT:   }
+// CHECK-NEXT:   perfify.define_symbol @C : i64
+// CHECK-NEXT:   perfify.assumptions {
+// CHECK-NEXT:     perfify.cost "arith.muli" {
+// CHECK-NEXT:       %0 = perfify.sym_cost @C : i64
+// CHECK-NEXT:       %c2_i64 = arith.constant 2 : i64
+// CHECK-NEXT:       %1 = arith.muli %0, %0 : i64
+// CHECK-NEXT:       %2 = arith.addi %1, %c2_i64 : i64
+// CHECK-NEXT:       perfify.yield %2 : i64
+// CHECK-NEXT:     }
+// CHECK-NEXT:     perfify.cost "func.return" {
+// CHECK-NEXT:       %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:       perfify.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:     perfify.cost "scf.yield" {
+// CHECK-NEXT:       %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:       perfify.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:     perfify.cost "arith.cmpi" {
+// CHECK-NEXT:       %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:       perfify.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:     perfify.cost "scf.if" {
+// CHECK-NEXT:       %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:       perfify.yield
+// CHECK-NEXT:     }
+// CHECK-NEXT:     perfify.conditions @foo true pre {
+// CHECK-NEXT:       %0 = perfify.arg 0
+// CHECK-NEXT:       %1 = perfify.constant_cost 0 : !perfify.cost
+// CHECK-NEXT:       %2 = perfify.cmp eq, %1, %0
+// CHECK-NEXT:       perfify.assume %2 {satres = true}
+// CHECK-NEXT:       } post {
+// CHECK-NEXT:             %0 = perfify.fn_cost : !perfify.cost
+// CHECK-NEXT:             %1 = perfify.constant_cost {
+// CHECK-NEXT:               %3 = perfify.sym_cost @C : i64
+// CHECK-NEXT:               %c2_i64 = arith.constant 2 : i64
+// CHECK-NEXT:               %4 = arith.muli %3, %3 : i64
+// CHECK-NEXT:               %5 = arith.addi %4, %c2_i64 : i64
+// CHECK-NEXT:               %c3_i64 = arith.constant 3 : i64
+// CHECK-NEXT:               %6 = arith.muli %c3_i64, %5 : i64
+// CHECK-NEXT:               perfify.yield %6 : i64
+// CHECK-NEXT:             } : !perfify.cost
+// CHECK-NEXT:             %2 = perfify.cmp eq, %0, %1
+// CHECK-NEXT:             perfify.assume %2 {satres = true}
+// CHECK-NEXT:     }
+// CHECK-NEXT:   }
 // CHECK-NEXT: }
